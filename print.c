3fd66cc69b89568199469b26e2ccf1afe4cc4bdb
Linus Torvalds <torvalds@linux-foundation.org>
2011-09-20 22:09:38 -0700

    Printing: use pango markup for text generation
    This gets us text wrapping etc.  I think I have some serious memory leak
    somewhere, though, because if I print out all my dives it eventually
    ends up with broken dives and doesn't complete.  But I am going to
    commit this as a "it kind of works" point.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- print.c
+++ print.c
@@ -7,55 +7,101 @@
 #include "display.h"
 #include "display-gtk.h"
 
-static void show_text(cairo_t *cr, int size, double x, double y, const char *fmt, ...)
+/* Why doesn't pango/gtk have these quoting functions? */
+static inline int add_char(char *buffer, size_t size, int len, char c)
 {
-	va_list args;
-	char buffer[256], *p;
+	if (len < size)
+		buffer[len++] = c;
+	return len;
+}
 
-	va_start(args, fmt);
-	vsnprintf(buffer, sizeof(buffer), fmt, args);
-	va_end(args);
+/* Add an escape string "atomically" - all or nothing */
+static int add_str(char *buffer, size_t size, int len, const char *s)
+{
+	int oldlen = len;
+	char c;
+	while ((c = *s++) != 0) {
+		if (len >= size)
+			return oldlen;
+		buffer[len++] = c;
+	}
+	return len;
+}
 
-	cairo_set_font_size(cr, size);
+static int add_quoted_string(char *buffer, size_t size, int len, const char *s)
+{
+	if (!s)
+		return len;
 
-	p = buffer;
-	do {
-		char *n = strchr(p, '\n');
-		if (n)
-			*n++ = 0;
-		cairo_move_to(cr, x, y);
-		cairo_show_text(cr, p);
-		p = n;
-		y += size;
-	} while (p);
+	/* Room for '\0' */
+	size--;
+	for (;;) {
+		const char *escape;
+		unsigned char c = *s++;
+		switch(c) {
+		default:
+			len = add_char(buffer, size, len, c);
+			continue;
+		case 0:
+			escape = "\n";
+			break;
+		case '&':
+			escape = "&amp;";
+			break;
+		case '>':
+			escape = "&gt;";
+			break;
+		case '<':
+			escape = "&lt;";
+			break;
+		}
+		len = add_str(buffer, size, len, escape);
+		if (c)
+			continue;
+		buffer[len] = 0;
+		return len;
+	}
 }
 
 /*
  * You know what? Maybe somebody can do a real Pango layout thing.
- *
- * I'm going to do this with the cairo engine instead. I can only learn so
- * many new interfaces.
+ * This is hacky.
  */
 static void show_dive_text(struct dive *dive, cairo_t *cr, double w, double h)
 {
+	int len;
+	PangoLayout *layout;
 	struct tm *tm;
+	char buffer[1024], divenr[20];
+
+	layout = pango_cairo_create_layout(cr);
+	pango_layout_set_width(layout, w * PANGO_SCALE);
+	pango_layout_set_height(layout, h * PANGO_SCALE * 0.9);
+	pango_layout_set_wrap(layout, PANGO_WRAP_WORD_CHAR);
+
+	*divenr = 0;
+	if (dive->number)
+		snprintf(divenr, sizeof(divenr), "Dive #%d - ", dive->number);
+
 
 	tm = gmtime(&dive->when);
-	show_text(cr, 16, 0, 2, "Dive #%d - %s, %s %d, %d    %d:%02d",
-		dive->number,
+	len = snprintf(buffer, sizeof(buffer),
+		"<span size=\"large\">%s%s, %s %d, %d   %d:%02d</span>\n",
+		divenr,
 		weekday(tm->tm_wday),
 		monthname(tm->tm_mon),
 		tm->tm_mday, tm->tm_year + 1900,
 		tm->tm_hour, tm->tm_min);
 
-	show_text(cr, 10, w*0.6, 0,
-		"Max depth: %d ft\nDuration: %d:%02d",
-		to_feet(dive->maxdepth),
-		dive->duration.seconds / 60,
-		dive->duration.seconds % 60);
+	len = add_quoted_string(buffer, sizeof(buffer), len, dive->location);
+	len = add_quoted_string(buffer, sizeof(buffer), len, dive->notes);
 
-	show_text(cr, 10, 0, 20, "%s", dive->location ?: "");
-	show_text(cr, 10, 0, 30, "%s", dive->notes ?: "");
+	pango_layout_set_markup(layout, buffer, -1);
+
+	cairo_move_to(cr, 0, 0);
+	pango_cairo_show_layout(cr, layout);
+
+	g_object_unref(layout);
 }
 
 static void show_dive_profile(struct dive *dive, cairo_t *cr, double w, double h)
@@ -77,15 +123,20 @@ static void print(int divenr, cairo_t *cr, double x, double y, double w, double 
 	cairo_save(cr);
 	cairo_translate(cr, x, y);
 
+	/* Plus 5% on all sides */
+	cairo_translate(cr, w/20, h/20);
+	w *= 0.9; h *= 0.9;
+
 	/* We actually want to scale the text and the lines now */
 	cairo_scale(cr, 0.5, 0.5);
 
-	/* Dive plot in the upper 75% - note the scaling */
-	show_dive_profile(dive, cr, w*2, h*1.5);
+	/* Dive plot in the upper two thirds - note the scaling */
+	show_dive_profile(dive, cr, w*2, h*1.33);
 
-	/* Dive information in the lower 25% */
-	cairo_translate(cr, 0, h*1.5);
-	show_dive_text(dive, cr, w*2, h*0.5);
+	/* Dive information in the lower third */
+	cairo_translate(cr, 0, h*1.33);
+
+	show_dive_text(dive, cr, w*2, h*0.67);
 
 	cairo_restore(cr);
 }
@@ -109,8 +160,8 @@ static void draw_page(GtkPrintOperation *operation,
 	print(nr+1, cr, w,   0, w, h);
 	print(nr+2, cr, 0,   h, w, h);
 	print(nr+3, cr, w,   h, w, h);
-	print(nr+2, cr, 0, 2*h, w, h);
-	print(nr+3, cr, w, 2*h, w, h);
+	print(nr+4, cr, 0, 2*h, w, h);
+	print(nr+5, cr, w, 2*h, w, h);
 }
 
 static void begin_print(GtkPrintOperation *operation, gpointer user_data)
@@ -125,6 +176,7 @@ void do_print(void)
 	GtkPrintOperation *print;
 	GtkPrintOperationResult res;
 
+	repaint_dive();
 	print = gtk_print_operation_new();
 	if (settings != NULL)
 		gtk_print_operation_set_print_settings(print, settings);
