9b9355c770f39af6e140432645ca9f4b134cd1f2
Dirk Hohndel <dirk@hohndel.org>
2011-11-27 09:08:55 -0800

    Fix mingw-make.sh to find correct xslt-config
    I hadn't noticed that xslt was available as cross built library as well.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/windows/mingw-make.sh
+++ packaging/windows/mingw-make.sh
@@ -6,7 +6,9 @@
 # the paths work for the default mingw32 install on OpenSUSE - adjust as
 # necessary
 
+export PATH=/usr/i686-w64-mingw32/sys-root/mingw/bin:$PATH
 make CC=i686-w64-mingw32-gcc \
 	PKGCONFIG=i686-w64-mingw32-pkg-config \
 	PKG_CONFIG_PATH=/usr/i686-w64-mingw32/sys-root/i686-w64-mingw32/lib/pkgconfig/ \
+	XSLTCONFIG=/usr/i686-w64-mingw32/sys-root/mingw/bin/xslt-config \
 	XML2CONFIG=/usr/i686-w64-mingw32/sys-root/mingw/bin/xml2-config NAME=subsurface.exe
