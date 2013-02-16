bb44e108a3a53b22eb2b64f82e944852bea63c41
Dirk Hohndel <dirk@hohndel.org>
2013-02-16 15:53:53 -0800

    Force recreation of the Windows .nsi file when cross building
    This way we make sure that the version number for the installer is correct.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/windows/mingw-make.sh
+++ packaging/windows/mingw-make.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 #
 # just a small shell script that is used to invoke make with the right
 # parameters to cross compile a binary for Windows
 #
 # the paths work for the default mingw32 install on OpenSUSE - adjust as
 # necessary
+
+# force recreation of the nsi file in order to get the correct version
+# number
+rm packaging/windows/subsurface.nsi
 
 export PATH=/usr/i686-w64-mingw32/sys-root/mingw/bin:$PATH
 make CC=i686-w64-mingw32-gcc \
