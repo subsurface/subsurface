subsurface/subsurface

commit 90d3c5614a9c952a50e582e2dff6e21419c64729

Centralising and redefining values as integers
This patch centralizes the definition for surface pressure, oxygen in
air, (re)defines all such values as plain integers and adapts calculations.

It eliminates 11 (!) occurrences of definitions for surface pressure and
also a few for oxygen in air.

It also rewrites the calculation for EAD, END and EADD using the new
definitons, harmonizing it for OC and CC and fixes a bug for EADD OC
calculation.

And finally it removes the unneeded variable entry_ead in gtk-gui.c.

Jan

Signed-off-by: Jan Schubert <Jan.Schubert@GMX.li>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

dive.h
@@ -11,11 +11,13 @@
 #include <libxml/tree.h>
 #include <openssl/sha.h>
 
-#define O2_IN_AIR 0.20942
-#define N2_IN_AIR 0.78084 // has been set to 0.7902 before to ignore other components of air
-#define O2_DENSITY 1.429  // Gramm/Liter
-#define N2_DENSITY 1.251
-#define HE_DENSITY 0.1786
+#define O2_IN_AIR        209     // permille
+#define N2_IN_AIR        781
+#define O2_DENSITY       1429    // mg/Liter
+#define N2_DENSITY       1251
+#define HE_DENSITY       179
+#define SURFACE_PRESSURE 1013    // mbar
+#define SURFACE_PRESSURE_STRING "1013"
 
 
 /*
@@ -215,12 +217,12 @@ static inline int to_PSI(pressure_t pressure)
 
 static inline double bar_to_atm(double bar)
 {
-	return bar / 1.01325;
+	return bar / SURFACE_PRESSURE * 1000;
 }
 
 static inline double to_ATM(pressure_t pressure)
 {
-	return pressure.mbar / 1013.25;
+	return pressure.mbar / SURFACE_PRESSURE;
 }
 
 static inline int mbar_to_PSI(int mbar)
@@ -346,7 +348,7 @@ struct dive {
 static inline int depth_to_mbar(int depth, struct dive *dive)
 {
 	double specific_weight = 1.03 * 0.981;
-	int surface_pressure = 1013;
+	int surface_pressure = SURFACE_PRESSURE;
 	if (dive->salinity)
 		specific_weight = dive->salinity / 10000.0 * 0.981;
 	if (dive->surface_pressure.mbar)
@@ -583,7 +585,6 @@ extern const char *subsurface_gettext_domainpath(char *);
 extern gboolean subsurface_os_feature_available(os_feature_t);
 extern void subsurface_command_line_init(gint *, gchar ***);
 extern void subsurface_command_line_exit(gint *, gchar ***);
-#define AIR_PERMILLE 209
 
 #define FRACTION(n,x) ((unsigned)(n)/(x)),((unsigned)(n)%(x))
 

deco.c
@@ -134,7 +134,7 @@ static double tissue_tolerance_calc(const struct dive *dive)
 double add_segment(double pressure, struct gasmix *gasmix, int period_in_seconds, double ccpo2, const struct dive *dive)
 {
 	int ci;
-	int fo2 = gasmix->o2.permille ? gasmix->o2.permille : 209;
+	int fo2 = gasmix->o2.permille ? gasmix->o2.permille : O2_IN_AIR;
 	double ppn2 = (pressure - WV_PRESSURE) * (1000 - fo2 - gasmix->he.permille) / 1000.0;
 	double pphe = (pressure - WV_PRESSURE) * gasmix->he.permille / 1000.0;
 
@@ -206,7 +206,7 @@ void clear_deco(double surface_pressure)
 {
 	int ci;
 	for (ci = 0; ci < 16; ci++) {
-		tissue_n2_sat[ci] = (surface_pressure - WV_PRESSURE) * N2_IN_AIR;
+		tissue_n2_sat[ci] = (surface_pressure - WV_PRESSURE) * N2_IN_AIR / 1000;
 		tissue_he_sat[ci] = 0.0;
 		tissue_tolerated_ambient_pressure[ci] = 0.0;
 	}

dive.c
@@ -269,8 +269,8 @@ static void sanitize_gasmix(struct gasmix *mix)
 	if (!he) {
 		if (!o2)
 			return;
-		/* 20.9% or 21% O2 is just air */
-		if (o2 >= 209 && o2 <= 210) {
+		/* 20.8% to 21% O2 is just air */
+		if (o2 >= (O2_IN_AIR - 1) && o2 <= (O2_IN_AIR + 1)) {
 			mix->o2.permille = 0;
 			return;
 		}

divelist.c
@@ -525,7 +525,7 @@ static void get_dive_gas(struct dive *dive, int *o2_p, int *he_p, int *o2low_p)
 		if (cylinder_none(cyl))
 			continue;
 		if (!o2)
-			o2 = AIR_PERMILLE;
+			o2 = O2_IN_AIR;
 		if (o2 < mino2)
 			mino2 = o2;
 		if (he > maxhe)
@@ -539,7 +539,7 @@ newmax:
 		maxo2 = o2;
 	}
 	/* All air? Show/sort as "air"/zero */
-	if (!maxhe && maxo2 == AIR_PERMILLE && mino2 == maxo2)
+	if (!maxhe && maxo2 == O2_IN_AIR && mino2 == maxo2)
 		maxo2 = mino2 = 0;
 	*o2_p = maxo2;
 	*he_p = maxhe;
@@ -721,7 +721,7 @@ static int active_o2(struct dive *dive, struct divecomputer *dc, duration_t time
 	struct event *event = dc->events;
 
 	if (!o2permille)
-		o2permille = AIR_PERMILLE;
+		o2permille = O2_IN_AIR;
 
 	for (event = dc->events; event; event = event->next) {
 		if (event->time.seconds > time.seconds)
@@ -849,7 +849,7 @@ static int get_divenr(struct dive *dive)
 	return divenr;
 }
 
-static struct gasmix air = { .o2.permille = 209 };
+static struct gasmix air = { .o2.permille = O2_IN_AIR };
 
 /* take into account previous dives until there is a 48h gap between dives */
 double init_decompression(struct dive *dive)
@@ -881,7 +881,7 @@ double init_decompression(struct dive *dive)
 		/* again skip dives from different trips */
 		if (dive->divetrip && dive->divetrip != pdive->divetrip)
 			continue;
-		surface_pressure = pdive->surface_pressure.mbar ? pdive->surface_pressure.mbar / 1000.0 : 1.013;
+		surface_pressure = (pdive->surface_pressure.mbar ? pdive->surface_pressure.mbar : SURFACE_PRESSURE) / 1000;
 		if (!deco_init) {
 			clear_deco(surface_pressure);
 			deco_init = TRUE;
@@ -907,7 +907,7 @@ double init_decompression(struct dive *dive)
 	/* add the final surface time */
 	if (lasttime && dive->when > lasttime) {
 		surface_time = dive->when - lasttime;
-		surface_pressure = dive->surface_pressure.mbar ? dive->surface_pressure.mbar / 1000.0 : 1.013;
+		surface_pressure = (dive->surface_pressure.mbar ? dive->surface_pressure.mbar : SURFACE_PRESSURE) / 1000;
 		tissue_tolerance = add_segment(surface_pressure, &air, surface_time, 0.0, dive);
 #if DECO_CALC_DEBUG & 2
 		printf("after surface intervall of %d:%02u\n", FRACTION(surface_time,60));
@@ -915,7 +915,7 @@ double init_decompression(struct dive *dive)
 #endif
 	}
 	if (!deco_init) {
-		double surface_pressure = dive->surface_pressure.mbar ? dive->surface_pressure.mbar / 1000.0 : 1.013;
+		double surface_pressure = (dive->surface_pressure.mbar ? dive->surface_pressure.mbar : SURFACE_PRESSURE) / 1000;
 		clear_deco(surface_pressure);
 #if DECO_CALC_DEBUG & 2
 		printf("no previous dive\n");

equipment.c
@@ -422,7 +422,7 @@ static void show_cylinder(cylinder_t *cyl, struct cylinder_widget *cylinder)
 	o2 = cyl->gasmix.o2.permille / 10.0;
 	he = cyl->gasmix.he.permille / 10.0;
 	if (!o2)
-		o2 = AIR_PERMILLE / 10.0;
+		o2 = O2_IN_AIR / 10.0;
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(cylinder->o2), o2);
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(cylinder->he), he);
 }

gtk-gui.c
@@ -531,7 +531,7 @@ static void preferences_dialog(GtkWidget *w, gpointer data)
 {
 	int result;
 	GtkWidget *dialog, *notebook, *font, *frame, *box, *vbox, *button, *xmlfile_button;
-	GtkWidget *entry_po2, *entry_pn2, *entry_phe, *entry_mod, *entry_ead, *entry_gflow, *entry_gfhigh;
+	GtkWidget *entry_po2, *entry_pn2, *entry_phe, *entry_mod, *entry_gflow, *entry_gfhigh;
 	const char *current_default, *new_default;
 	char threshold_text[10], mod_text[10], utf8_buf[128];
 	struct preferences oldprefs = prefs;

libdivecomputer.c
@@ -127,7 +127,7 @@ static int parse_gasmixes(device_data_t *devdata, struct dive *dive, dc_parser_t
 		he = gasmix.helium * 1000 + 0.5;
 
 		/* Ignore bogus data - libdivecomputer does some crazy stuff */
-		if (o2 + he <= AIR_PERMILLE || o2 >= 1000)
+		if (o2 + he <= O2_IN_AIR || o2 >= 1000)
 			o2 = 0;
 		if (he < 0 || he >= 800 || o2+he >= 1000)
 			he = 0;

parse-xml.c
@@ -677,7 +677,7 @@ void add_gas_switch_event(struct dive *dive, struct divecomputer *dc, int second
 	int value;
 
 	if (!o2)
-		o2 = AIR_PERMILLE;
+		o2 = O2_IN_AIR;
 	o2 = (o2+5) / 10;
 	he = (he+5) / 10;
 	value = o2 + (he << 16);

commit d753805542cf15a35000ca3b952c719a3470d388

Fix cylinder air size calculations
Commit 90d3c5614a9c ("Centralising and redefining values as integers")
broke SAC-rate calculations. In particular, it changed "to_ATM()": to
use the centralized SURFACE_PRESSURE helper define, but in the process
it changed a floating point calculation to an integer calculation, and
it threw away all the fractional details. Any user of "to_ATM()"
basically dropped to an accuracy of a single atmosphere.

The good news is that we didn't use to_ATM() for things like depth
calculations, but only for cylinder pressures. As a result, the error
ends up being relatively small, since the pressures involved are big,
and thus the error of rounding to whole atmospheres is usually in the
1% range. The cylinder sizing tends to be off by more than that
anyway. But it was wrong, and not intentional.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

dive.h
@@ -205,7 +205,7 @@ static inline double bar_to_atm(double bar)
 
 static inline double to_ATM(pressure_t pressure)
 {
-	return pressure.mbar / SURFACE_PRESSURE;
+	return pressure.mbar / (double) SURFACE_PRESSURE;
 }
 
 static inline int mbar_to_PSI(int mbar)