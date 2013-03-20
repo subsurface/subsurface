ca0ecc234adc8948e093f47feaf622435849f48a
Salvador Cuñat <salvador.cunat@gmail.com>
2013-03-20 20:30:57 +0100

    Print_c Make colorButton remember last state during session
    While colorButton defaults to "color" in first access to print menu, it seems
    preferable to keep the last state selected by the user.  At least until exit session.
    
    Signed-off-by: Salvador Cuñat <salvador.cunat@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- print.c
+++ print.c
@@ -986,7 +986,7 @@ static GtkWidget *print_dialog(GtkPrintOperation *operation, gpointer user_data)
 	colorButton = gtk_check_button_new_with_label(_("Print in color"));
 	g_signal_connect(G_OBJECT(colorButton), "toggled",
 		G_CALLBACK(color_selection_toggle), NULL);
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(colorButton),TRUE);
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(colorButton),print_options.color_selected);
 	gtk_box_pack_start(GTK_BOX(box), colorButton, FALSE, FALSE, 2);
 
 	frame1 = gtk_frame_new(_("Ordering"));
