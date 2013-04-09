1182c989973a3006a482413ddd2d523be64e5443
Amit Chaudhuri <amit.k.chaudhuri@gmail.com>
2013-04-09 17:26:23 +0100

    Add stub slots for all menu items
    Naming of QActions was inconsistent wrt abbreviations - fixed.
    
    Add stub slots for each action relying on connect by name.
    
    Add qDebug() message to allow people to check that menu items fire
    slots; not really necessary but may provide some reassurance as we build
    familiarity with Qt.
    
    Some changes to display text for menu items (e.g. Tree becomes View
    All).
    
    Signed-off-by: Amit Chaudhuri <amit.k.chaudhuri@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-ui/mainwindow.cpp
+++ qt-ui/mainwindow.cpp
@@ -11,5 +11,141 @@ MainWindow::MainWindow() : ui(new Ui::MainWindow())
 
 void MainWindow::on_actionNew_triggered()
 {
-    qDebug() << "actionNew";
+	qDebug() << "actionNew";
 }
+
+void MainWindow::on_actionOpen_triggered()
+{
+	qDebug() << "actionOpen";
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+	qDebug() << "actionSave";
+}
+
+void MainWindow::on_actionSaveAs_triggered()
+{
+	qDebug() << "actionSaveAs";
+}
+void MainWindow::on_actionClose_triggered()
+{
+	qDebug() << "actionClose";
+}
+
+void MainWindow::on_actionImport_triggered()
+{
+	qDebug() << "actionImport";
+}
+
+void MainWindow::on_actionExportUDDF_triggered()
+{
+	qDebug() << "actionExportUDDF";
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+	qDebug() << "actionPrint";
+}
+
+void MainWindow::on_actionPreferences_triggered()
+{
+	qDebug() << "actionPreferences";
+}
+
+void MainWindow::on_actionQuit_triggered()
+{
+	qDebug() << "actionQuit";
+}
+
+void MainWindow::on_actionDownloadDC_triggered()
+{
+	qDebug() << "actionDownloadDC";
+}
+
+void MainWindow::on_actionDownloadWeb_triggered()
+{
+	qDebug() << "actionDownloadWeb";}
+
+void MainWindow::on_actionEditDeviceNames_triggered()
+{
+	qDebug() << "actionEditDeviceNames";}
+
+void MainWindow::on_actionAddDive_triggered()
+{
+	qDebug() << "actionAddDive";
+}
+
+void MainWindow::on_actionRenumber_triggered()
+{
+	qDebug() << "actionRenumber";
+}
+
+void MainWindow::on_actionAutoGroup_triggered()
+{
+	qDebug() << "actionAutoGroup";
+}
+
+void MainWindow::on_actionToggleZoom_triggered()
+{
+	qDebug() << "actionToggleZoom";
+}
+
+void MainWindow::on_actionYearlyStatistics_triggered()
+{
+	qDebug() << "actionYearlyStatistics";
+}
+
+void MainWindow::on_actionViewList_triggered()
+{
+	qDebug() << "actionViewList";
+}
+
+void MainWindow::on_actionViewProfile_triggered()
+{
+	qDebug() << "actionViewProfile";
+}
+
+void MainWindow::on_actionViewInfo_triggered()
+{
+	qDebug() << "actionViewInfo";
+}
+
+void MainWindow::on_actionViewAll_triggered()
+{
+	qDebug() << "actionViewAll";
+}
+
+void MainWindow::on_actionPreviousDC_triggered()
+{
+	qDebug() << "actionPreviousDC";
+}
+
+void MainWindow::on_actionNextDC_triggered()
+{
+	qDebug() << "actionNextDC";
+}
+
+void MainWindow::on_actionSelectEvents_triggered()
+{
+	qDebug() << "actionSelectEvents";
+}
+
+void MainWindow::on_actionInputPlan_triggered()
+{
+	qDebug() << "actionInputPlan";
+}
+
+void MainWindow::on_actionAboutSubsurface_triggered()
+{
+	qDebug() << "actionAboutSubsurface";
+}
+
+void MainWindow::on_actionUserManual_triggered()
+{
+	qDebug() << "actionUserManual";
+}
+
+
+
+#include "mainwindow.moc"
--- qt-ui/mainwindow.h
+++ qt-ui/mainwindow.h
@@ -22,7 +22,41 @@ public:
 
 private Q_SLOTS:
 
+	/* file menu action */
 	void on_actionNew_triggered();
+	void on_actionOpen_triggered();
+	void on_actionSave_triggered();
+	void on_actionSaveAs_triggered();
+	void on_actionClose_triggered();
+	void on_actionImport_triggered();
+	void on_actionExportUDDF_triggered();
+	void on_actionPrint_triggered();
+	void on_actionPreferences_triggered();
+	void on_actionQuit_triggered();
+
+	/* log menu actions */
+	void on_actionDownloadDC_triggered();
+	void on_actionDownloadWeb_triggered();
+	void on_actionEditDeviceNames_triggered();
+	void on_actionAddDive_triggered();
+	void on_actionRenumber_triggered();
+	void on_actionAutoGroup_triggered();
+	void on_actionToggleZoom_triggered();
+	void on_actionYearlyStatistics_triggered();
+
+	/* view menu actions */
+	void on_actionViewList_triggered();
+	void on_actionViewProfile_triggered();
+	void on_actionViewInfo_triggered();
+	void on_actionViewAll_triggered();
+	void on_actionPreviousDC_triggered();
+	void on_actionNextDC_triggered();
+
+	/* other menu actions */
+	void on_actionSelectEvents_triggered();
+	void on_actionInputPlan_triggered();
+	void on_actionAboutSubsurface_triggered();
+	void on_actionUserManual_triggered();
 
 private:
 	Ui::MainWindow *ui;
