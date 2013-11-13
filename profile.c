179a5f6f33fc4635cdb65e757370f267973a5e88
Anton Lundin <glance@acc.umu.se>
2013-11-13 19:20:10 +0100

    Remove duplicated SAC data from information overlay
    When reordering the info overlay content to make it jump around less,
    somehow the SAC data got printed twice.
    
    [Dirk Hohndel: rewrote incorrect commit message]
    
    Signed-off-by: Anton Lundin <glance@acc.umu.se>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- profile.c
+++ profile.c
@@ -1276,10 +1276,6 @@ static void plot_string(struct plot_data *entry, char *buf, int bufsize,
 		memcpy(buf2, buf, bufsize);
 		snprintf(buf, bufsize, translate("gettextFromC","%s\nNDL:%umin"), buf2, DIV_UP(entry->ndl, 60));
 	}
-	if (entry->sac && prefs.show_sac) {
-		memcpy(buf2, buf, bufsize);
-		snprintf(buf, bufsize, translate("gettextFromC","%s\nSAC:%2.1fl/min"), buf2, entry->sac / 1000.0);
-	}
 	if (entry->stopdepth_calc && entry->stoptime_calc) {
 		depthvalue = get_depth_units(entry->stopdepth_calc, NULL, &depth_unit);
 		memcpy(buf2, buf, bufsize);
