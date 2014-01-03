subsurface/subsurface

commit f9b7c5dfe9d07b69f6b063cf7102922aa7e17746

Make units in cells consistant in CylindersModel
Now they all have units in the cells.

Fixes #259

Signed-off-by: Anton Lundin <glance@acc.umu.se>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

qt-ui/models.cpp
@@ -114,16 +114,7 @@ QVariant CylindersModel::data(const QModelIndex& index, int role) const
 			// we can't use get_volume_string because the idiotic imperial tank
 			// sizes take working pressure into account...
 			if (cyl->type.size.mliter) {
-				double volume;
-				int mbar = cyl->type.workingpressure.mbar;
-
-				if (mbar && prefs.units.volume == prefs.units.CUFT) {
-					volume = ml_to_cuft(cyl->type.size.mliter);
-					volume *= bar_to_atm(mbar / 1000.0);
-				} else {
-					volume = cyl->type.size.mliter / 1000.0;
-				}
-				ret = QString("%1").arg(volume, 0, 'f', 1);
+				ret = get_volume_string(cyl->type.size, TRUE);
 			}
 			break;
 		case WORKINGPRESS:
@@ -132,15 +123,15 @@ QVariant CylindersModel::data(const QModelIndex& index, int role) const
 			break;
 		case START:
 			if (cyl->start.mbar)
-				ret = get_pressure_string(cyl->start, FALSE);
+				ret = get_pressure_string(cyl->start, TRUE);
 			else if (cyl->sample_start.mbar)
-				ret = get_pressure_string(cyl->sample_start, FALSE);
+				ret = get_pressure_string(cyl->sample_start, TRUE);
 			break;
 		case END:
 			if (cyl->end.mbar)
-				ret = get_pressure_string(cyl->end, FALSE);
+				ret = get_pressure_string(cyl->end, TRUE);
 			else if (cyl->sample_end.mbar)
-				ret = get_pressure_string(cyl->sample_end, FALSE);
+				ret = get_pressure_string(cyl->sample_end, TRUE);
 			break;
 		case O2:
 			ret = percent_string(cyl->gasmix.o2);
@@ -149,10 +140,7 @@ QVariant CylindersModel::data(const QModelIndex& index, int role) const
 			ret = percent_string(cyl->gasmix.he);
 			break;
 		case DEPTH:
-			if (prefs.units.length == prefs.units.FEET)
-				ret = mm_to_feet(cyl->depth.mm);
-			else
-				ret = cyl->depth.mm / 1000;
+			ret = get_depth_string(cyl->depth, TRUE);
 			break;
 		}
 		break;

commit 4cce5df7e22575d5e37f99b15dbf283e2da53ea9

Fix imperial cylinder sizes in equipment tab
The imperial cylinder sizes are not just in cubic feet: they are in
cubic feet of gas at STP. So the imperial/metric difference is not
just about converting blindly from liters to cubic feet, you also have
to take the working pressure of the cylinder into account.

This was broken by commit f9b7c5dfe9d0 ("Make units in cells
consistant in CylindersModel"), because those poor sheltered Swedish
people have never had to work with the wondrous imperial cylinder
sizing, and think that units should make _sense_. Hah.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

helpers.h
@@ -19,7 +19,7 @@ QString get_weight_unit();
 QString get_cylinder_used_gas_string(cylinder_t *cyl, bool showunit = false);
 QString get_temperature_string(temperature_t temp, bool showunit = false);
 QString get_temp_unit();
-QString get_volume_string(volume_t volume, bool showunit = false);
+QString get_volume_string(volume_t volume, bool showunit = false, unsigned int mbar = 0);
 QString get_volume_unit();
 QString get_pressure_string(pressure_t pressure, bool showunit = false);
 QString get_pressure_unit();

qt-gui.cpp
@@ -322,13 +322,15 @@ QString get_temp_unit()
 		return QString(UTF8_DEGREE "F");
 }
 
-QString get_volume_string(volume_t volume, bool showunit)
+QString get_volume_string(volume_t volume, bool showunit, unsigned int mbar)
 {
 	if (prefs.units.volume == units::LITER) {
 		double liter = volume.mliter / 1000.0;
 		return QString("%1%2").arg(liter, 0, 'f', liter >= 40.0 ? 0 : 1 ).arg(showunit ? translate("gettextFromC","l") : "");
 	} else {
 		double cuft = ml_to_cuft(volume.mliter);
+		if (mbar)
+			cuft *= bar_to_atm(mbar / 1000.0);
 		return QString("%1%2").arg(cuft, 0, 'f', cuft >= 20.0 ? 0 : (cuft >= 2.0 ? 1 : 2)).arg(showunit ? translate("gettextFromC","cuft") : "");
 	}
 }

qt-ui/models.cpp
@@ -111,11 +111,8 @@ QVariant CylindersModel::data(const QModelIndex& index, int role) const
 			ret = QString(cyl->type.description);
 			break;
 		case SIZE:
-			// we can't use get_volume_string because the idiotic imperial tank
-			// sizes take working pressure into account...
-			if (cyl->type.size.mliter) {
-				ret = get_volume_string(cyl->type.size, TRUE);
-			}
+			if (cyl->type.size.mliter)
+				ret = get_volume_string(cyl->type.size, TRUE, cyl->type.workingpressure.mbar);
 			break;
 		case WORKINGPRESS:
 			if (cyl->type.workingpressure.mbar)