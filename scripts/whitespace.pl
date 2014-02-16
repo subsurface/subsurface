42f32671a877bd5d0c36dcea5cc58220f1d47a82
Dirk Hohndel <dirk@hohndel.org>
2014-02-16 15:38:01 -0800

    Continue to improve whitespace cleanup helper
    The more I try the cleanup helper, the more corner cases I find...
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/whitespace.pl
+++ scripts/whitespace.pl
@@ -3,7 +3,10 @@
 my $input = $ARGV[0];
 my $source = `clang-format $input`;
 $source =~ s/^(.*each.*\(.*\).*)\n\s*{\s*$/$1 {/img;
-$source =~ s/^(\s*struct.*)\n\s*{\s*$/$1 {/img;
+$source =~ s/^(\s*struct[^()\n]*)\n\s*{\s*$/$1 {/img;
+$source =~ s/^(\s*static\s+struct[^()\n]*)\n\s*{\s*$/$1 {/img;
+$source =~ s/^(\s*union[^()\n]*)\n\s*{\s*$/$1 {/img;
+$source =~ s/^(\s*static\s+union[^()\n]*)\n\s*{\s*$/$1 {/img;
 $source =~ s/^(\s*class.*)\n\s*{\s*$/$1 {/img;
 $source =~ s/^(\S*::\S*.*)\n\s*: /$1 : /img;
 $source =~ s/(?:\G|^)[ ]{6}/\t/mg;
