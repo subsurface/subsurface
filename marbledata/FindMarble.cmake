907d8b8fbbb0bc07ccc4c4b4d1374f764c5d8dae
Tomaz Canabrava <tomaz.canabrava@intel.com>
2014-04-14 14:21:01 -0300

    Add a CMake based buildsystem for the tests.
    This commit adds a CMake based buildsystem for the tests,
    it separated the subsurface sources in small libraries to
    make testing easyer ( subsurface core, subsurface ui,
    subsurface profile, etc. )
    
    There's no test yet, this however produces a compiled
    and executable binary *on linux*. This file shouldn't
    be needed on any other platform as this is not the main
    build system, but the test build system.
    
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ marbledata/FindMarble.cmake
@@ -0,0 +1,20 @@
+# - Try to find the Marble Library
+# Once done this will define
+#
+#  MARBLE_FOUND - system has Marble
+#  MARBLE_INCLUDE_DIR - the Marble include directory
+#  MARBLE_LIBRARIES
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+IF ( MARBLE_INCLUDE_DIR AND MARBLE_LIBRARIES )
+   # in cache already
+   SET( MARBLE_FIND_QUIETLY TRUE )
+ENDIF ( MARBLE_INCLUDE_DIR AND MARBLE_LIBRARIES )
+
+FIND_PATH( MARBLE_INCLUDE_DIR NAMES marble/MarbleModel.h )
+FIND_LIBRARY( MARBLE_LIBRARIES NAMES marblewidget )
+
+INCLUDE( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( marble DEFAULT_MSG MARBLE_INCLUDE_DIR MARBLE_LIBRARIES )
