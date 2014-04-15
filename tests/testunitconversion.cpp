2e80d79396936d379aa71d65db597b7edc84e4da
Lakshman <acrlakshman@gmail.com>
2014-04-15 11:39:51 -0500

    Corrections to unit tests
    Make the initial unit tests actually pass
    
    Signed-off-by: Lakshman Anumolu <acrlakshman@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- tests/testunitconversion.cpp
+++ tests/testunitconversion.cpp
@@ -3,11 +3,11 @@
 
 void TestUnitConversion::testUnitConversions()
 {
-	QCOMPARE(IS_FP_SAME(grams_to_lbs(1000), 2.20459), true);
+	QCOMPARE(IS_FP_SAME(grams_to_lbs(1000), 2.204586), true);
 	QCOMPARE(lbs_to_grams(1), 454);
 	QCOMPARE(IS_FP_SAME(ml_to_cuft(1000), 0.0353147), true);
-	QCOMPARE(IS_FP_SAME(cuft_to_l(1), 28.3168), true);
-	QCOMPARE(IS_FP_SAME(mm_to_feet(1000), 3.28084), true);
+	QCOMPARE(IS_FP_SAME(cuft_to_l(1), 28.316847), true);
+	QCOMPARE(IS_FP_SAME(mm_to_feet(1000), 3.280840), true);
 	QCOMPARE(feet_to_mm(1), (long unsigned int) 305);
 }
 
