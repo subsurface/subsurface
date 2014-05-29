9f12e7086d9d71bed7863ba0e3e63ed36831197a
Gehad elrobey <gehadelrobey@gmail.com>
2014-05-30 02:13:44 +0300

    HTML: Quote the '<' and '>' operators before inserting break tags
    The smaller than and greater than operators should be quoted before
    inserting the <br> tags in HTML. Otherwise breaks will be quoted which
    corrupts the format.
    
    Signed-off-by: Gehad elrobey <gehadelrobey@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- save-html.c
+++ save-html.c
@@ -49,10 +49,10 @@ char *replace_char(char *str, char replace, char *replace_by)
 
 char *quote(char *string)
 {
-	char *new_line_removed = replace_char(string, '\n', "<br>");
-	char *less_than_removed = replace_char(new_line_removed, '<', "&lt;");
+	char *less_than_removed = replace_char(string, '<', "&lt;");
 	char *greater_than_removed = replace_char(less_than_removed, '>', "&gt;");
-	char *double_quotes_removed = replace_char(greater_than_removed, '"', "&quot;");
+	char *new_line_removed = replace_char(greater_than_removed, '\n', "<br>");
+	char *double_quotes_removed = replace_char(new_line_removed, '"', "&quot;");
 	char *single_quotes_removed = replace_char(double_quotes_removed, '\'', "&#39;");
 	free(new_line_removed);
 	free(less_than_removed);
