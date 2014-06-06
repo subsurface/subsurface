e6fc5d2370db30092fec9f316a1d8edb4cb88d0b
Robert C. Helling <helling@atdotde.de>
2014-06-06 21:13:54 +0200

    Planner: incease step size in deco ascends to 3 seconds
    Triple time step in deco ascents to improve responsiveness of interactive
    deco calculation.
    
    [Dirk Hohndel: split commit into two]
    
    Signed-off-by: Robert C. Helling <helling@atdotde.de>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- planner.c
+++ planner.c
@@ -14,7 +14,7 @@
 #include "gettext.h"
 #include "libdivecomputer/parser.h"
 
-#define TIMESTEP 1 /* second */
+#define TIMESTEP 3 /* second */
 #define DECOTIMESTEP 60 /* seconds. Unit of deco stop times */
 
 int decostoplevels[] = { 0, 3000, 6000, 9000, 12000, 15000, 18000, 21000, 24000, 27000,
