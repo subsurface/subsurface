8e682c41e831c174e3a513ff6704d9ff3734f7a4
Tomaz Canabrava <tomaz.canabrava@intel.com>
2014-06-08 13:11:00 -0300

    Add a nice white rectangle around the picture
    This makes the picture looks a bit more like a real paper picture
    shadow's missing, though.
    
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-ui/profile/divepixmapitem.cpp
+++ qt-ui/profile/divepixmapitem.cpp
@@ -1,5 +1,9 @@
 #include "divepixmapitem.h"
 #include "animationfunctions.h"
+
+#include <QPen>
+#include <QBrush>
+
 DivePixmapItem::DivePixmapItem(QObject *parent) : QObject(parent), QGraphicsPixmapItem()
 {
 }
@@ -13,6 +17,11 @@ DivePictureItem::DivePictureItem(QObject *parent): DivePixmapItem(parent)
 void DivePictureItem::setPixmap(const QPixmap &pix)
 {
 	DivePixmapItem::setPixmap(pix);
+	QRectF r = boundingRect();
+	QGraphicsRectItem *rect = new QGraphicsRectItem(0 - 10, 0 -10, r.width() + 20, r.height() + 20, this);
+	rect->setPen(Qt::NoPen);
+	rect->setBrush(QColor(Qt::white));
+	rect->setFlag(ItemStacksBehindParent);
 	setTransformOriginPoint(boundingRect().width()/2, boundingRect().height()/2);
 }
 
