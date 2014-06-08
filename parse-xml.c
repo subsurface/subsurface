subsurface/subsurface

commit eaf6d564874ac838dc4f3cca7a781034ae60ef7f

CCR code: Change to sample structure
1) All the variables in the sample structures are strongly typed
2) Two additional types were declared in units.h:
     o2pressure_t
     bearing_t
3) The following variables were added:
     diluentpressure
     o2setpoint
     o2sensor[3]
4) Changes to a number of files were made to chanf
     sample->po2 to sample->po2.mbar
     bearing to bearring.degrees

Signed-off-by: Willem Ferguson <willemferguson@zoology.up.ac.za>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

parse-xml.c
@@ -1327,7 +1327,7 @@ static void sample_start(void)
 	cur_sample->stoptime.seconds = laststoptime;
 	cur_sample->stopdepth.mm = laststopdepth;
 	cur_sample->cns = lastcns;
-	cur_sample->po2 = lastpo2;
+	cur_sample->po2.mbar = lastpo2;
 	cur_sample->sensor = lastsensor;
 }
 
@@ -1342,7 +1342,7 @@ static void sample_end(void)
 	laststoptime = cur_sample->stoptime.seconds;
 	laststopdepth = cur_sample->stopdepth.mm;
 	lastcns = cur_sample->cns;
-	lastpo2 = cur_sample->po2;
+	lastpo2 = cur_sample->po2.mbar;
 	cur_sample = NULL;
 }
 
@@ -1929,7 +1929,7 @@ extern int shearwater_profile_sample(void *handle, int columns, char **data, cha
 	if (data[2])
 		cur_sample->temperature.mkelvin = metric ? C_to_mkelvin(atof(data[2])) : F_to_mkelvin(atof(data[2]));
 	if (data[3])
-		cur_sample->po2 = atof(data[3]) * 1000;
+		cur_sample->po2.mbar = atof(data[3]) * 1000;
 	if (data[4])
 		cur_sample->ndl.seconds = atoi(data[4]) * 60;
 	if (data[5])

dive.c
@@ -1105,7 +1105,7 @@ static void merge_samples(struct divecomputer *res, struct divecomputer *a, stru
 			sample.sensor = as->sensor;
 		if (as->cns)
 			sample.cns = as->cns;
-		if (as->po2)
+		if (as->po2.mbar)
 			sample.po2 = as->po2;
 		if (as->ndl.seconds)
 			sample.ndl = as->ndl;

dive.h
@@ -141,21 +141,25 @@ static inline depth_t gas_mod(struct gasmix *mix, pressure_t po2_limit) {
 void get_gas_string(const struct gasmix *gasmix, char *text, int len);
 const char *gasname(const struct gasmix *gasmix);
 
-struct sample {
-	duration_t time;
-	depth_t depth;
-	temperature_t temperature;
-	pressure_t cylinderpressure;
-	int sensor; /* Cylinder pressure sensor index */
-	duration_t ndl;
-	duration_t stoptime;
-	depth_t stopdepth;
-	bool in_deco;
-	int cns;
-	int po2;
-	int heartbeat;
-	int bearing;
-};
+struct sample                         // BASE TYPE BYTES  UNITS    RANGE      DESCRIPTION
+{                                     // --------- -----  -----    -----      -----------
+        duration_t time;               // uint32_t   4  seconds  (0-68 yrs)   elapsed dive time up to this sample
+        duration_t stoptime;           // uint32_t   4  seconds  (0-18 h)     time duration of next deco stop
+        duration_t ndl;                // uint32_t   4  seconds  (0-18 h)     time duration before no-deco limit
+        depth_t depth;                 // int32_t    4    mm     (0-2000 km)  dive depth of this sample
+        depth_t stopdepth;             // int32_t    4    mm     (0-2000 km)  depth of next deco stop
+        temperature_t temperature;     // int32_t    4  mdegrK   (0-2 MdegK)  ambient temperature
+        pressure_t cylinderpressure;   // int32_t    4    mbar   (0-2 Mbar)   main cylinder pressure
+        pressure_t diluentpressure;    // int32_t    4    mbar   (0-2 Mbar)   CCR diluent pressure (rebreather)
+        o2pressure_t po2;              // uint16_t    2    mbar  (0-65 bar)   O2 partial pressure
+        o2pressure_t o2setpoint;       // uint16_t    2    mbar  (0-65 bar)   CCR O2 setpoint (rebreather)
+        o2pressure_t o2sensor[3];      // uint16_t    6    mbar  (0-65 bar)   Up to 3 PO2 sensor values (rebreather)
+        bearing_t bearing;             // int16_t    2  degrees  (-32k to 32k deg) compass bearing
+        uint8_t sensor;                // uint8_t    1  sensorID (0-255)      ID of cylinder pressure sensor
+        uint8_t cns;                   // uint8_t    1     %     (0-255 %)    cns% accumulated
+        uint8_t heartbeat;             // uint8_t    1  beats/m  (0-255)      heart rate measurement
+        bool    in_deco;               // bool       1    y/n      y/n        this sample is part of deco
+};                      // Total size of structure: 48 bytes, excluding padding at end
 
 struct divetag {
 	/*

divelist.c
@@ -178,8 +178,8 @@ static int calculate_otu(struct dive *dive)
 		struct sample *sample = dc->sample + i;
 		struct sample *psample = sample - 1;
 		t = sample->time.seconds - psample->time.seconds;
-		if (sample->po2) {
-			po2 = sample->po2;
+		if (sample->po2.mbar) {
+			po2 = sample->po2.mbar;
 		} else {
 			int o2 = active_o2(dive, dc, sample->time);
 			po2 = o2 * depth_to_atm(sample->depth.mm, dive);
@@ -242,8 +242,8 @@ static int calculate_cns(struct dive *dive)
 		struct sample *sample = dc->sample + i;
 		struct sample *psample = sample - 1;
 		t = sample->time.seconds - psample->time.seconds;
-		if (sample->po2) {
-			po2 = sample->po2;
+		if (sample->po2.mbar) {
+			po2 = sample->po2.mbar;
 		} else {
 			int o2 = active_o2(dive, dc, sample->time);
 			po2 = o2 / depth_to_atm(sample->depth.mm, dive);
@@ -326,7 +326,7 @@ static void add_dive_to_deco(struct dive *dive)
 		for (j = t0; j < t1; j++) {
 			int depth = interpolate(psample->depth.mm, sample->depth.mm, j - t0, t1 - t0);
 			(void)add_segment(depth_to_mbar(depth, dive) / 1000.0,
-					  &dive->cylinder[sample->sensor].gasmix, 1, sample->po2, dive);
+					  &dive->cylinder[sample->sensor].gasmix, 1, sample->po2.mbar, dive);
 		}
 	}
 }

libdivecomputer.c
@@ -198,7 +198,7 @@ sample_cb(dc_sample_type_t type, dc_sample_value_t value, void *userdata)
 			sample->ndl.seconds = ndl;
 			sample->stoptime.seconds = stoptime;
 			sample->stopdepth.mm = stopdepth;
-			sample->po2 = po2;
+			sample->po2.mbar = po2;
 			sample->cns = cns;
 		}
 		sample = prepare_sample(dc);
@@ -225,7 +225,7 @@ sample_cb(dc_sample_type_t type, dc_sample_value_t value, void *userdata)
 		sample->heartbeat = value.heartbeat;
 		break;
 	case DC_SAMPLE_BEARING:
-		sample->bearing = value.bearing;
+		sample->bearing.degrees = value.bearing;
 		break;
 	case DC_SAMPLE_VENDOR:
 		printf("   <vendor time='%u:%02u' type=\"%u\" size=\"%u\">", FRACTION(sample->time.seconds, 60),
@@ -237,10 +237,10 @@ sample_cb(dc_sample_type_t type, dc_sample_value_t value, void *userdata)
 #if DC_VERSION_CHECK(0, 3, 0)
 	case DC_SAMPLE_SETPOINT:
 		/* for us a setpoint means constant pO2 from here */
-		sample->po2 = po2 = rint(value.setpoint * 1000);
+		sample->po2.mbar = po2 = rint(value.setpoint * 1000);
 		break;
 	case DC_SAMPLE_PPO2:
-		sample->po2 = po2 = rint(value.ppo2 * 1000);
+		sample->po2.mbar = po2 = rint(value.ppo2 * 1000);
 		break;
 	case DC_SAMPLE_CNS:
 		sample->cns = cns = rint(value.cns * 100);

load-git.c
@@ -375,8 +375,7 @@ static void parse_sample_keyvalue(void *_sample, const char *key, const char *va
 	}
 	if (!strcmp(key, "po2")) {
 		pressure_t p = get_pressure(value);
-		// Ugh, typeless.
-		sample->po2 = p.mbar;
+		sample->po2.mbar = p.mbar;
 		return;
 	}
 	if (!strcmp(key, "heartbeat")) {
@@ -384,7 +383,7 @@ static void parse_sample_keyvalue(void *_sample, const char *key, const char *va
 		return;
 	}
 	if (!strcmp(key, "bearing")) {
-		sample->bearing = atoi(value);
+		sample->bearing.degrees = atoi(value);
 		return;
 	}
 	report_error("Unexpected sample key/value pair (%s/%s)", key, value);

planner.c
@@ -159,7 +159,7 @@ double tissue_at_end(struct dive *dive, char **cached_datap)
 		}
 		if (i > 0)
 			lastdepth = psample->depth.mm;
-		tissue_tolerance = interpolate_transition(dive, t0, t1, lastdepth, sample->depth.mm, &dive->cylinder[gasidx].gasmix, sample->po2);
+		tissue_tolerance = interpolate_transition(dive, t0, t1, lastdepth, sample->depth.mm, &dive->cylinder[gasidx].gasmix, sample->po2.mbar);
 		psample = sample;
 		t0 = t1;
 	}
@@ -263,7 +263,7 @@ static struct dive *create_dive_from_plan(struct diveplan *diveplan, struct dive
 	cyl = &master_dive->cylinder[0];
 	oldgasmix = cyl->gasmix;
 	sample = prepare_sample(dc);
-	sample->po2 = dp->po2;
+	sample->po2.mbar = dp->po2;
 	finish_sample(dc);
 	while (dp) {
 		struct gasmix gasmix = dp->gasmix;
@@ -300,7 +300,7 @@ static struct dive *create_dive_from_plan(struct diveplan *diveplan, struct dive
 			/* need to insert a first sample for the new gas */
 			add_gas_switch_event(dive, dc, lasttime + 1, idx);
 			sample = prepare_sample(dc);
-			sample[-1].po2 = po2;
+			sample[-1].po2.mbar = po2;
 			sample->time.seconds = lasttime + 1;
 			sample->depth.mm = lastdepth;
 			finish_sample(dc);
@@ -311,8 +311,8 @@ static struct dive *create_dive_from_plan(struct diveplan *diveplan, struct dive
 		sample = prepare_sample(dc);
 		/* set po2 at beginning of this segment */
 		/* and keep it valid for last sample - where it likely doesn't matter */
-		sample[-1].po2 = po2;
-		sample->po2 = po2;
+		sample[-1].po2.mbar = po2;
+		sample->po2.mbar = po2;
 		sample->time.seconds = lasttime = time;
 		sample->depth.mm = lastdepth = depth;
 		update_cylinder_pressure(dive, sample[-1].depth.mm, depth, time - sample[-1].time.seconds,
@@ -703,7 +703,7 @@ void plan(struct diveplan *diveplan, char **cached_datap, struct dive **divep, s
 	/* we start with gas 0, then check if that was changed */
 	gas = dive->cylinder[0].gasmix;
 	get_gas_from_events(&dive->dc, sample->time.seconds, &gas);
-	po2 = dive->dc.sample[dive->dc.samples - 1].po2;
+	po2 = dive->dc.sample[dive->dc.samples - 1].po2.mbar;
 	if ((current_cylinder = get_gasidx(dive, &gas)) == -1) {
 		report_error(translate("gettextFromC", "Can't find gas %s"), gasname(&gas));
 		current_cylinder = 0;

profile.c
@@ -834,7 +834,7 @@ struct plot_data *populate_plot_entries(struct dive *dive, struct divecomputer *
 		pi->has_ndl |= sample->ndl.seconds;
 		entry->in_deco = sample->in_deco;
 		entry->cns = sample->cns;
-		entry->po2 = sample->po2 / 1000.0;
+		entry->po2 = sample->po2.mbar / 1000.0;
 		/* FIXME! sensor index -> cylinder index translation! */
 		entry->cylinderindex = sample->sensor;
 		SENSOR_PRESSURE(entry) = sample->cylinderpressure.mbar;
@@ -843,7 +843,7 @@ struct plot_data *populate_plot_entries(struct dive *dive, struct divecomputer *
 		else
 			entry->temperature = lasttemp;
 		entry->heartbeat = sample->heartbeat;
-		entry->bearing = sample->bearing;
+		entry->bearing = sample->bearing.degrees;
 
 		/* skip events that happened at this time */
 		while (ev && ev->time.seconds == time)

commit f46f9139a82fd93bcdb84735649b804c27b0b9c1

Make parse-xml callbacks be type-safe
.. and fix the type breakage brought in by commit eaf6d564874a ("CCR code:
Change to sample structure")

The XML parsing callbacks pass a "void *" around, because the helper
function that matches the XML node names ("match()") does so for all the
different dive/sample/dc member nodes that all have different types.

But that also hid the fact that it very much depended on the various types
being regular "int" etc, rather than the denser types that were introduced
so that the CCR data wouldn't expand memory use excessively. As a result,
XML loading would overwrite other members, and possibly even the
allocation, when it wrote an "int" value to something that only was a
8-bit allocation.

I left the "utf8_string()" without type checking - so it still uses
"void *_res" for the result type, with the cast happening inside the
function.

That's because the result destination ends up being a bit mixed-up wrt
"const char **" and just plain "char **". Note that the thing we modify
itself isn't const (it's not "char *const *"), but the pointer, but we
basically sometimes assign a "const char *", and sometimes a "char *".

I considered making two different versions of the callback, but it just
wasn't worth it. So "utf8_string()" users still aren't type-checked, and
you'd better give it a pointer to something that is some kind of "char *"

This patch doesn't really change the calling convention of the matching
function itself, but it makes the wrapper macro ("MATCH()") take a
properly type-checked function pointer instead (with a dummy call to do
type checking), and then casts the pointer to the "void *" type for the
actual real call.

The function pointer call is not really portable (although it works on
all sane architectures, particularly since the cast only changes one
argument from one type of pointer to another), and to make matters worse
uses the gcc statement-expression extension. But all the compilers we use

parse-xml.c
@@ -134,11 +134,10 @@ static enum import_source {
 	UDDF,
 } import_source;
 
-static void divedate(char *buffer, void *_when)
+static void divedate(char *buffer, timestamp_t *when)
 {
 	int d, m, y;
 	int hh, mm, ss;
-	timestamp_t *when = _when;
 
 	hh = 0;
 	mm = 0;
@@ -161,10 +160,9 @@ static void divedate(char *buffer, void *_when)
 	*when = utc_mktime(&cur_tm);
 }
 
-static void divetime(char *buffer, void *_when)
+static void divetime(char *buffer, timestamp_t *when)
 {
 	int h, m, s = 0;
-	timestamp_t *when = _when;
 
 	if (sscanf(buffer, "%d:%d:%d", &h, &m, &s) >= 2) {
 		cur_tm.tm_hour = h;
@@ -175,11 +173,10 @@ static void divetime(char *buffer, void *_when)
 }
 
 /* Libdivecomputer: "2011-03-20 10:22:38" */
-static void divedatetime(char *buffer, void *_when)
+static void divedatetime(char *buffer, timestamp_t *when)
 {
 	int y, m, d;
 	int hr, min, sec;
-	timestamp_t *when = _when;
 
 	if (sscanf(buffer, "%d-%d-%d %d:%d:%d",
 		   &y, &m, &d, &hr, &min, &sec) == 6) {
@@ -197,9 +194,8 @@ enum ParseState {
 	FINDSTART,
 	FINDEND
 };
-static void divetags(char *buffer, void *_tags)
+static void divetags(char *buffer, struct tag_entry **tags)
 {
-	struct tag_entry **tags = _tags;
 	int i = 0, start = 0, end = 0;
 	enum ParseState state = FINDEND;
 	int len = buffer ? strlen(buffer) : 0;
@@ -283,10 +279,9 @@ static enum number_type integer_or_float(char *buffer, union int_or_float *res)
 	return parse_float(buffer, &res->fp, &end);
 }
 
-static void pressure(char *buffer, void *_press)
+static void pressure(char *buffer, pressure_t *pressure)
 {
 	double mbar = 0.0;
-	pressure_t *pressure = _press;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -318,9 +313,8 @@ static void pressure(char *buffer, void *_press)
 	}
 }
 
-static void salinity(char *buffer, void *_salinity)
+static void salinity(char *buffer, int *salinity)
 {
-	int *salinity = _salinity;
 	union int_or_float val;
 	switch (integer_or_float(buffer, &val)) {
 	case FLOAT:
@@ -331,9 +325,8 @@ static void salinity(char *buffer, void *_salinity)
 	}
 }
 
-static void depth(char *buffer, void *_depth)
+static void depth(char *buffer, depth_t *depth)
 {
-	depth_t *depth = _depth;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -352,9 +345,8 @@ static void depth(char *buffer, void *_depth)
 	}
 }
 
-static void weight(char *buffer, void *_weight)
+static void weight(char *buffer, weight_t *weight)
 {
-	weight_t *weight = _weight;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -373,9 +365,8 @@ static void weight(char *buffer, void *_weight)
 	}
 }
 
-static void temperature(char *buffer, void *_temperature)
+static void temperature(char *buffer, temperature_t *temperature)
 {
-	temperature_t *temperature = _temperature;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -401,11 +392,10 @@ static void temperature(char *buffer, void *_temperature)
 		temperature->mkelvin = 0;
 }
 
-static void sampletime(char *buffer, void *_time)
+static void sampletime(char *buffer, duration_t *time)
 {
 	int i;
 	int min, sec;
-	duration_t *time = _time;
 
 	i = sscanf(buffer, "%d:%d", &min, &sec);
 	switch (i) {
@@ -421,7 +411,7 @@ static void sampletime(char *buffer, void *_time)
 	}
 }
 
-static void duration(char *buffer, void *_time)
+static void duration(char *buffer, duration_t *time)
 {
 	/* DivingLog 5.08 (and maybe other versions) appear to sometimes
 	 * store the dive time as 44.00 instead of 44:00;
@@ -430,16 +420,15 @@ static void duration(char *buffer, void *_time)
 		char *mybuffer = strdup(buffer);
 		char *dot = strchr(mybuffer, '.');
 		*dot = ':';
-		sampletime(mybuffer, _time);
+		sampletime(mybuffer, time);
 		free(mybuffer);
 	} else {
-		sampletime(buffer, _time);
+		sampletime(buffer, time);
 	}
 }
 
-static void percent(char *buffer, void *_fraction)
+static void percent(char *buffer, fraction_t *fraction)
 {
-	fraction_t *fraction = _fraction;
 	double val;
 	const char *end;
 
@@ -464,23 +453,22 @@ static void percent(char *buffer, void *_fraction)
 	}
 }
 
-static void gasmix(char *buffer, void *_fraction)
+static void gasmix(char *buffer, fraction_t *fraction)
 {
 	/* libdivecomputer does negative percentages. */
 	if (*buffer == '-')
 		return;
 	if (cur_cylinder_index < MAX_CYLINDERS)
-		percent(buffer, _fraction);
+		percent(buffer, fraction);
 }
 
-static void gasmix_nitrogen(char *buffer, void *_gasmix)
+static void gasmix_nitrogen(char *buffer, struct gasmix *gasmix)
 {
 	/* Ignore n2 percentages. There's no value in them. */
 }
 
-static void cylindersize(char *buffer, void *_volume)
+static void cylindersize(char *buffer, volume_t *volume)
 {
-	volume_t *volume = _volume;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -511,39 +499,46 @@ static void utf8_string(char *buffer, void *_res)
 	*(char **)_res = res;
 }
 
-#define MATCH(pattern, fn, dest) \
-	match(pattern, strlen(pattern), name, fn, buf, dest)
+#define MATCH(pattern, fn, dest) ({ 			\
+	/* Silly type compatibility test */ 		\
+	if (0) (fn)("test", dest);			\
+	match(pattern, strlen(pattern), name, (matchfn_t) (fn), buf, dest); })
 
-static void get_index(char *buffer, void *_i)
+static void get_index(char *buffer, int *i)
 {
-	int *i = _i;
 	*i = atoi(buffer);
 }
 
-static void get_rating(char *buffer, void *_i)
+static void get_uint8(char *buffer, uint8_t *i)
 {
-	int *i = _i;
+	*i = atoi(buffer);
+}
+
+static void get_bearing(char *buffer, bearing_t *bearing)
+{
+	bearing->degrees = atoi(buffer);
+}
+
+static void get_rating(char *buffer, int *i)
+{
 	int j = atoi(buffer);
 	if (j >= 0 && j <= 5) {
 		*i = j;
 	}
 }
 
-static void double_to_permil(char *buffer, void *_i)
+static void double_to_o2pressure(char *buffer, o2pressure_t *i)
 {
-	int *i = _i;
-	*i = rint(ascii_strtod(buffer, NULL) * 1000.0);
+	i->mbar = rint(ascii_strtod(buffer, NULL) * 1000.0);
 }
 
-static void hex_value(char *buffer, void *_i)
+static void hex_value(char *buffer, uint32_t *i)
 {
-	uint32_t *i = _i;
 	*i = strtoul(buffer, NULL, 16);
 }
 
-static void get_tripflag(char *buffer, void *_tf)
+static void get_tripflag(char *buffer, tripflag_t *tf)
 {
-	tripflag_t *tf = _tf;
 	*tf = strcmp(buffer, "NOTRIP") ? TF_NONE : NO_TRIP;
 }
 
@@ -570,9 +565,8 @@ static void get_tripflag(char *buffer, void *_tf)
  * - temperature == 32.0  -> garbage, it's a missing temperature (zero converted from C to F)
  * - temperatures > 32.0 == Fahrenheit
  */
-static void fahrenheit(char *buffer, void *_temperature)
+static void fahrenheit(char *buffer, temperature_t *temperature)
 {
-	temperature_t *temperature = _temperature;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -609,9 +603,8 @@ static void fahrenheit(char *buffer, void *_temperature)
  * have to have some arbitrary cut-off point where we assume
  * that smaller values mean bar.. Not good.
  */
-static void psi_or_bar(char *buffer, void *_pressure)
+static void psi_or_bar(char *buffer, pressure_t *pressure)
 {
-	pressure_t *pressure = _pressure;
 	union int_or_float val;
 
 	switch (integer_or_float(buffer, &val)) {
@@ -635,9 +628,8 @@ static int divinglog_fill_sample(struct sample *sample, const char *name, char *
 	       0;
 }
 
-static void uddf_gasswitch(char *buffer, void *_sample)
+static void uddf_gasswitch(char *buffer, struct sample *sample)
 {
-	struct sample *sample = _sample;
 	int idx = atoi(buffer);
 	int seconds = sample->time.seconds;
 	struct dive *dive = cur_dive;
@@ -656,9 +648,8 @@ static int uddf_fill_sample(struct sample *sample, const char *name, char *buf)
 	       0;
 }
 
-static void eventtime(char *buffer, void *_duration)
+static void eventtime(char *buffer, duration_t *duration)
 {
-	duration_t *duration = _duration;
 	sampletime(buffer, duration);
 	if (cur_sample)
 		duration->seconds += cur_sample->time.seconds;
@@ -781,9 +772,8 @@ void add_gas_switch_event(struct dive *dive, struct divecomputer *dc, int second
 	add_event(dc, seconds, 25, 0, value, "gaschange"); /* SAMPLE_EVENT_GASCHANGE2 */
 }
 
-static void get_cylinderindex(char *buffer, void *_i)
+static void get_cylinderindex(char *buffer, uint8_t *i)
 {
-	int *i = _i;
 	*i = atoi(buffer);
 	if (lastcylinderindex != *i) {
 		add_gas_switch_event(cur_dive, get_dc(), cur_sample->time.seconds, *i);
@@ -791,9 +781,8 @@ static void get_cylinderindex(char *buffer, void *_i)
 	}
 }
 
-static void get_sensor(char *buffer, void *_i)
+static void get_sensor(char *buffer, uint8_t *i)
 {
-	int *i = _i;
 	*i = atoi(buffer);
 	lastsensor = *i;
 }
@@ -832,13 +821,13 @@ static void try_to_fill_sample(struct sample *sample, const char *name, char *bu
 		return;
 	if (MATCH("stopdepth.sample", depth, &sample->stopdepth))
 		return;
-	if (MATCH("cns.sample", get_index, &sample->cns))
+	if (MATCH("cns.sample", get_uint8, &sample->cns))
 		return;
-	if (MATCH("po2.sample", double_to_permil, &sample->po2))
+	if (MATCH("po2.sample", double_to_o2pressure, &sample->po2))
 		return;
-	if (MATCH("heartbeat", get_index, &sample->heartbeat))
+	if (MATCH("heartbeat", get_uint8, &sample->heartbeat))
 		return;
-	if (MATCH("bearing", get_index, &sample->bearing))
+	if (MATCH("bearing", get_bearing, &sample->bearing))
 		return;
 
 	switch (import_source) {
@@ -867,9 +856,8 @@ void try_to_fill_userid(const char *name, char *buf)
 
 static const char *country, *city;
 
-static void divinglog_place(char *place, void *_location)
+static void divinglog_place(char *place, char **location)
 {
-	char **location = _location;
 	char buffer[1024], *p;
 	int len;
 
@@ -914,11 +902,10 @@ static int divinglog_dive_match(struct dive *dive, const char *name, char *buf)
  *
  * There are many variations on that. This handles the useful cases.
  */
-static void uddf_datetime(char *buffer, void *_when)
+static void uddf_datetime(char *buffer, timestamp_t *when)
 {
 	char c;
 	int y, m, d, hh, mm, ss;
-	timestamp_t *when = _when;
 	struct tm tm = { 0 };
 	int i;
 
@@ -951,12 +938,11 @@ success:
 	*when = utc_mktime(&tm);
 }
 
-#define uddf_datedata(name, offset)                        \
-	static void uddf_##name(char *buffer, void *_when) \
-	{                                                  \
-		timestamp_t *when = _when;                 \
-		cur_tm.tm_##name = atoi(buffer) + offset;  \
-		*when = utc_mktime(&cur_tm);               \
+#define uddf_datedata(name, offset)                              \
+	static void uddf_##name(char *buffer, timestamp_t *when) \
+	{                                                        \
+		cur_tm.tm_##name = atoi(buffer) + offset;        \
+		*when = utc_mktime(&cur_tm);                     \
 	}
 
 uddf_datedata(year, 0)
@@ -1026,26 +1012,23 @@ degrees_t parse_degrees(char *buf, char **end)
 	return ret;
 }
 
-static void gps_lat(char *buffer, void *_dive)
+static void gps_lat(char *buffer, struct dive *dive)
 {
 	char *end;
-	struct dive *dive = _dive;
 
 	dive->latitude = parse_degrees(buffer, &end);
 }
 
-static void gps_long(char *buffer, void *_dive)
+static void gps_long(char *buffer, struct dive *dive)
 {
 	char *end;
-	struct dive *dive = _dive;
 
 	dive->longitude = parse_degrees(buffer, &end);
 }
 
-static void gps_location(char *buffer, void *_dive)
+static void gps_location(char *buffer, struct dive *dive)
 {
 	char *end;
-	struct dive *dive = _dive;
 
 	dive->latitude = parse_degrees(buffer, &end);
 	dive->longitude = parse_degrees(end, &end);