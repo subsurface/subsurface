subsurface/subsurface

commit f29f41ae9ea8b9c6217feab2cd97b165af843c2a

Planner: fix start time handling
We setup the startTime for the dive plan in too many places... but never
actually copied it into the planned dive.

Fixes #640

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

qt-ui/diveplanner.cpp
@@ -69,9 +69,6 @@ void DivePlannerPointsModel::createSimpleDive()
 {
 	struct gasmix gas = { 0 };
 
-	// set the start time of the plan
-	diveplan.when = displayed_dive.when;
-
 	if (isPlanner())
 		// let's use the gas from the first cylinder
 		gas = displayed_dive.cylinder[0].gasmix;
@@ -666,7 +663,6 @@ int DivePlannerPointsModel::rowCount(const QModelIndex &parent) const
 DivePlannerPointsModel::DivePlannerPointsModel(QObject *parent) : QAbstractTableModel(parent), mode(NOTHING)
 {
 	memset(&diveplan, 0, sizeof(diveplan));
-	startTime = QDateTime::currentDateTimeUtc();
 }
 
 DivePlannerPointsModel *DivePlannerPointsModel::instance()

planner.c
@@ -262,6 +262,7 @@ static void create_dive_from_plan(struct diveplan *diveplan, bool track_gas)
 	// displayed dive so we can restart
 	reset_cylinders(&displayed_dive, track_gas);
 	dc = &displayed_dive.dc;
+	dc->when = displayed_dive.when = diveplan->when;
 	free(dc->sample);
 	dc->sample = NULL;
 	dc->samples = 0;

commit 1863452a25b452432a8cebf8474185fd1ee28508

Add dive: initialize start time of dive
commit f29f41ae9ea8 ("Planner: fix start time handling") broke the start
time handling for Add dive. While in actual planner mode we set the start
time for the plan, we did not do this when simply adding a dive. The
moment the time / date was changed (which admittedly in real life a user
would most likely do) all was well, but if the user just accepted the
"now + 1h" default, things went badly wrong.

Fixes #658
See #655

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

qt-ui/diveplanner.cpp
@@ -69,6 +69,9 @@ void DivePlannerPointsModel::createSimpleDive()
 {
 	struct gasmix gas = { 0 };
 
+	// initialize the start time in the plan
+	diveplan.when = displayed_dive.when;
+
 	if (isPlanner())
 		// let's use the gas from the first cylinder
 		gas = displayed_dive.cylinder[0].gasmix;