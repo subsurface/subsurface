589f277477a95be53a9a24a29ae15c1bca05ff80
Dirk Hohndel <dirk@hohndel.org>
2014-12-02 10:55:52 -0800

    Adjust version numbering for Ubuntu package
    Let's use all four digits as 'version'.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/ubuntu/make-package.sh
+++ packaging/ubuntu/make-package.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # start from the directory above the combined subsurface & subsurface/libdivecomputer directory
-VERSION=$(cd subsurface ; git describe | sed -e 's/-g.*$// ; s/^v//')
+VERSION=$(cd subsurface ; git describe | sed -e 's/-g.*$// ; s/^v// ; s/-/./')
 echo "building Subsurface" $VERSION
-if [[ -d subsurface-$VERSION ]]; then
-	rm -rf subsurface-$VERSION.bak.prev
-	mv subsurface-$VERSION.bak subsurface-$VERSION.bak.prev
-	mv subsurface-$VERSION subsurface-$VERSION.bak
+if [[ -d subsurface_$VERSION ]]; then
+	rm -rf subsurface_$VERSION.bak.prev
+	mv subsurface_$VERSION.bak subsurface_$VERSION.bak.prev
+	mv subsurface_$VERSION subsurface_$VERSION.bak
 fi
-mkdir subsurface-$VERSION
-(cd subsurface ; tar cf - . .git ) | (cd subsurface-$VERSION ; tar xf - )
-cd subsurface-$VERSION
-dh_make --email dirk@hohndel.org -c gpl2 --createorig --single --yes
+mkdir subsurface_$VERSION
+(cd subsurface ; tar cf - . .git ) | (cd subsurface_$VERSION ; tar xf - )
+cd subsurface_$VERSION
+dh_make --email dirk@hohndel.org -c gpl2 --createorig --single --yes -p subsurface_$VERSION
 rm debian/*.ex debian/*.EX debian/README.*
 cp ../subsurface/packaging/ubuntu/control debian/control
 cp ../subsurface/packaging/ubuntu/copyright debian/copyright
@@ -24,13 +24,4 @@ tail -1 debian/autocl >> debian/changelog
 rm -f debian/autocl
 
 debuild -S 
-
-
-
-
-exit
-cd subsurface-$VERSION/libdivecomputer
-autoreconf --install
-./configure
-make
 
