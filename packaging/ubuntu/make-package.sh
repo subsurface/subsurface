8f224c17373c8714ffeb13127a4e266a47990a16
Dirk Hohndel <dirk@hohndel.org>
2014-12-03 09:38:52 -0800

    Ubuntu packaging: move support files into debian directory
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/ubuntu/make-package.sh
+++ packaging/ubuntu/make-package.sh
@@ -22,14 +22,14 @@ echo $GITVERSION > .gitversion
 
 dh_make --email dirk@hohndel.org -c gpl2 --createorig --single --yes -p subsurface_$VERSION
 rm debian/*.ex debian/*.EX debian/README.*
-cp ../subsurface/packaging/ubuntu/control debian/control
-cp ../subsurface/packaging/ubuntu/copyright debian/copyright
-cp ../subsurface/packaging/ubuntu/rules debian/rules
-cp ../subsurface/packaging/ubuntu/source.lintian-overrides debian/source.lintian-overrides
+cp ../subsurface/packaging/ubuntu/debian/control debian/control
+cp ../subsurface/packaging/ubuntu/debian/copyright debian/copyright
+cp ../subsurface/packaging/ubuntu/debian/rules debian/rules
+cp ../subsurface/packaging/ubuntu/debian/source.lintian-overrides debian/source.lintian-overrides
 # do something clever with changelog
 mv debian/changelog debian/autocl
 head -1 debian/autocl | sed -e 's/unstable/trusty/' > debian/changelog
-cat ../subsurface/packaging/ubuntu/changelog.txt >> debian/changelog
+cat ../subsurface/packaging/ubuntu/debian/changelog >> debian/changelog
 tail -1 debian/autocl >> debian/changelog
 rm -f debian/autocl
 
