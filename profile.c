e5c3799270fcc9c6726ce743f7662f5f42698f09
Miika Turkia <miika.turkia@gmail.com>
2015-01-01 14:27:57 +0200

    Fix mean depth on info box
    The running depth must be divided by current time to get the average
    depth.
    
    Signed-off-by: Miika Turkia <miika.turkia@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- profile.c
+++ profile.c
@@ -1200,7 +1200,7 @@ static void plot_string(struct plot_info *pi, struct plot_data *entry, struct me
 	if (entry->bearing)
 		put_format(b, translate("gettextFromC", "bearing: %d\n"), entry->bearing);
 	if (entry->running_sum) {
-		depthvalue = get_depth_units(entry->running_sum / 1000, NULL, &depth_unit);
+		depthvalue = get_depth_units(entry->running_sum / entry->sec, NULL, &depth_unit);
 		put_format(b, translate("gettextFromC", "mean depth to here %.1f%s\n"), depthvalue, depth_unit);
 	}
 
