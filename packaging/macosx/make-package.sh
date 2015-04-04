1ed1e188fd167e3ea3074bd2e6ec5cdd9ab984d3
Dirk Hohndel <dirk@hohndel.org>
2015-04-04 16:31:16 -0700

    Update Mac convenience shell script
    This script likely is useless for anyone but me, but I like having it in the
    source tree in case others can benefit from seeing how the packages are built.
    
    This now is based on out-of-tree cmake builds.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/macosx/make-package.sh
+++ packaging/macosx/make-package.sh
@@ -16,29 +16,30 @@ DMGCREATE=../yoursway-create-dmg/create-dmg
 # for the naming of volume and dmg we don't need the "always 3 digits"
 # darwin version - the 'regular' version that has 2 digits for releases
 # is better
-VERSION=$(./scripts/get-version linux)
+VERSION=$(cd ../subsurface; ./scripts/get-version linux)
 
 
 # first build and install Subsurface and then clean up the staging area
 rm -rf ./Subsurface.app
 make -j8
-make mac-deploy
+make install
 install_name_tool -change /Users/hohndel/src/marble/install/libssrfmarblewidget.0.19.2.dylib @executable_path/../Frameworks/libssrfmarblewidget.0.19.2.dylib Subsurface.app/Contents/MacOS/Subsurface
+install_name_tool -change /Users/hohndel/src/libgit2/build/libgit2.22.dylib @executable_path/../Frameworks/libgit2.22.dylib Subsurface.app/Contents/MacOS/Subsurface
 
 # copy things into staging so we can create a nice DMG
 rm -rf ./staging
 mkdir ./staging
 cp -a ./Subsurface.app ./staging
 
-sh ./packaging/macosx/sign
+sh ../subsurface/packaging/macosx/sign
 
 if [ -f ./Subsurface-$VERSION.dmg ]; then
 	rm ./Subsurface-$VERSION.dmg.bak
 	mv ./Subsurface-$VERSION.dmg ./Subsurface-$VERSION.dmg.bak
 fi
 
-$DMGCREATE --background ./packaging/macosx/DMG-Background.png \
+$DMGCREATE --background ../subsurface/packaging/macosx/DMG-Background.png \
 	--window-size 500 300 --icon-size 96 --volname Subsurface-$VERSION \
 	--app-drop-link 380 205 \
-	--volicon ~/src/subsurface/packaging/macosx/Subsurface.icns \
+	--volicon ../subsurface/packaging/macosx/Subsurface.icns \
 	--icon "Subsurface" 110 205 ./Subsurface-$VERSION.dmg ./staging
