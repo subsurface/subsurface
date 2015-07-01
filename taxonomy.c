36657c019be70be11430ce8e785dea3fb2e6a415
Dirk Hohndel <dirk@hohndel.org>
2015-07-01 12:25:47 -0700

    Geo taxonomy: create some data structures and helper functions
    This is designed to store taxonomy information for dive sites, including
    information where the data came from.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ taxonomy.c
@@ -0,0 +1,36 @@
+#include "taxonomy.h"
+#include "gettext.h"
+#include <stdlib.h>
+
+char *taxonomy_category_names[NR_CATEGORIES] = {
+	QT_TRANSLATE_NOOP("getTextFromC", "None"),
+	QT_TRANSLATE_NOOP("getTextFromC", "Ocean"),
+	QT_TRANSLATE_NOOP("getTextFromC", "Country"),
+	QT_TRANSLATE_NOOP("getTextFromC", "State"),
+	QT_TRANSLATE_NOOP("getTextFromC", "County"),
+	QT_TRANSLATE_NOOP("getTextFromC", "City")
+};
+
+// these are the names for geoname.org
+char *taxonomy_api_names[NR_CATEGORIES] = {
+	"none",
+	"name",
+	"countryName",
+	"adminName1",
+	"adminName2",
+	"toponymName"
+};
+
+struct taxonomy *alloc_taxonomy()
+{
+	return calloc(NR_CATEGORIES, sizeof(struct taxonomy));
+}
+
+void free_taxonomy(struct taxonomy *t)
+{
+	if (t) {
+		for (int i = 0; i < NR_CATEGORIES; i++)
+			free((void *)t[i].value);
+		free(t);
+	}
+}
