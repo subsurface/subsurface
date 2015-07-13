ca5606d3e9783edbd3012684eff1a829088870b5
Tomaz Canabrava <tomaz.canabrava@intel.com>
2015-07-13 15:13:48 -0300

    Create/retrieve dive_site by name.
    When we are working on the location management, we want to get a new
    dive_site if the dive_site name changed unless there's no dive_site by
    that name, then we create it.
    
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- divesite.h
+++ divesite.h
@@ -59,7 +59,7 @@ uint32_t get_dive_site_uuid_by_gps_proximity(degrees_t latitude, degrees_t longi
 bool dive_site_is_empty(struct dive_site *ds);
 void copy_dive_site(struct dive_site *orig, struct dive_site *copy);
 void clear_dive_site(struct dive_site *ds);
-
+uint32_t find_or_create_dive_site_with_name(const char *name);
 #ifdef __cplusplus
 }
 #endif
