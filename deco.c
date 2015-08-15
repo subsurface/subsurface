ffe2884f72ace4d04158b72fc806823c87a62e4b
Jan Darowski <jan.darowski@gmail.com>
2015-08-15 15:55:46 +0200

    VPM-B: Set radius constants to values reccomended by V-Planner. V-Planner reccomends smaller values for the VPM with the Boyles compensation. Also missing units comments were added.
    Signed-off-by: Jan Darowski <jan.darowski@gmail.com>

--- deco.c
+++ deco.c
@@ -36,14 +36,14 @@ struct buehlmann_config buehlmann_config = { 1.0, 1.01, 0, 0.75, 0.35, 1.0, fals
 struct vpmb_config {
 	double crit_radius_N2;            //! Critical radius of N2 nucleon (microns).
 	double crit_radius_He;            //! Critical radius of He nucleon (microns).
-	double crit_volume_lambda;        //! Constant corresponding to critical gas volume (bar-min).
+	double crit_volume_lambda;        //! Constant corresponding to critical gas volume (bar * min).
 	double gradient_of_imperm;        //! Gradient after which bubbles become impermeable (bar).
-	double surface_tension_gamma;     //! Nucleons surface tension constant.
-	double skin_compression_gammaC;   //! Skin compression gammaC.
+	double surface_tension_gamma;     //! Nucleons surface tension constant (N / 10m).
+	double skin_compression_gammaC;   //! Skin compression gammaC (N / 10m).
 	double regeneration_time;         //! Time needed for the bubble to regenerate to the start radius (min).
 	double other_gases_pressure;      //! Always present pressure of other gasses in tissues (bar).
 };
-struct vpmb_config vpmb_config = { 0.8, 0.7, 230.284, 8.2, 0.179, 2.57, 20160, 0.1359888 };
+struct vpmb_config vpmb_config = { 0.55, 0.45, 230.284, 8.2, 0.179, 2.57, 20160, 0.1359888 };
 
 const double buehlmann_N2_a[] = { 1.1696, 1.0, 0.8618, 0.7562,
 				  0.62, 0.5043, 0.441, 0.4,
