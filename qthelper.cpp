subsurface/subsurface

commit 45b1d0d73db6e2116c6de7113feed381dcc5875f

Display day number in trips longer than 1 day
Signed-off-by: Giorgio Marzano <marzano.giorgio@gmail.com>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

qthelper.cpp
@@ -1043,20 +1043,41 @@ const char *get_dive_date_c_string(timestamp_t when)
 	return strdup(text.toUtf8().data());
 }
 
-QString get_trip_date_string(timestamp_t when, int nr)
+bool is_same_day(timestamp_t trip_when, timestamp_t dive_when)
+{
+	static timestamp_t twhen = (timestamp_t) 0;
+	static struct tm tmt;
+	struct tm tmd;
+
+	utc_mkdate(dive_when, &tmd);
+
+	if (twhen != trip_when) {
+		twhen = trip_when;
+		utc_mkdate(twhen, &tmt);
+	}
+
+	return ((tmd.tm_mday == tmt.tm_mday) && (tmd.tm_mon == tmt.tm_mon) && (tmd.tm_year == tmt.tm_year));
+}
+
+QString get_trip_date_string(timestamp_t when, int nr, bool getday)
 {
 	struct tm tm;
 	utc_mkdate(when, &tm);
+	QDateTime localTime = QDateTime::fromTime_t(when);
+	localTime.setTimeSpec(Qt::UTC);
+	QString ret ;
+
 	if (nr != 1) {
-		QString ret =  translate("gettextFromC", "%1 %2 (%3 dives)");
-		return ret.arg(monthname(tm.tm_mon))
-			.arg(tm.tm_year + 1900)
-			.arg(nr);
+		if (getday) {
+			ret = localTime.date().toString(dateFormat).append(" (%1 dives)").arg(nr);
+		} else {
+			ret = localTime.date().toString("MMM yy").append(" (%1 dives)").arg(nr);
+		}
 	} else {
-		QString ret = translate("gettextFromC", "%1 %2 (1 dive)");
-		return ret.arg(monthname(tm.tm_mon))
-			.arg(tm.tm_year + 1900);
+		ret = localTime.date().toString(dateFormat).append(" (1 dive)");
 	}
+	return ret;
+
 }
 
 extern "C" void reverseGeoLookup(degrees_t latitude, degrees_t longitude, uint32_t uuid)

helpers.h
@@ -36,7 +36,8 @@ int parseTemperatureToMkelvin(const QString &text);
 QString get_dive_duration_string(timestamp_t when, QString hourText, QString minutesText);
 QString get_dive_date_string(timestamp_t when);
 QString get_short_dive_date_string(timestamp_t when);
-QString get_trip_date_string(timestamp_t when, int nr);
+bool is_same_day (timestamp_t trip_when, timestamp_t dive_when);
+QString get_trip_date_string(timestamp_t when, int nr, bool getday);
 QString uiLanguage(QLocale *callerLoc);
 QLocale getLocale();
 QString getDateFormat();

qt-models/divetripmodel.cpp
@@ -43,6 +43,7 @@ static QVariant dive_table_alignment(int column)
 QVariant TripItem::data(int column, int role) const
 {
 	QVariant ret;
+	bool oneDayTrip=true;
 
 	if (role == DiveTripModel::TRIP_ROLE)
 		return QVariant::fromValue<void *>(trip);
@@ -59,14 +60,15 @@ QVariant TripItem::data(int column, int role) const
 			while (d) {
 				if (!d->hidden_by_filter)
 					countShown++;
+				oneDayTrip &= is_same_day (trip->when,  d->when);
 				d = d->next;
 			}
 			if (countShown < trip->nrdives)
 				shownText = tr("(%1 shown)").arg(countShown);
 			if (trip->location && *trip->location)
-				ret = QString(trip->location) + ", " + get_trip_date_string(trip->when, trip->nrdives) + " " + shownText;
+				ret = QString(trip->location) + ", " + get_trip_date_string(trip->when, trip->nrdives, oneDayTrip) + " "+ shownText;
 			else
-				ret = get_trip_date_string(trip->when, trip->nrdives) + shownText;
+				ret = get_trip_date_string(trip->when, trip->nrdives, oneDayTrip) + shownText;
 			break;
 		}
 	}

commit f682ea00855cc2b52cefeeaf463f5d4ffbd4b33e

Fix broken translation in divelist
Fixes a broken translation in the divelist introduced in commit 45b1d0d73db6e21.
Now, the (.. dives) string on a trip line in de divelist is translated (again).

Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

qthelper.cpp
@@ -1069,12 +1069,12 @@ QString get_trip_date_string(timestamp_t when, int nr, bool getday)
 
 	if (nr != 1) {
 		if (getday) {
-			ret = localTime.date().toString(dateFormat).append(" (%1 dives)").arg(nr);
+			ret = localTime.date().toString(dateFormat) + " " + QObject::tr("(%1 dives)").arg(nr);
 		} else {
-			ret = localTime.date().toString("MMM yy").append(" (%1 dives)").arg(nr);
+			ret = localTime.date().toString("MMM yy") + " " + QObject::tr("(%1 dives)").arg(nr);
 		}
 	} else {
-		ret = localTime.date().toString(dateFormat).append(" (1 dive)");
+		ret = localTime.date().toString(dateFormat) + " " + QObject::tr("(1 dive)");
 	}
 	return ret;
 