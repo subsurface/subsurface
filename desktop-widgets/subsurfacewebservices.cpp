c2ff2be210cba14386d8b468dd3e6fde5f4abc40
Lubomir I. Ivanov <neolit123@gmail.com>
2015-11-07 22:40:55 +0200

    subsurfacewebservices.cpp: make sure 'changed' is not unused
    Make sure 'changed' in SubsurfaceWebServices::buttonClicked()
    is not unused even if NO_MARBLE is defined.
    
    It appears to be used only in the #ifndef NO_MARBLE branch.
    
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- desktop-widgets/subsurfacewebservices.cpp
+++ desktop-widgets/subsurfacewebservices.cpp
@@ -411,6 +411,7 @@ void SubsurfaceWebServices::buttonClicked(QAbstractButton *button)
 		struct dive *d;
 		struct dive_site *ds;
 		bool changed = false;
+		(void)changed;
 		clear_table(&gps_location_table);
 		QByteArray url = tr("Webservice").toLocal8Bit();
 		parse_xml_buffer(url.data(), downloadedData.data(), downloadedData.length(), &gps_location_table, NULL);
