136b87e7c00b79ba8ee316ecf66dbc326e78c823
Dirk Hohndel <dirk@hohndel.org>
2015-11-11 10:50:55 -0800

    Location service: Add GPS location infrastructure for Subsurface-mobile
    This doesn't do a thing - just adds the empty class and sets up the Cmake
    file so it finds the required Qt components for Subsurface-mobile.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ gpslocation.cpp
@@ -0,0 +1,7 @@
+#include "gpslocation.h"
+
+GpsLocation::GpsLocation()
+{
+
+}
+
---
+++ gpslocation.h
@@ -0,0 +1,16 @@
+#ifndef GPSLOCATION_H
+#define GPSLOCATION_H
+
+#include <QGeoCoordinate>
+
+class GpsLocation
+{
+public:
+	GpsLocation();
+
+signals:
+
+public slots:
+};
+
+#endif // GPSLOCATION_H
