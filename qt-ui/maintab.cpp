b73f56bc43dac5d43d3d8fe16c9d40ed5987ab1d
Miika Turkia <miika.turkia@gmail.com>
2016-01-17 12:53:51 +0200

    Hide the time from trip tab
    This hides the start time when viewing trip details.
    
    See #964
    
    Signed-off-by: Miika Turkia <miika.turkia@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-ui/maintab.cpp
+++ qt-ui/maintab.cpp
@@ -524,6 +524,8 @@ void MainTab::updateDiveInfo(bool clear)
 			ui.waterTempLabel->setVisible(false);
 			ui.watertemp->setVisible(false);
 			ui.dateEdit->setReadOnly(true);
+			ui.label->setVisible(false);
+			ui.timeEdit->setVisible(false);
 			ui.diveTripLocation->show();
 			ui.location->hide();
 			ui.editDiveSiteButton->hide();
@@ -563,6 +565,8 @@ void MainTab::updateDiveInfo(bool clear)
 			ui.waterTempLabel->setVisible(true);
 			ui.watertemp->setVisible(true);
 			ui.dateEdit->setReadOnly(false);
+			ui.label->setVisible(true);
+			ui.timeEdit->setVisible(true);
 			/* and fill them from the dive */
 			ui.rating->setCurrentStars(displayed_dive.rating);
 			ui.visibility->setCurrentStars(displayed_dive.visibility);
