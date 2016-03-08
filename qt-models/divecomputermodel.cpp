302f3ac6ae3491a5f422646a63ca5f68eab1900b
Tomaz Canabrava <tomaz.canabrava@gmail.com>
2016-03-08 02:25:40 -0300

    Silence warnings in divecomputermodel
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-models/divecomputermodel.cpp
+++ qt-models/divecomputermodel.cpp
@@ -2,7 +2,7 @@
 #include "dive.h"
 #include "divelist.h"
 
-DiveComputerModel::DiveComputerModel(QMultiMap<QString, DiveComputerNode> &dcMap, QObject *parent) : CleanerTableModel()
+DiveComputerModel::DiveComputerModel(QMultiMap<QString, DiveComputerNode> &dcMap, QObject *parent) : CleanerTableModel(parent)
 {
 	setHeaderDataStrings(QStringList() << "" << tr("Model") << tr("Device ID") << tr("Nickname"));
 	dcWorkingMap = dcMap;
@@ -78,6 +78,10 @@ Qt::ItemFlags DiveComputerModel::flags(const QModelIndex &index) const
 
 bool DiveComputerModel::setData(const QModelIndex &index, const QVariant &value, int role)
 {
+	// We should test if the role == Qt::EditRole
+	Q_UNUSED(role);
+
+	// WARN: This seems wrong - The values don't are ordered - we need a map from the Key to Index, or something.
 	QList<DiveComputerNode> values = dcWorkingMap.values();
 	DiveComputerNode node = values.at(index.row());
 	dcWorkingMap.remove(node.model, node);
