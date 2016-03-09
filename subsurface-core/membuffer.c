3d7a9a20958ac5567a20ff51663a4e15d02ea705
Tomaz Canabrava <tomaz.canabrava@gmail.com>
2016-03-09 15:18:34 -0300

    Silence warnings in membuffer.c
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- subsurface-core/membuffer.c
+++ subsurface-core/membuffer.c
@@ -1,3 +1,6 @@
+// Clang has a bug on zero-initialization of C structs.
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
