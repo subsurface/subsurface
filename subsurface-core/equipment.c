1048845650d3ae7e8cea93eec06e8ab86f02d3e5
Tomaz Canabrava <tomaz.canabrava@gmail.com>
2016-03-09 15:17:17 -0300

    Silence warnings in equipment.c
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- subsurface-core/equipment.c
+++ subsurface-core/equipment.c
@@ -1,3 +1,6 @@
+// Clang has a bug on zero-initialization of C structs.
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+
 /* equipment.c */
 #include <stdio.h>
 #include <string.h>
