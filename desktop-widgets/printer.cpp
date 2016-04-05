subsurface/subsurface

commit 7be962bfc2879a72c32ff67518731347dcdff6de

Move subsurface-core to core and qt-mobile to mobile-widgets
Having subsurface-core as a directory name really messes with
autocomplete and is obviously redundant. Simmilarly, qt-mobile caused an
autocomplete conflict and also was inconsistent with the desktop-widget
name for the directory containing the "other" UI.

And while cleaning up the resulting change in the path name for include
files, I decided to clean up those even more to make them consistent
overall.

This could have been handled in more commits, but since this requires a
make clean before the build, it seemed more sensible to do it all in one.

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

desktop-widgets/maintab.ui
@@ -1205,47 +1205,47 @@
   <customwidget>
    <class>KMessageWidget</class>
    <extends>QFrame</extends>
-   <header>kmessagewidget.h</header>
+   <header>desktop-widgets/kmessagewidget.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>StarWidget</class>
    <extends>QWidget</extends>
-   <header>starwidget.h</header>
+   <header>desktop-widgets/starwidget.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>MinMaxAvgWidget</class>
    <extends>QWidget</extends>
-   <header>simplewidgets.h</header>
+   <header>desktop-widgets/simplewidgets.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>TableView</class>
    <extends>QWidget</extends>
-   <header>tableview.h</header>
+   <header>desktop-widgets/desktop-widget/tableview.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>TagWidget</class>
    <extends>QPlainTextEdit</extends>
-   <header>tagwidget.h</header>
+   <header>desktop-widgets/tagwidget.h</header>
   </customwidget>
   <customwidget>
    <class>DivePictureWidget</class>
    <extends>QListView</extends>
-   <header>divepicturewidget.h</header>
+   <header>desktop-widgets/divepicturewidget.h</header>
   </customwidget>
   <customwidget>
    <class>QtWaitingSpinner</class>
    <extends>QWidget</extends>
-   <header>qtwaitingspinner.h</header>
+   <header>desktop-widgets/qtwaitingspinner.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
    <class>DiveLocationLineEdit</class>
    <extends>QLineEdit</extends>
-   <header>locationinformation.h</header>
+   <header>desktop-widgets/locationinformation.h</header>
   </customwidget>
  </customwidgets>
  <tabstops>

desktop-widgets/templatelayout.h
@@ -7,7 +7,7 @@
 #include "statistics.h"
 #include "qthelper.h"
 #include "helpers.h"
-#include "subsurface-core/subsurface-qt/DiveObjectHelper.h"
+#include "core/subsurface-qt/DiveObjectHelper.h"
 
 int getTotalWork(print_options *printOptions);
 void find_all_templates();

commit ad2ac58ea2c7a696227b2db243a76adb633158dd

Fix include paths
As expected, commit 7be962bfc287 ("Move subsurface-core to core and qt-mobile
to mobile-widgets") caused some breakage.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

desktop-widgets/maintab.ui
@@ -1223,7 +1223,7 @@
   <customwidget>
    <class>TableView</class>
    <extends>QWidget</extends>
-   <header>desktop-widgets/desktop-widget/tableview.h</header>
+   <header>desktop-widgets/tableview.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>

desktop-widgets/printer.cpp
@@ -1,7 +1,7 @@
 #include "printer.h"
 #include "templatelayout.h"
-#include "statistics.h"
-#include "helpers.h"
+#include "core/statistics.h"
+#include "core/helpers.h"
 
 #include <algorithm>
 #include <QtWebKitWidgets>

desktop-widgets/printoptions.cpp
@@ -1,6 +1,6 @@
 #include "printoptions.h"
 #include "templateedit.h"
-#include "helpers.h"
+#include "core/helpers.h"
 
 #include <QDebug>
 #include <QFileDialog>

desktop-widgets/templatelayout.cpp
@@ -1,8 +1,8 @@
 #include <string>
 
 #include "templatelayout.h"
-#include "helpers.h"
-#include "display.h"
+#include "core/helpers.h"
+#include "core/display.h"
 
 QList<QString> grantlee_templates, grantlee_statistics_templates;
 

desktop-widgets/templatelayout.h
@@ -4,9 +4,9 @@
 #include <grantlee_templates.h>
 #include "mainwindow.h"
 #include "printoptions.h"
-#include "statistics.h"
-#include "qthelper.h"
-#include "helpers.h"
+#include "core/statistics.h"
+#include "core/qthelper.h"
+#include "core/helpers.h"
 #include "core/subsurface-qt/DiveObjectHelper.h"
 
 int getTotalWork(print_options *printOptions);