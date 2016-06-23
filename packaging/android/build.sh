9443b0b86cad298cffbab0f9577af2b925defdd8
Anton Lundin <glance@acc.umu.se>
2016-06-23 22:56:30 +0200

    android: Use a ndk toolchain matching sdk version
    Signed-off-by: Anton Lundin <glance@acc.umu.se>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/android/build.sh
+++ packaging/android/build.sh
@@ -80,7 +80,7 @@ fi
 export QT5_ANDROID_BIN=${QT5_ANDROID}/android_${QT_ARCH}/bin
 
 if [ ! -e ndk-$ARCH ] ; then
-	$ANDROID_NDK_ROOT/build/tools/make_standalone_toolchain.py --arch=$ARCH --install-dir=ndk-$ARCH --api=14
+	$ANDROID_NDK_ROOT/build/tools/make_standalone_toolchain.py --arch=$ARCH --install-dir=ndk-$ARCH --api=16
 fi
 export BUILDROOT=$PWD
 export PATH=${BUILDROOT}/ndk-$ARCH/bin:$PATH
