87ced29e0954b7776d13e578727a7a216b97055c
Robert C. Helling <helling@atdotde.de>
2016-10-01 17:38:12 +0200

    Initialize tooltip tissue pixmap properly
    ...otherwise we show garbage before the mouse enters the
    profile for the first time.
    
    Signed-off-by: Robert C. Helling <helling@atdotde.de>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- profile-widget/divetooltipitem.cpp
+++ profile-widget/divetooltipitem.cpp
@@ -253,15 +253,16 @@ void ToolTipItem::refresh(const QPointF &pos)
 
 	mb.len = 0;
 	entry = get_plot_details_new(&pInfo, time, &mb);
+
+	tissues.fill();
+	painter.setPen(QColor(0, 0, 0, 0));
+	painter.setBrush(QColor(LIMENADE1));
+	painter.drawRect(0, 10 + (100 - AMB_PERCENTAGE) / 2, 16, AMB_PERCENTAGE / 2);
+	painter.setBrush(QColor(SPRINGWOOD1));
+	painter.drawRect(0, 10, 16, (100 - AMB_PERCENTAGE) / 2);
+	painter.setBrush(QColor(Qt::red));
+	painter.drawRect(0,0,16,10);
 	if (entry) {
-		tissues.fill();
-		painter.setPen(QColor(0, 0, 0, 0));
-		painter.setBrush(QColor(LIMENADE1));
-		painter.drawRect(0, 10 + (100 - AMB_PERCENTAGE) / 2, 16, AMB_PERCENTAGE / 2);
-		painter.setBrush(QColor(SPRINGWOOD1));
-		painter.drawRect(0, 10, 16, (100 - AMB_PERCENTAGE) / 2);
-		painter.setBrush(QColor(Qt::red));
-		painter.drawRect(0,0,16,10);
 		painter.setPen(QColor(0, 0, 0, 255));
 		painter.drawLine(0, 60 - entry->gfline / 2, 16, 60 - entry->gfline / 2);
 		painter.drawLine(0, 60 - AMB_PERCENTAGE * (entry->pressures.n2 + entry->pressures.he) / entry->ambpressure / 2,
@@ -270,9 +271,9 @@ void ToolTipItem::refresh(const QPointF &pos)
 		for (int i=0; i<16; i++) {
 			painter.drawLine(i, 60, i, 60 - entry->percentages[i] / 2);
 		}
-		entryToolTip.first->setPixmap(tissues);
 		entryToolTip.second->setText(QString::fromUtf8(mb.buffer, mb.len));
 	}
+	entryToolTip.first->setPixmap(tissues);
 
 	Q_FOREACH (QGraphicsItem *item, scene()->items(pos, Qt::IntersectsItemBoundingRect
 		,Qt::DescendingOrder, scene()->views().first()->transform())) {
