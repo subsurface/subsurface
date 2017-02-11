e7f3a0f27304287ee9acfcc4ca85e8bb8d46198a
Dirk Hohndel <dirk@hohndel.org>
2017-02-11 10:41:19 -0800

    Update translation source strings
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- translations/subsurface_source.ts
+++ translations/subsurface_source.ts
@@ -3695,6 +3695,7 @@ p, li { white-space: pre-wrap; }
         <source>, %n dive(s) here)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
@@ -6194,6 +6195,7 @@ See http://doc.qt.io/qt-5/qdatetime.html#toString</source>
         <location filename="../core/qthelper.cpp" line="962"/>
         <source>(%n dive(s))</source>
         <translation type="unfinished">
+            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
