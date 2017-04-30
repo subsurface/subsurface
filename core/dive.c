subsurface/subsurface

commit b5de08b709819783e5d79ffffa333cea05f5186f

No gas change event on merging dives with same gas
When merging dives, this will skip the gas change event if both dives
use same gas.

Fixes #1099

Signed-off-by: Miika Turkia <miika.turkia@gmail.com>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

core/dive.c
@@ -1765,6 +1765,15 @@ static int sort_event(struct event *a, struct event *b)
 	return strcmp(a->name, b->name);
 }
 
+static int same_gas(struct event *a, struct event *b)
+{
+	if (a->type == b->type && a->flags == b->flags && a->value == b->value && !strcmp(a->name, b->name) &&
+			a->gas.mix.o2.permille == b->gas.mix.o2.permille && a->gas.mix.he.permille == b->gas.mix.he.permille) {
+		return true;
+	}
+	return false;
+}
+
 static void merge_events(struct divecomputer *res, struct divecomputer *src1, struct divecomputer *src2, int offset)
 {
 	struct event *a, *b;
@@ -1799,6 +1808,19 @@ static void merge_events(struct divecomputer *res, struct divecomputer *src1, st
 			break;
 		}
 		s = sort_event(a, b);
+
+		/* No gas change event when continuing with same gas */
+		if (same_gas(a, b)) {
+			if (s > 0) {
+				p = &b->next;
+				b = b->next;
+			} else {
+				p = &a->next;
+				a = a->next;
+			}
+			continue;
+		}
+
 		/* Pick b */
 		if (s > 0) {
 			*p = b;

commit 9084bbae578b3996d8e8c7adb74ef9bb25da6720

Fix event merging when interleaving dives
The core to avoid adding redundant gas switch events was completely
buggered, and caused the result list to be corrupted if it ever
triggered.  This should fix it.

Fixes: b5de08b7 ("No gas change event on merging dives with same gas")
Reported-by: Jan Mulder <jlmulder@xs4all.nl>
Cc: Miika Turkia <miika.turkia@gmail.com>
Cc: Dirk Hohndel <dirk@hohndel.org>
Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

core/dive.c
@@ -1783,6 +1783,7 @@ static void merge_events(struct divecomputer *res, struct divecomputer *src1, st
 {
 	struct event *a, *b;
 	struct event **p = &res->events;
+	struct event *last_gas = NULL;
 
 	/* Always use positive offsets */
 	if (offset < 0) {
@@ -1804,6 +1805,8 @@ static void merge_events(struct divecomputer *res, struct divecomputer *src1, st
 
 	while (a || b) {
 		int s;
+		struct event *pick;
+
 		if (!b) {
 			*p = a;
 			break;
@@ -1814,32 +1817,34 @@ static void merge_events(struct divecomputer *res, struct divecomputer *src1, st
 		}
 		s = sort_event(a, b);
 
-		/* No gas change event when continuing with same gas */
-		if (same_gas(a, b)) {
-			if (s > 0) {
-				p = &b->next;
-				b = b->next;
-			} else {
-				p = &a->next;
-				a = a->next;
-			}
+		/* Identical events? Just skip one of them (we pick a) */
+		if (!s) {
+			a = a->next;
 			continue;
 		}
 
-		/* Pick b */
-		if (s > 0) {
-			*p = b;
-			p = &b->next;
-			b = b->next;
-			continue;
-		}
-		/* Pick 'a' or neither */
+		/* Otherwise, pick the one that sorts first */
 		if (s < 0) {
-			*p = a;
-			p = &a->next;
+			pick = a;
+			a = a->next;
+		} else {
+			pick = b;
+			b = b->next;
 		}
-		a = a->next;
-		continue;
+
+		/*
+		 * If that's a gas-change that matches the previous
+		 * gas change, we'll just skip it
+		 */
+		if (event_is_gaschange(pick)) {
+			if (last_gas && same_gas(pick, last_gas))
+				continue;
+			last_gas = pick;
+		}
+
+		/* Add it to the target list */
+		*p = pick;
+		p = &pick->next;
 	}
 }
 