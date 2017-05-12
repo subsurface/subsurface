bb6ceba4ac0310c504f931674a71e9e77c0afa1c
Robert C. Helling <helling@atdotde.de>
2017-05-12 15:36:24 +0200

    Compute and display gas density
    This appears to be critical for work of breathing so it might be
    worthwhile to compute. So far only in infobox.
    
    For background, see
    
    https://www.youtube.com/watch?v=QBajM3xmOtc
    
    Signed-off-by: Robert C. Helling <helling@atdotde.de>

--- core/dive.h
+++ core/dive.h
@@ -139,6 +139,8 @@ extern int units_to_sac(double volume);
 extern int gas_volume(cylinder_t *cyl, pressure_t p);
 extern double gas_compressibility_factor(struct gasmix *gas, double bar);
 extern double isothermal_pressure(struct gasmix *gas, double p1, int volume1, int volume2);
+extern double gas_density(struct gasmix *gas, int pressure);
+
 
 
 static inline int get_o2(const struct gasmix *mix)
