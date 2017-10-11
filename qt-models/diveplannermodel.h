3072a7c8cc140783f1d4755f21ed395cf2f6c824
Stefan Fuchs <sfuchs@gmx.de>
2017-10-11 22:03:03 +0200

    Correctly name member function gasChange of DivePlannerPointsModel
    Signed-off-by: Stefan Fuchs <sfuchs@gmx.de>

--- qt-models/diveplannermodel.h
+++ qt-models/diveplannermodel.h
@@ -31,7 +31,7 @@ public:
 	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 	virtual bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
 	virtual Qt::ItemFlags flags(const QModelIndex &index) const;
-	void gaschange(const QModelIndex &index, int newcylinderid);
+	void gasChange(const QModelIndex &index, int newcylinderid);
 	void cylinderRenumber(int mapping[]);
 	void removeSelectedPoints(const QVector<int> &rows);
 	void setPlanMode(Mode mode);
