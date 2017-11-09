b425d5641d77e934d74b29a4e1abf2a83922fb93
Dirk Hohndel <dirk@hohndel.org>
2017-11-09 12:38:04 -0800

    Travis: use our own Qt binaries for AppImage build
    This is no longer based on the upstream Qt binaries but completely on
    binaries built by ourselves, trying to remove some of the features that
    we don't need and trying to avoid some of the issues with certain
    libraries (like different OpenSSL dependencies).
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/linux/before_install.sh
+++ scripts/linux/before_install.sh
@@ -2,22 +2,15 @@
 
 # prep things so we can build for Linux
 # we have a custom built Qt some gives us just what we need, including QtWebKit
+#
+# this is built from the latest version as of 2017-11-09 in the 5.9 branch and
+# therefore calls itself Qt-5.9.3
 
+export QT_ROOT=$PWD/Qt/5.9.3
 rm -rf Qt
-mkdir -p Qt/5.9.1
-wget http://subsurface-divelog.org/downloads/Qt-5.9.1.tar.xz
-tar -xJ -C Qt/5.9.1 -f Qt-5.9.1.tar.xz
-cd Qt/5.9.1
-
-# this should all be handled in the packaged tar file, for now we hack it here
-
-ln -s . gcc_64
-cd ..
-ln -s 5.9.1/* .
-cd ..
-
-# terrifying hack to fix the OpenSSL dependency issue
-sed -i -e 's|1.0.1e|1.0.0\x00|g' Qt/lib/libQt5Network.so.5
+mkdir -p $QT_ROOT
+wget http://subsurface-divelog.org/downloads/Qt-5.9.3-trusty.tar.xz
+tar -xJ -C $QT_ROOT -f Qt-5.9.3-trusty.tar.xz
 
 # TestPreferences uses gui calls, so run a xvfb so it has something to talk to
 export DISPLAY=:99.0
