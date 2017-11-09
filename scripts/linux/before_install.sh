bee160a9e2f4c94e2d5e777f26bcb39bffbb9c74
Dirk Hohndel <dirk@hohndel.org>
2017-11-09 12:41:30 -0800

    Travis: work around the way build.sh looks for private headers
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/linux/before_install.sh
+++ scripts/linux/before_install.sh
@@ -6,11 +6,15 @@
 # this is built from the latest version as of 2017-11-09 in the 5.9 branch and
 # therefore calls itself Qt-5.9.3
 
+set -x
+
 export QT_ROOT=$PWD/Qt/5.9.3
 rm -rf Qt
 mkdir -p $QT_ROOT
 wget http://subsurface-divelog.org/downloads/Qt-5.9.3-trusty.tar.xz
 tar -xJ -C $QT_ROOT -f Qt-5.9.3-trusty.tar.xz
+
+sudo ln -s $QT_ROOT /usr/local/Qt-5.9.3
 
 # TestPreferences uses gui calls, so run a xvfb so it has something to talk to
 export DISPLAY=:99.0
