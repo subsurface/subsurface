806c7077f2abbf8978e3fefa95b1663a31a00503
Jan Mulder <jlmulder@xs4all.nl>
2017-11-22 19:22:02 +0100

    mobile: get GPS data from dive site name
    When the user entered a dive site using autocompletion, it
    is a known site, of which we might have a GPS location already.
    Just fill the known site coordinates.
    
    Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

--- mobile-widgets/qmlmanager.h
+++ mobile-widgets/qmlmanager.h
@@ -184,6 +184,7 @@ public slots:
 	QString getNumber(const QString& diveId);
 	QString getDate(const QString& diveId);
 	QString getCurrentPosition();
+	QString getGpsFromSiteName(const QString& siteName);
 	QString getVersion() const;
 	void deleteGpsFix(quint64 when);
 	void revertToNoCloudIfNeeded();
