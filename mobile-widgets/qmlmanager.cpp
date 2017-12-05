4dbbf1ff27daccd1b2abc9a2257cc2089ac9b173
Murillo Bernardes <mfbernardes@gmail.com>
2017-12-05 16:34:50 +0800

    iOS: report location services availability
    This enable use of current location on Add/Edit
    
    Signed-off-by: Murillo Bernardes <mfbernardes@gmail.com>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- mobile-widgets/qmlmanager.cpp
+++ mobile-widgets/qmlmanager.cpp
@@ -1344,11 +1344,7 @@ void QMLManager::setLocationServiceEnabled(bool locationServiceEnabled)
 
 bool QMLManager::locationServiceAvailable() const
 {
-#if defined(Q_OS_IOS)
-	return false;
-#else
 	return m_locationServiceAvailable;
-#endif
 }
 
 void QMLManager::setLocationServiceAvailable(bool locationServiceAvailable)
