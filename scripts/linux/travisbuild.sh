88a371671f7b923db9abbc5ee0b9797c53f67894
Dirk Hohndel <dirk@hohndel.org>
2017-12-05 11:50:57 -0800

    Travis: no longer test Subsurface-mobile without BT
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/linux/travisbuild.sh
+++ scripts/linux/travisbuild.sh
@@ -10,13 +10,13 @@ export PATH=$QT_ROOT/bin:$PATH # Make sure correct qmake is found on the $PATH f
 export CMAKE_PREFIX_PATH=$QT_ROOT/lib/cmake
 
 # the global build script expects to be called from the directory ABOVE subsurface
-# build both desktop and mobile - first without BT support and without
+# build both desktop and mobile - first desktop without BT support and without
 # webkit to make sure that still works, then with all components in order
 # to create an AppImage
 cd ..
 
-bash -e -x ./subsurface/scripts/build.sh -both -no-bt
-rm -rf subsurface/build subsurface/build-mobile
+bash -e -x ./subsurface/scripts/build.sh -desktop -no-bt
+rm -rf subsurface/build
 bash -e -x ./subsurface/scripts/build.sh -both -create-appdir -build-with-webkit
 
 export QT_PLUGIN_PATH=$QT_ROOT/plugins
