subsurface/subsurface

commit 46004c39e266fe76f9945e52000dd051de3f4099

Store and load the planner related depth info of a cylinder in logfile
Store cylinder.depth in XML files and in git storage.
This info is in fact the gas switch depth of a specific gas/cylinder
in the planner.
This change avoids the need of typing in a user specific depth value
again when replanning an existing planned dive.

Signed-off-by: Stefan Fuchs <sfuchs@gmx.de>

core/load-git.c
@@ -390,6 +390,10 @@ static void parse_cylinder_keyvalue(void *_cylinder, const char *key, const char
 		cylinder->cylinder_use = cylinderuse_from_text(value);
 		return;
 	}
+	if (!strcmp(key, "depth")) {
+		cylinder->depth = get_depth(value);
+		return;
+	}
 	report_error("Unknown cylinder key/value pair (%s/%s)", key, value);
 }
 

core/parse-xml.c
@@ -1467,6 +1467,8 @@ static void try_to_fill_dive(struct dive *dive, const char *name, char *buf)
 			return;
 		if (MATCH("use.cylinder", cylinder_use, &dive->cylinder[cur_cylinder_index].cylinder_use))
 			return;
+		if (MATCH("depth.cylinder", depth, &dive->cylinder[cur_cylinder_index].depth))
+			return;
 		if (MATCH("o2", gasmix, &dive->cylinder[cur_cylinder_index].gasmix.o2))
 			return;
 		if (MATCH("o2percent", gasmix, &dive->cylinder[cur_cylinder_index].gasmix.o2))

core/save-git.c
@@ -157,7 +157,8 @@ static void save_cylinder_info(struct membuffer *b, struct dive *dive)
 		put_pressure(b, cylinder->end, " end=", "bar");
 		if (cylinder->cylinder_use != OC_GAS)
 			put_format(b, " use=%s", cylinderuse_text[cylinder->cylinder_use]);
-
+		if (cylinder->depth.mm != 0)
+			put_milli(b, " depth='", cylinder->depth.mm, " m'");
 		put_string(b, "\n");
 	}
 }

core/save-xml.c
@@ -159,6 +159,8 @@ static void save_cylinder_info(struct membuffer *b, struct dive *dive)
 		put_pressure(b, cylinder->end, " end='", " bar'");
 		if (cylinder->cylinder_use != OC_GAS)
 			show_utf8(b, cylinderuse_text[cylinder->cylinder_use], " use='", "'", 1);
+		if (cylinder->depth.mm != 0)
+			put_milli(b, " depth='", cylinder->depth.mm, " m'");
 		put_format(b, " />\n");
 	}
 }

commit c9a59257f49277c40f37e3e9d8ef1256e6312a04

core: ignore bogus m in cylinder
A bogus key/value pair was introduced in the cylinder,
consisting of a lonely "m" without value. This is caused
by commit 46004c39e26 and fixed in 48d9c8eb6eb0. See referenced
commits for more info.

Just ignore this key/value pair. No processing is broken
due to this, as the git storage stores only metric SI type data.
In fact, the m unit is superfluous anyway.

Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

core/load-git.c
@@ -394,6 +394,18 @@ static void parse_cylinder_keyvalue(void *_cylinder, const char *key, const char
 		cylinder->depth = get_depth(value);
 		return;
 	}
+	if (!strcmp(key, "m'") && strlen(value) == 0) {
+		/* found a bogus key/value pair in the cylinder, consisting
+		 * of a lonely "m" without value. This is caused by commit
+		 * 46004c39e26 and fixed in 48d9c8eb6eb0. See referenced
+		 * commits for more info.
+		 *
+		 * Just ignore this key/value pair. No processing is broken
+		 * due to this, as the git storage stores only metric SI type data.
+		 * In fact, the m unit is superfluous anyway.
+		 */
+		return;
+	}
 	report_error("Unknown cylinder key/value pair (%s/%s)", key, value);
 }
 