subsurface/subsurface

commit 46004c39e266fe76f9945e52000dd051de3f4099

Store and load the planner related depth info of a cylinder in logfile
Store cylinder.depth in XML files and in git storage.
This info is in fact the gas switch depth of a specific gas/cylinder
in the planner.
This change avoids the need of typing in a user specific depth value
again when replanning an existing planned dive.

Signed-off-by: Stefan Fuchs <sfuchs@gmx.de>

core/save-git.c
@@ -157,7 +157,8 @@ static void save_cylinder_info(struct membuffer *b, struct dive *dive)
 		put_pressure(b, cylinder->end, " end=", "bar");
 		if (cylinder->cylinder_use != OC_GAS)
 			put_format(b, " use=%s", cylinderuse_text[cylinder->cylinder_use]);
-
+		if (cylinder->depth.mm != 0)
+			put_milli(b, " depth='", cylinder->depth.mm, " m'");
 		put_string(b, "\n");
 	}
 }

core/load-git.c
@@ -390,6 +390,10 @@ static void parse_cylinder_keyvalue(void *_cylinder, const char *key, const char
 		cylinder->cylinder_use = cylinderuse_from_text(value);
 		return;
 	}
+	if (!strcmp(key, "depth")) {
+		cylinder->depth = get_depth(value);
+		return;
+	}
 	report_error("Unknown cylinder key/value pair (%s/%s)", key, value);
 }
 

core/parse-xml.c
@@ -1467,6 +1467,8 @@ static void try_to_fill_dive(struct dive *dive, const char *name, char *buf)
 			return;
 		if (MATCH("use.cylinder", cylinder_use, &dive->cylinder[cur_cylinder_index].cylinder_use))
 			return;
+		if (MATCH("depth.cylinder", depth, &dive->cylinder[cur_cylinder_index].depth))
+			return;
 		if (MATCH("o2", gasmix, &dive->cylinder[cur_cylinder_index].gasmix.o2))
 			return;
 		if (MATCH("o2percent", gasmix, &dive->cylinder[cur_cylinder_index].gasmix.o2))

core/save-xml.c
@@ -159,6 +159,8 @@ static void save_cylinder_info(struct membuffer *b, struct dive *dive)
 		put_pressure(b, cylinder->end, " end='", " bar'");
 		if (cylinder->cylinder_use != OC_GAS)
 			show_utf8(b, cylinderuse_text[cylinder->cylinder_use], " use='", "'", 1);
+		if (cylinder->depth.mm != 0)
+			put_milli(b, " depth='", cylinder->depth.mm, " m'");
 		put_format(b, " />\n");
 	}
 }

commit b984fb98c38e44af5372a21fd6921c23808fe531

core: fix commit 46004c39e266fe7 for real (erroneous quotes)
Unfortunately, in my commit 48d9c8eb6eb07a, I fixed only half of
the problems related to the functionality introduced by Stefan in
commit 46004c39e266fe7. The lonely m (that was fixed) caused
a parsing error, but forgotten where the single quotes around
the depth value. These quotes simply causes the new functionality
not to work. Again, the fix is simple: do not erroneously save
quotes. And as the new functionality is pretty obscure
(replanning a non-planned dive, and manually entering a gas switch
depth), another bug that could go unnoticed for years.

Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

core/save-git.c
@@ -158,7 +158,7 @@ static void save_cylinder_info(struct membuffer *b, struct dive *dive)
 		if (cylinder->cylinder_use != OC_GAS)
 			put_format(b, " use=%s", cylinderuse_text[cylinder->cylinder_use]);
 		if (cylinder->depth.mm != 0)
-			put_milli(b, " depth='", cylinder->depth.mm, "m'");
+			put_milli(b, " depth=", cylinder->depth.mm, "m");
 		put_string(b, "\n");
 	}
 }