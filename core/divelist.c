subsurface/subsurface

commit b3901aa8f90499ee2a34efdddc2463105afc53f1

Enable cloud-online menu entry only if connected to cloud
Enable the menu item cloud-online only if the current file is the
cloud storage. Since this has to be checked every time the current
file is set, factor this out into the new MainWindow::setCurrentFile()
function.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

desktop-widgets/mainwindow.cpp
@@ -441,8 +441,6 @@ void MainWindow::enableDisableCloudActions()
 {
 	ui.actionCloudstorageopen->setEnabled(prefs.cloud_verification_status == CS_VERIFIED);
 	ui.actionCloudstoragesave->setEnabled(prefs.cloud_verification_status == CS_VERIFIED);
-	ui.actionCloudOnline->setEnabled(prefs.cloud_verification_status == CS_VERIFIED);
-	ui.actionCloudOnline->setChecked(prefs.cloud_verification_status == CS_VERIFIED && !prefs.git_local_only);
 }
 
 PlannerDetails *MainWindow::plannerDetails() const {
@@ -611,10 +609,8 @@ void MainWindow::on_actionCloudstorageopen_triggered()
 
 	showProgressBar();
 	QByteArray fileNamePtr = QFile::encodeName(filename);
-	if (!parse_file(fileNamePtr.data())) {
-		set_filename(fileNamePtr.data());
-		setTitle();
-	}
+	if (!parse_file(fileNamePtr.data()))
+		setCurrentFile(fileNamePtr.data());
 	process_dives(false, false);
 	hideProgressBar();
 	refreshDisplay();
@@ -641,8 +637,7 @@ void MainWindow::on_actionCloudstoragesave_triggered()
 	if (error)
 		return;
 
-	set_filename(filename.toUtf8().data());
-	setTitle();
+	setCurrentFile(filename.toUtf8().data());
 	mark_divelist_changed(false);
 }
 
@@ -684,7 +679,7 @@ void MainWindow::on_actionCloudOnline_triggered()
 	}
 
 	setTitle();
-	ui.actionCloudOnline->setChecked(!prefs.git_local_only);
+	updateCloudOnlineStatus();
 }
 
 void learnImageDirs(QStringList dirnames)
@@ -757,6 +752,21 @@ void MainWindow::closeCurrentFile()
 	clear_events();
 
 	dcList.dcMap.clear();
+}
+
+void MainWindow::updateCloudOnlineStatus()
+{
+	bool is_cloud = existing_filename && prefs.cloud_git_url && prefs.cloud_verification_status == CS_VERIFIED &&
+			strstr(existing_filename, prefs.cloud_git_url);
+	ui.actionCloudOnline->setEnabled(is_cloud);
+	ui.actionCloudOnline->setChecked(is_cloud && !prefs.git_local_only);
+}
+
+void MainWindow::setCurrentFile(const char *f)
+{
+	set_filename(f);
+	setTitle();
+	updateCloudOnlineStatus();
 }
 
 void MainWindow::on_actionClose_triggered()
@@ -1685,8 +1695,7 @@ int MainWindow::file_save_as(void)
 	if (save_dives(filename.toUtf8().data()))
 		return -1;
 
-	set_filename(filename.toUtf8().data());
-	setTitle();
+	setCurrentFile(filename.toUtf8().data());
 	mark_divelist_changed(false);
 	addRecentFile(filename, true);
 	return 0;
@@ -1740,7 +1749,6 @@ QString MainWindow::displayedFilename(QString fullFilename)
 
 	if (fullFilename.contains(prefs.cloud_git_url)) {
 		QString email = fileName.left(fileName.indexOf('['));
-		ui.actionCloudOnline->setChecked(!prefs.git_local_only);
 		if (prefs.git_local_only)
 			return tr("[local cache for] %1").arg(email);
 		else
@@ -1824,9 +1832,8 @@ void MainWindow::loadFiles(const QStringList fileNames)
 	for (int i = 0; i < fileNames.size(); ++i) {
 		fileNamePtr = QFile::encodeName(fileNames.at(i));
 		if (!parse_file(fileNamePtr.data())) {
-			set_filename(fileNamePtr.data());
+			setCurrentFile(fileNamePtr.data());
 			addRecentFile(fileNamePtr, false);
-			setTitle();
 		}
 	}
 	hideProgressBar();

desktop-widgets/mainwindow.h
@@ -198,6 +198,8 @@ private:
 	bool askSaveChanges();
 	bool okToClose(QString message);
 	void closeCurrentFile();
+	void setCurrentFile(const char *f);
+	void updateCloudOnlineStatus();
 	void showProgressBar();
 	void hideProgressBar();
 	void writeSettings();

commit e1ef021baff604f1c52219f6afcabd6cc120c8bf

Move resetting of current file out of clear_dive_file_data()
This is the only case where C-code sets the current file.
Remove this call for a better separation of C-backend and
C++-frontend parts.

There were four callers of clear_dive_file_data(). Two of them
would call set_filename() anyway. For the remaining two add an
explicit call to set_filename().

This commit fixes a bug introduced in commit b3901aa8f90499ee2a34efdddc2463105afc53f1:
The cloud-online menu entry was still enabled after "closing" the
cloud storage.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

core/divelist.c
@@ -1426,9 +1426,6 @@ void clear_dive_file_data()
 	clear_dive(&displayed_dive);
 	clear_dive_site(&displayed_dive_site);
 
-	free((void *)existing_filename);
-	existing_filename = NULL;
-
 	reset_min_datafile_version();
 	saved_git_id = "";
 }

desktop-widgets/mainwindow.cpp
@@ -746,6 +746,7 @@ void MainWindow::closeCurrentFile()
 	/* free the dives and trips */
 	clear_git_id();
 	clear_dive_file_data();
+	setCurrentFile(NULL);
 	cleanUpEmpty();
 	mark_divelist_changed(false);
 

mobile-widgets/qmlmanager.cpp
@@ -603,6 +603,7 @@ void QMLManager::loadDivesWithValidCredentials()
 		appendTextToLog(errorString);
 		setNotificationText(errorString);
 		revertToNoCloudIfNeeded();
+		set_filename(NULL);
 		return;
 	}
 	consumeFinishedLoad(currentDiveTimestamp);