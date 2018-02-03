subsurface/subsurface

commit d23bd46a1be2dfb25293639abcf06b5b4d4b94df

Reset device field in DownloadFromDCWidget according to dive computer
Fixes minor interface inconsistency: The device field in the
download-from-dive-computer widget is disabled when selecting
a non-serial-transport dive computer. In contrast, post-download
the field was reset to enabled for all dive computers.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

desktop-widgets/downloadfromdivecomputer.cpp
@@ -258,18 +258,7 @@ void DownloadFromDCWidget::on_vendor_currentIndexChanged(const QString &vendor)
 
 void DownloadFromDCWidget::on_product_currentIndexChanged(const QString &product)
 {
-	// Set up the DC descriptor
-	dc_descriptor_t *descriptor = NULL;
-	descriptor = descriptorLookup[ui.vendor->currentText() + product];
-
-	// call dc_descriptor_get_transport to see if the dc_transport_t is DC_TRANSPORT_SERIAL
-	if (dc_descriptor_get_transport(descriptor) == DC_TRANSPORT_SERIAL) {
-		// if the dc_transport_t is DC_TRANSPORT_SERIAL, then enable the device node box.
-		ui.device->setEnabled(true);
-	} else {
-		// otherwise disable the device node box
-		ui.device->setEnabled(false);
-	}
+	updateDeviceEnabled();
 }
 
 void DownloadFromDCWidget::on_search_clicked()
@@ -489,6 +478,22 @@ void DownloadFromDCWidget::on_ok_clicked()
 	accept();
 }
 
+void DownloadFromDCWidget::updateDeviceEnabled()
+{
+	// Set up the DC descriptor
+	dc_descriptor_t *descriptor = NULL;
+	descriptor = descriptorLookup[ui.vendor->currentText() + ui.product->currentText()];
+
+	// call dc_descriptor_get_transport to see if the dc_transport_t is DC_TRANSPORT_SERIAL
+	if (dc_descriptor_get_transport(descriptor) == DC_TRANSPORT_SERIAL) {
+		// if the dc_transport_t is DC_TRANSPORT_SERIAL, then enable the device node box.
+		ui.device->setEnabled(true);
+	} else {
+		// otherwise disable the device node box
+		ui.device->setEnabled(false);
+	}
+}
+
 void DownloadFromDCWidget::markChildrenAsDisabled()
 {
 	ui.device->setEnabled(false);
@@ -511,7 +516,7 @@ void DownloadFromDCWidget::markChildrenAsDisabled()
 
 void DownloadFromDCWidget::markChildrenAsEnabled()
 {
-	ui.device->setEnabled(true);
+	updateDeviceEnabled();
 	ui.vendor->setEnabled(true);
 	ui.product->setEnabled(true);
 	ui.forceDownload->setEnabled(true);

desktop-widgets/downloadfromdivecomputer.h
@@ -58,6 +58,7 @@ slots:
 private:
 	void markChildrenAsDisabled();
 	void markChildrenAsEnabled();
+	void updateDeviceEnabled();
 
 	Ui::DownloadFromDiveComputer ui;
 	DownloadThread thread;

commit 0776da2ed0ea25e251b19db76647e9b32def9130

Desktop: enable device field for IRDA transports
Fixes a bug introduced in d23bd46a1be2dfb25293639abcf06b5b4d4b94df:
device field was disabled for IRDA transports.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

desktop-widgets/downloadfromdivecomputer.cpp
@@ -480,13 +480,19 @@ void DownloadFromDCWidget::updateDeviceEnabled()
 	dc_descriptor_t *descriptor = NULL;
 	descriptor = descriptorLookup.value(ui.vendor->currentText() + ui.product->currentText());
 
-	// call dc_descriptor_get_transport to see if the dc_transport_t is DC_TRANSPORT_SERIAL
-	if (dc_descriptor_get_transport(descriptor) == DC_TRANSPORT_SERIAL) {
-		// if the dc_transport_t is DC_TRANSPORT_SERIAL, then enable the device node box.
+	// Enable the device selection field only for transports that need a device
+	switch (dc_descriptor_get_transport(descriptor)) {
+	case DC_TRANSPORT_SERIAL:
+	case DC_TRANSPORT_IRDA:
+	default:
 		ui.device->setEnabled(true);
-	} else {
-		// otherwise disable the device node box
+		break;
+	case DC_TRANSPORT_BLUETOOTH:
+	case DC_TRANSPORT_USB:
+	case DC_TRANSPORT_USBHID:
+	case DC_TRANSPORT_NONE:
 		ui.device->setEnabled(false);
+		break;
 	}
 }
 