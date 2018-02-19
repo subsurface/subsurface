9ec6aafe5156f8eec71e51f00923406da79caffa
Salvador Cuñat <salvador.cunat@gmail.com>
2018-02-19 19:38:16 +0100

    smtk-import: Add an AppImage for smtk2ssrf
    Build an AppImage for smtk2ssrf using previously build binaries.
    
    Signed-off-by: Salvador Cuñat <salvador.cunat@gmail.com>

--- scripts/linux/travisbuild.sh
+++ scripts/linux/travisbuild.sh
@@ -51,3 +51,22 @@ find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " "
 
 # SmartTrak import tool
 bash -e -x ./subsurface/scripts/smtk2ssrf-build.sh
+
+# Create AppImage for smtk2ssrf
+mkdir -p ./smtk2ssrf_appdir/usr/share
+mkdir -p ./smtk2ssrf_appdir/usr/plugins
+mkdir -p ./smtk2ssrf_appdir/usr/bin
+mkdir -p ./smtk2ssrf_appdir/usr/lib
+cp -f subsurface/icons/subsurface-icon.svg smtk2ssrf_appdir/
+cp -f subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
+cp -f install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
+cp -f install-root/lib/libdivecomputer.so.0 smtk2ssrf_appdir/usr/lib/
+cp -f install-root/lib/libgit2* smtk2ssrf_appdir/usr/lib/
+# Why is Grantlee needed? We have built subsurface without printing support!!!
+cp -f install-root/lib/libGrantlee* smtk2ssrf_appdir/usr/lib/
+cp -rf appdir/usr/plugins/{bearer,iconengines,imageformats,platforms,xcbglintegrations} smtk2ssrf_appdir/usr/plugins
+
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -bundle-non-qt-libs -verbose=2
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -appimage -verbose=2
+find ./smtk2ssrf_appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+
