8fc34459b815bf7ace26de9ba20a97994a8fc8f9
jan Iversen <jani@libreoffice.org>
2018-05-22 08:14:06 +0200

    core: add ssrf.h as global product header
    ssrf contains macros/includes etc. used in ssrf, but
    not related to dives (mainly in dive.h)
    
    currently the header is created to add the macro UNUSED
    later it will be used to remove non-dive related items
    from dive.h
    
    Signed-off-by: Jan Iversen <jani@apache.org>

---
+++ core/ssrf.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef SSRF_H
+#define SSRF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#else
+
+// Macro to be used for silencing unused parameters
+#define UNUSED(x) (void)x
+#endif
+
+#endif // SSRF_H
