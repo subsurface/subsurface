7797274b6b34f8f97f280e12e4d8920e4414ce4c
jan Iversen <jani@libreoffice.org>
2018-06-05 12:43:51 +0200

    core: remove "static void copyPath" unused
    Remove copyPath since it is static and not called
    to avoid clang warning
    
    Signed-off-by: Jan Iversen <jani@apache.org>

--- core/qthelper.cpp
+++ core/qthelper.cpp
@@ -711,20 +711,6 @@ QString getPrintingTemplatePathBundle()
 	return path;
 }
 
-static void copyPath(QString src, QString dst)
-{
-	QDir dir(src);
-	if (!dir.exists())
-		return;
-	foreach (QString d, dir.entryList(QDir::Dirs | QDir::NoDotAndDotDot)) {
-		QString dst_path = dst + QDir::separator() + d;
-		dir.mkpath(dst_path);
-		copyPath(src + QDir::separator() + d, dst_path);
-	}
-	foreach (QString f, dir.entryList(QDir::Files))
-		QFile::copy(src + QDir::separator() + f, dst + QDir::separator() + f);
-}
-
 int gettimezoneoffset(timestamp_t when)
 {
 	QDateTime dt1, dt2;
