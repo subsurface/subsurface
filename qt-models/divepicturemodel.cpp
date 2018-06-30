subsurface/subsurface

commit fbe1144eaf7e800a014c7a97b846835ba9f3bc7f

Dive pictures: use modelReset signal when reseting picture list
In the old code a combination of removeRows()/insertRows() was used
to signal a model reset.

Replace this by a single, modelReset signal. This saves a call to
plotPictures() and will allow us to be smarter in the future,
when removing pictures.

Reset-model and remove-items are semantically different.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

qt-models/divepicturemodel.cpp
@@ -57,10 +57,9 @@ void DivePictureModel::updateThumbnails()
 
 void DivePictureModel::updateDivePictures()
 {
+	beginResetModel();
 	if (!pictures.isEmpty()) {
-		beginRemoveRows(QModelIndex(), 0, pictures.count() - 1);
 		pictures.clear();
-		endRemoveRows();
 		rowDDStart = rowDDEnd = 0;
 		Thumbnailer::instance()->clearWorkQueue();
 	}
@@ -83,11 +82,7 @@ void DivePictureModel::updateDivePictures()
 	}
 
 	updateThumbnails();
-
-	if (!pictures.isEmpty()) {
-		beginInsertRows(QModelIndex(), 0, pictures.count() - 1);
-		endInsertRows();
-	}
+	endResetModel();
 }
 
 int DivePictureModel::columnCount(const QModelIndex &parent) const

profile-widget/profilewidget2.cpp
@@ -1119,6 +1119,7 @@ void ProfileWidget2::setProfileState()
 	connect(DivePictureModel::instance(), &DivePictureModel::dataChanged, this, &ProfileWidget2::updatePictures);
 	connect(DivePictureModel::instance(), SIGNAL(rowsInserted(const QModelIndex &, int, int)), this, SLOT(plotPictures()));
 	connect(DivePictureModel::instance(), SIGNAL(rowsRemoved(const QModelIndex &, int, int)), this, SLOT(plotPictures()));
+	connect(DivePictureModel::instance(), &DivePictureModel::modelReset, this, &ProfileWidget2::plotPictures);
 #endif
 	/* show the same stuff that the profile shows. */
 

commit 24a6ad45107d7f33d906de0b33b10ec7a5324394

Dive pictures: remove unnecessary check for no dives
This fixes a bug introduced in fbe1144eaf7e800a014c7a97b846835ba9f3bc7f:
For an empty log, in DivePictureModel::updateDivePictures()
beginResetModel() would be called without a corresponding endResetModel().

It is unclear whether this can ever be hit, because in the no-dives
case, at least in the desktop version no profile is shown.
Note, that this makes the check double-unnecessary.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

qt-models/divepicturemodel.cpp
@@ -64,10 +64,6 @@ void DivePictureModel::updateDivePictures()
 		Thumbnailer::instance()->clearWorkQueue();
 	}
 
-	// if the dive_table is empty, quit
-	if (dive_table.nr == 0)
-		return;
-
 	int i;
 	struct dive *dive;
 	for_each_dive (i, dive) {