c0a08b889ed3b9912b5a4f455cce26ca009af184
Dirk Hohndel <dirk@hohndel.org>
2018-07-07 10:59:22 -0700

    build-system/macOS: correctly test if we need to adjust the id of libgit2
    The existing test was backwards and never worked.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -238,7 +238,7 @@ if [[ "$LIBGIT" < "24" ]] ; then
 		# in order for macdeployqt to do its job correctly, we need the full path in the dylib ID
 		cd $INSTALL_ROOT/lib
 		NAME=$(otool -L libgit2.dylib | grep -v : | head -1 | cut -f1 -d\  | tr -d '\t')
-		echo $NAME | if grep / > /dev/null 2>&1 ; then
+		echo $NAME | if grep -v / > /dev/null 2>&1 ; then
 			install_name_tool -id "$INSTALL_ROOT/lib/$NAME" "$INSTALL_ROOT/lib/$NAME"
 		fi
 	fi
