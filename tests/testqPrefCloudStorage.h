41d98ce51a10f0479ecd0b89dc57cf6929dc6b4f
jan Iversen <jani@apache.org>
2018-07-14 17:31:51 +0200

    tests: add qPrefCloudStorage testcases
    add test cases to secure struct preferences and qPrefCloudStorage work together
    
    Signed-off-by: Jan Iversen <jani@apache.org>

---
+++ tests/testqPrefCloudStorage.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef TESTQPREFCLOUDSTORAGE_H
+#define TESTQPREFCLOUDSTORAGE_H
+
+#include <QObject>
+
+class TestQPrefCloudStorage : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void initTestCase();
+	void test_struct_get();
+	void test_set_struct();
+	void test_set_load_struct();
+	void test_struct_disk();
+	void test_multiple();
+};
+
+#endif // TESTQPREFCLOUDSTORAGE_H
