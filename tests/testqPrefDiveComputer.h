17c7c477dc4a555788f6593b68eb3b974e5e5726
jan Iversen <jani@apache.org>
2018-07-22 17:02:26 +0200

    tests: add qPrefDiveComputer testcases
    add test cases to secure struct preferences and qPrefDiveComputer work together
    
    Signed-off-by: Jan Iversen <jani@apache.org>

---
+++ tests/testqPrefDiveComputer.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef TESTQPREFDIVECOMPUTER_H
+#define TESTQPREFDIVECOMPUTER_H
+
+#include <QObject>
+
+class TestQPrefDiveComputer : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void initTestCase();
+	void test_struct_get();
+	void test_set_struct();
+	void test_set_load_struct();
+	void test_struct_disk();
+	void test_multiple();
+};
+
+#endif // TESTQPREFDIVECOMPUTER_H
