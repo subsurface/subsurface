711b96a143165bf991c69193c0e65f88155e4ba4
jan Iversen <jani@apache.org>
2018-07-23 18:50:08 +0200

    tests: move Facebook test from testpreferences
    Remove Facebook test in testpreferences
    add the same Facebook tests to testqPrefFacebook
    
    Signed-off-by: Jan Iversen <jani@apache.org>

--- tests/testpreferences.cpp
+++ tests/testpreferences.cpp
@@ -160,23 +160,6 @@ void TestPreferences::testPreferences()
 	TEST(pp->po2ThresholdMin(), 4.0);
 	TEST(pp->po2ThresholdMax(), 5.0);
 
-	auto fb = qPrefFacebook::instance();
-	fb->set_access_token("rand-access-token");
-	fb->set_user_id("tomaz-user-id");
-	fb->set_album_id("album-id");
-
-	TEST(fb->access_token(),QStringLiteral("rand-access-token"));
-	TEST(fb->user_id(),     QStringLiteral("tomaz-user-id"));
-	TEST(fb->album_id(),    QStringLiteral("album-id"));
-
-	fb->set_access_token("rand-access-token-2");
-	fb->set_user_id("tomaz-user-id-2");
-	fb->set_album_id("album-id-2");
-
-	TEST(fb->access_token(),QStringLiteral("rand-access-token-2"));
-	TEST(fb->user_id(),     QStringLiteral("tomaz-user-id-2"));
-	TEST(fb->album_id(),    QStringLiteral("album-id-2"));
-
 	auto geo = pref->geocoding;
 	geo->setFirstTaxonomyCategory(TC_NONE);
 	geo->setSecondTaxonomyCategory(TC_OCEAN);
