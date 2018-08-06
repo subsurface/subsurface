3174c7156371f64efedcf19281ea0dc565f23bf8
jan Iversen <jani@apache.org>
2018-08-06 17:50:08 +0200

    tests: add qPrefLocationService testcases
    add test cases to secure struct preferences and qPrefLocationService work together
    
    Signed-off-by: Jan Iversen <jani@apache.org>

---
+++ tests/testqPrefLocationService.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef TESTQPREFLOCATIONSERVICE_H
+#define TESTQPREFLOCATIONSERVICE_H
+
+#include <QObject>
+
+class TestQPrefLocationService : public QObject {
+	Q_OBJECT
+
+private slots:
+	void initTestCase();
+	void test_struct_get();
+	void test_set_struct();
+	void test_set_load_struct();
+	void test_struct_disk();
+	void test_multiple();
+};
+
+#endif // TESTQPREFLOCATIONSERVICE_H
