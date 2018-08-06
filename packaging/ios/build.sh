7b4d661ff2127de7adc6b46521687d178439365d
Dirk Hohndel <dirk@hohndel.org>
2018-08-05 17:31:55 -0700

    iOS: quiet verbose warnings for libzip
    And don't try to build manuals, regression tests and examples, while we are at
    it.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- packaging/ios/build.sh
+++ packaging/ios/build.sh
@@ -150,6 +150,9 @@ for ARCH in $ARCHS; do
 	fi
 
 	if [ ! -e $PKG_CONFIG_LIBDIR/libzip.pc ] ; then
+		pushd ${SSRF_CLONE}/libzip
+		# don't waste time on building command line tools, examples, manual, and regression tests - and don't build the BZIP2 support we don't need
+		sed -i.bak 's/ADD_SUBDIRECTORY(src)//;s/ADD_SUBDIRECTORY(examples)//;s/ADD_SUBDIRECTORY(man)//;s/ADD_SUBDIRECTORY(regress)//;s/FIND_PACKAGE(BZip2)/# FIND_PACKAGE(BZip2)/' CMakeLists.txt
 		mkdir -p build-ios/libzip-build-$ARCH_NAME
 		pushd build-ios/libzip-build-$ARCH_NAME
 		cmake -DBUILD_SHARED_LIBS="OFF" \
@@ -157,8 +160,12 @@ for ARCH in $ARCHS; do
 			-DCMAKE_INSTALL_PREFIX=${PREFIX} \
 			-DCMAKE_PREFIX_PATH=${PREFIX} \
 			${SSRF_CLONE}/libzip
+		# quiet the super noise warnings
+		sed -i.bak 's/C_FLAGS = /C_FLAGS = -Wno-nullability-completeness -Wno-expansion-to-defined /' lib/CMakeFiles/zip.dir/flags.make
 		make
 		make install
+		popd
+		mv CMakeLists.txt.bak CMakeLists.txt
 		popd
 	fi
 
