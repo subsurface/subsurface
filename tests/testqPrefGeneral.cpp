91ec0fe8d40d619c32fab30c45565a7a2f24a749
jan Iversen <jani@apache.org>
2018-08-13 22:12:06 +0200

    tests: move General test from testpreferences
    Remove General test in testpreferences
    add the same General tests to testqPrefGeneral
    
    Signed-off-by: Jan Iversen <jani@apache.org>

--- tests/testqPrefGeneral.cpp
+++ tests/testqPrefGeneral.cpp
@@ -181,4 +181,49 @@ void TestQPrefGeneral::test_multiple()
 	QCOMPARE(tst_direct->pscr_ratio(), 18);
 }
 
+#define TEST(METHOD, VALUE)      \
+	QCOMPARE(METHOD, VALUE); \
+	general->sync();           \
+	general->load();           \
+	QCOMPARE(METHOD, VALUE);
+
+void TestQPrefGeneral::test_oldPreferences()
+{
+	auto general = qPrefGeneral::instance();
+
+	general->set_default_filename("filename");
+	general->set_default_cylinder("cylinder_2");
+	general->set_default_file_behavior(LOCAL_DEFAULT_FILE);
+	general->set_defaultsetpoint(0);
+	general->set_o2consumption(0);
+	general->set_pscr_ratio(0);
+	general->set_use_default_file(true);
+
+	TEST(general->default_filename(), QStringLiteral("filename"));
+	TEST(general->default_cylinder(), QStringLiteral("cylinder_2"));
+	TEST(general->default_file_behavior(), LOCAL_DEFAULT_FILE); // since we have a default file, here it returns
+	TEST(general->defaultsetpoint(), 0);
+	TEST(general->o2consumption(), 0);
+	TEST(general->pscr_ratio(), 0);
+	TEST(general->use_default_file(), true);
+
+	general->set_default_filename("no_file_name");
+	general->set_default_cylinder("cylinder_1");
+	//TODOl: Change this to a enum.
+	general->set_default_file_behavior(CLOUD_DEFAULT_FILE);
+
+	general->set_defaultsetpoint(1);
+	general->set_o2consumption(1);
+	general->set_pscr_ratio(1);
+	general->set_use_default_file(false);
+
+	TEST(general->default_filename(), QStringLiteral("no_file_name"));
+	TEST(general->default_cylinder(), QStringLiteral("cylinder_1"));
+	TEST(general->default_file_behavior(), CLOUD_DEFAULT_FILE);
+	TEST(general->defaultsetpoint(), 1);
+	TEST(general->o2consumption(), 1);
+	TEST(general->pscr_ratio(), 1);
+	TEST(general->use_default_file(), false);
+}
+
 QTEST_MAIN(TestQPrefGeneral)
