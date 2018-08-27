e491b789f4d6d0be0c3f9d4dab0085a7af4d590b
Jan Mulder <jlmulder@xs4all.nl>
2018-08-27 21:09:40 +0200

    Show new firmware notice for OSTC Plus
    Also show a new firmware notice for a HW OSTC Plus. This appeared
    to be broken (as can be verified on the current master from today,
    as HW just released version 2.98 of the firmware).
    
    Notice that there is some confusion in both Subsurface and
    Libdivecomputer with respect to types of OSTCs. Physcically,
    there exist 2 type of OSTC3's. The first edition with
    only DC_TRANSPORT_SERIAL (and an USB connector), and a second
    version that lacks the connector but supports both BT and BLE.
    Confusingly, the second version is marked OSTC3 on the device,
    but the user needs to select OSTC Plus (which is basically a 3rd
    version of the OSTC3, combined with a successor of the OSTC Sport)
    to download dives using BT/BLE.
    
    Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

--- desktop-widgets/downloadfromdivecomputer.cpp
+++ desktop-widgets/downloadfromdivecomputer.cpp
@@ -331,7 +331,7 @@ void DownloadFromDCWidget::on_downloadCancelRetryButton_clicked()
 	//
 	// We shouldn't do this for memory dumps.
 	if ((product == "OSTC 3" || product == "OSTC 3+" || product == "OSTC cR" ||
-	     product == "OSTC Sport" || product == "OSTC 4") &&
+	     product == "OSTC Sport" || product == "OSTC 4" || product == "OSTC Plus") &&
 	    !data->saveDump()) {
 		ostcFirmwareCheck = new OstcFirmwareCheck(product);
 	}
