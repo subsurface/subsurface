26d8beb7668d88124a954c163de9b1ce1b082c39
Dirk Hohndel <dirk@hohndel.org>
2018-09-15 07:27:07 -0700

    build-system: don't modify a file under git control
    Instead of editing appdata/subsurface.appdata.xml in place, switch to our usual
    pattern of modifying a .in file and add the resulting file to .gitignore.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/add-version-to-appdata.sh
+++ scripts/add-version-to-appdata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 version=$(git describe --abbrev=12)
 date=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d)
-sed -e "s|<release version=\"\" date=\"\" />|<release version=\"$version\" date=\"$date\" />|" -i appdata/subsurface.appdata.xml
+sed -e "s|<release version=\"\" date=\"\" />|<release version=\"$version\" date=\"$date\" />|" appdata/subsurface.appdata.xml.in > appdata/subsurface.appdata.xml
