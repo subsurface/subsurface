subsurface/subsurface

commit 504e9125126cb24467ee8e038a461d731672c4b9

core: connect qPref..::time_thresholdChanged to setGpsTimeThreshold
Automate calling of setGpsTimeThreshold when qPref..::time_threshold changes
and thus avoiding the need to call setGpsTimeThreshold directly.

Signed-off-by: Jan Iversen <jani@apache.org>

core/gpslocation.cpp
@@ -3,6 +3,7 @@
 #include "qt-models/gpslistmodel.h"
 #include "core/pref.h"
 #include "core/qthelper.h"
+#include "core/settings/qPrefLocationService.h"
 #include <time.h>
 #include <unistd.h>
 #include <QDebug>
@@ -36,6 +37,9 @@ GpsLocation::GpsLocation(void (*showMsgCB)(const char *), QObject *parent) :
 	userAgent = getUserAgent();
 	(void)getGpsSource();
 	loadFromStorage();
+
+	// register changes in time threshold
+	connect(qPrefLocationService::instance(), SIGNAL(qPrefLocationService::time_thresholdChanged()), this, SLOT(setGpsTimeThreshold(int seconds)));
 }
 
 GpsLocation *GpsLocation::instance()

commit 9071dd42f2f37d75220577b1bd2a9851f8a91e0a

core: fix connect call
Fix multiple run-time errors in connect call introduced in 504e9125126.
1) Set the proper signature of the signal. 2) make the used slot
a real slot (so move it to the proper section in the header) and
3) set the proper signature for the slot.

Highly unlikely that normal users notice the runtime errors and
possibly unwantend behavior, as this all deals with the subtile GPS
service update threshold.

Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

core/gpslocation.cpp
@@ -39,7 +39,7 @@ GpsLocation::GpsLocation(void (*showMsgCB)(const char *), QObject *parent) :
 	loadFromStorage();
 
 	// register changes in time threshold
-	connect(qPrefLocationService::instance(), SIGNAL(qPrefLocationService::time_thresholdChanged()), this, SLOT(setGpsTimeThreshold(int seconds)));
+	connect(qPrefLocationService::instance(), SIGNAL(time_thresholdChanged(int)), this, SLOT(setGpsTimeThreshold(int)));
 }
 
 GpsLocation *GpsLocation::instance()

core/gpslocation.h
@@ -32,7 +32,6 @@ public:
 	int getGpsNum() const;
 	bool hasLocationsSource();
 	QString currentPosition();
-	void setGpsTimeThreshold(int seconds);
 
 	QMap<qint64, gpsTracker> currentGPSInfo() const;
 
@@ -66,6 +65,7 @@ public slots:
 	void updateTimeout();
 	void positionSourceError(QGeoPositionInfoSource::Error error);
 	void postError(QNetworkReply::NetworkError error);
+	void setGpsTimeThreshold(int seconds);
 #ifdef SUBSURFACE_MOBILE
 	void clearGpsData();
 #endif