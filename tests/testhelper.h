e730608c4516af09119a87461c71653e709f9587
Dirk Hohndel <dirk@hohndel.org>
2018-09-24 17:24:15 -0700

    tests: add test for the BT/BLE address recognition
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ tests/testhelper.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef TESTPARSE_H
+#define TESTPARSE_H
+
+#include <QtTest>
+#include <sqlite3.h>
+
+class TestHelper : public QObject {
+	Q_OBJECT
+private slots:
+	void initTestCase();
+	void recognizeBtAddress();
+};
+
+#endif
