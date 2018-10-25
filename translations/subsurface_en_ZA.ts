765a5277671aa2971cfa2978d5ad2d7afd715e1c
Dirk Hohndel <dirk@hohndel.org>
2018-10-25 15:21:27 -0700

    Translations: add South African English
    This way we get plurals correctly displayed.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ translations/subsurface_en_ZA.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en_ZA" version="2.1">
+<context>
+    <name>CheckCloudConnection</name>
+    <message numerus="yes">
+        <location filename="../core/checkcloudconnection.cpp" line="58"/>
+        <source>Waiting for cloud connetion (%n second(s) passed)</source>
+        <translation><numerusform>Waiting for cloud connetion (%n second passed)</numerusform><numerusform>Waiting for cloud connetion (%n seconds passed)</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>DiveObjectHelper</name>
+    <message numerus="yes">
+        <location filename="../core/subsurface-qt/DiveObjectHelper.cpp" line="336"/>
+        <source>(%n dive(s))</source>
+        <translation><numerusform>(%n dive)</numerusform><numerusform>(%n dives)</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>LocationFilterDelegate</name>
+    <message numerus="yes">
+        <location filename="../desktop-widgets/modeldelegates.cpp" line="504"/>
+        <source>, %n dive(s) here)</source>
+        <translation><numerusform>, %n dive here)</numerusform><numerusform>, %n dives here)</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message numerus="yes">
+        <location filename="../core/qthelper.cpp" line="997"/>
+        <source>(%n dive(s))</source>
+        <translation><numerusform>(%n dive)</numerusform><numerusform>(%n dives)</numerusform></translation>
+    </message>
+</context>
+</TS>
