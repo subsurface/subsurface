bc7450ad35d1c5785dabcd1cc028384f7d42c3b0
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2018-12-09 12:31:31 +0100

    Cleanup: remove remove_autogen_trips() function
    In f427226b3b605523bc8285dbdaaa7f6993af6e6a autogrouping / removal
    of autogrouping was moved into the undo-machinery. This made the
    remove_autogen_trips() function caller-less. Remove it.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/divelist.h
+++ core/divelist.h
@@ -14,7 +14,6 @@ struct dive;
 extern void update_cylinder_related_info(struct dive *);
 extern void mark_divelist_changed(bool);
 extern int unsaved_changes(void);
-extern void remove_autogen_trips(void);
 extern int init_decompression(struct deco_state *ds, struct dive *dive);
 
 /* divelist core logic functions */
