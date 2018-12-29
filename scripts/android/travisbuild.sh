e39e0816e731a84b14b9aa877cf5c29bf95a1180
Jan Mulder <jlmulder@xs4all.nl>
2018-12-29 08:56:08 +0100

    Android Docker: give the android builder its own name
    Users that use docker locally for Windows style build and Android style
    builds will (probably) not like that we use the same name for both
    docker containers. So, give the android builder its own name.
    
    Signed-off-by: Jan Mulder <jlmulder@xs4all.nl>

--- scripts/android/travisbuild.sh
+++ scripts/android/travisbuild.sh
@@ -6,6 +6,6 @@ set -e
 # by running the build wrapper again we should be able to test newer
 # versions of the dependencies even without updating the docker image
 # (but of course having the right things in place will save a ton of time)
-docker exec -e TRAVIS="$TRAVIS" -t builder subsurface/packaging/android/android-build-wrapper.sh
+docker exec -e TRAVIS="$TRAVIS" -t android-builder subsurface/packaging/android/android-build-wrapper.sh
 
 ls -l ../subsurface-mobile-build-arm*/build/outputs/apk/
