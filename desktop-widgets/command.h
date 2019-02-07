a9bd0690fe1d6bd841b0ede2ac53b280ef907b93
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-02-07 21:00:09 +0100

    Undo: implement undo of buddy editing
    This was mostly trivial by reusing the code for tag-editing.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/command.h
+++ desktop-widgets/command.h
@@ -62,6 +62,7 @@ void editWaterTemp(const QVector<dive *> dives, int newValue, int oldValue);
 void editDiveSite(const QVector<dive *> dives, struct dive_site *newValue, struct dive_site *oldValue);
 void editDiveSiteNew(const QVector<dive *> dives, const QString &newName, struct dive_site *oldValue);
 void editTags(const QVector<dive *> &dives, const QStringList &newList, struct dive *d);
+void editBuddies(const QVector<dive *> &dives, const QStringList &newList, struct dive *d);
 
 } // namespace Command
 
