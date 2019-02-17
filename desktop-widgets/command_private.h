03dbacff581d968a4ec9096f31a1de8a0d99e6d7
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-02-17 10:18:00 +0100

    Undo: move selection function into command_private.cpp file
    Undo of editing should probably also restore the old selection and
    current dive. Therefore, move the functions that set and restore the
    selection and the current dive from the command_divelist.cpp into the
    command_private.cpp translation unit.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/command_private.h
+++ desktop-widgets/command_private.h
@@ -4,12 +4,11 @@
 #ifndef COMMAND_PRIVATE_H
 #define COMMAND_PRIVATE_H
 
+#include "core/dive.h"
+
 #include <vector>
 #include <utility>
 #include <QVector>
-
-struct dive;
-struct dive_trip;
 
 namespace Command {
 
@@ -55,6 +54,14 @@ void processByTrip(Vector &divesIn, Function action)
 	processByTrip(dives, action);
 
 }
+
+// Reset the selection to the dives of the "selection" vector and send the appropriate signals.
+// Set the current dive to "currentDive". "currentDive" must be an element of "selection" (or
+// null if "seletion" is empty). Return true if the selection or current dive changed.
+bool setSelection(const std::vector<dive *> &selection, dive *currentDive);
+
+// Get currently selectd dives
+std::vector<dive *> getDiveSelection();
 
 } // namespace Command
 
