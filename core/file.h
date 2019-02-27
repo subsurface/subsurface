18e7481188b56de9202cb2245223157fd738d821
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-02-27 21:10:26 +0100

    Cleanup: put all function declarations in file.h in extern "C"
    Some of the functions declarations were not in 'extern "C"',
    despite being C functions. This worked only because they weren't
    called from C++. Nevertheless, it seems like a dangerous proposition
    to have the same function declared once as C and once as C++.
    Therefore, always put them in extern "C" (if compiling in C++ mode,
    evidently).
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/file.h
+++ core/file.h
@@ -7,14 +7,14 @@ struct memblock {
 	size_t size;
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern int try_to_open_cochran(const char *filename, struct memblock *mem, struct dive_table *table, struct trip_table *trips);
 extern int try_to_open_liquivision(const char *filename, struct memblock *mem, struct dive_table *table, struct trip_table *trips);
 extern int datatrak_import(struct memblock *mem, struct dive_table *table, struct trip_table *trips);
 extern void ostctools_import(const char *file, struct dive_table *table, struct trip_table *trips);
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 extern int readfile(const char *filename, struct memblock *mem);
 extern int try_to_open_zip(const char *filename, struct dive_table *table, struct trip_table *trips);
 #ifdef __cplusplus
