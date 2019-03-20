cf666c861d9b261b4e728324653a4e501f2a5f2b
Dirk Hohndel <dirk@hohndel.org>
2019-03-20 07:31:24 -0700

    Core: simplify ConnectionListModel
    The complicated setup with the AddressRole is unnecessary. All we want to be
    able to do is get the index of a specific text in the list. In hindsight I am
    puzzled why I implemented this in such a complex fashion.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- core/connectionlistmodel.h
+++ core/connectionlistmodel.h
@@ -6,13 +6,8 @@
 class ConnectionListModel : public QAbstractListModel {
 	Q_OBJECT
 public:
-	enum CLMRole {
-		AddressRole = Qt::UserRole + 1
-	};
 	ConnectionListModel(QObject *parent = 0);
-	QHash<int, QByteArray> roleNames() const;
-	QVariant data(const QModelIndex &index, int role = AddressRole) const;
-	QString address(int idx) const;
+	QVariant data(const QModelIndex &index, int role) const;
 	int rowCount(const QModelIndex &parent = QModelIndex()) const;
 	void addAddress(const QString address);
 	void removeAllAddresses();
