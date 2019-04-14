88dc32fdfcb7cb63b0e47280d1f38395077df757
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-04-14 15:37:19 +0200

    Core: turn add_single_dive() to append_dive()
    The only external caller of add_single_dive() used it to append a
    dive to the global dive list. Rename the function accordingly and
    remove the index parameter.
    
    The internal caller can use the local insert_dive() function, which
    doesn't consider selection. That shouldn't be a problem, as the
    caller is doing import.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/divelist.h
+++ core/divelist.h
@@ -34,7 +34,7 @@ extern char *get_dive_gas_string(const struct dive *dive);
 extern struct dive **grow_dive_table(struct dive_table *table);
 extern int dive_table_get_insertion_index(struct dive_table *table, struct dive *dive);
 extern void add_to_dive_table(struct dive_table *table, int idx, struct dive *dive);
-extern void add_single_dive(int idx, struct dive *dive);
+extern void append_dive(struct dive *dive);
 extern void get_dive_gas(const struct dive *dive, int *o2_p, int *he_p, int *o2low_p);
 extern int get_divenr(const struct dive *dive);
 extern struct dive_trip *unregister_dive_from_trip(struct dive *dive);
