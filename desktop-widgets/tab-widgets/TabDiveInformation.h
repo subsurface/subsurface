26edea7f71bb1726cebe3955494ab049f337ecda
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-04-26 10:03:32 +0200

    Desktop: automatically update dive information tab
    Currently, the dive information tab was not updated when the user
    edited fields. The fields were only updated when switching between
    dives.
    
    Therefore, hook into the "divesChanged" signal and update the fields
    accordingly.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/tab-widgets/TabDiveInformation.h
+++ desktop-widgets/tab-widgets/TabDiveInformation.h
@@ -3,6 +3,7 @@
 #define TAB_DIVE_INFORMATION_H
 
 #include "TabBase.h"
+#include "core/subsurface-qt/DiveListNotifier.h"
 
 namespace Ui {
 	class TabDiveInformation;
@@ -15,10 +16,12 @@ public:
 	~TabDiveInformation();
 	void updateData() override;
 	void clear() override;
-
+private slots:
+	void divesChanged(dive_trip *trip, const QVector<dive *> &dives, DiveField field);
 private:
 	Ui::TabDiveInformation *ui;
+	void updateProfile();
+	void updateWhen();
 };
-
 
 #endif
