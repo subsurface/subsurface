705362ef63d016b319c4d7349a777bf3f1bd5db0
Dirk Hohndel <dirk@hohndel.org>
2019-06-24 13:58:00 +0800

    Desktop: update flags on the map when dive sites change
    Now when we change dive site location or name through a redo, the flags and
    associated name are always reflected correctly.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- desktop-widgets/locationinformation.cpp
+++ desktop-widgets/locationinformation.cpp
@@ -133,6 +133,7 @@ void LocationInformationWidget::diveSiteChanged(struct dive_site *ds, int field)
 	switch (field) {
 	case LocationInformationModel::NAME:
 		ui.diveSiteName->setText(diveSite->name);
+		MapWidget::instance()->repopulateLabels();
 		return;
 	case LocationInformationModel::DESCRIPTION:
 		ui.diveSiteDescription->setText(diveSite->description);
@@ -153,6 +154,7 @@ void LocationInformationWidget::diveSiteChanged(struct dive_site *ds, int field)
 			enableLocationButtons(false);
 			ui.diveSiteCoordinates->clear();
 		}
+		MapWidget::instance()->repopulateLabels();
 	default:
 		return;
 	}
