4e71c7af7c401c5c9b6cc6367c5ff3d1ee8de0a2
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-07-15 20:41:42 +0200

    Cleanup: free plot data on exit
    Some widgets copy the full plot info. Free these data on exit to
    prevent monstrous valgrind reports.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- qt-models/diveplotdatamodel.h
+++ qt-models/diveplotdatamodel.h
@@ -73,6 +73,7 @@ public:
 		COLUMNS
 	};
 	explicit DivePlotDataModel(QObject *parent = 0);
+	~DivePlotDataModel();
 	int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
