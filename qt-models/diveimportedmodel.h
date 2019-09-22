4d5acc84612e3536b4665edb75aa5f074f2cad1c
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-09-22 22:07:13 +0200

    Import: remove DiveImportedModel::lastIndex
    This is redundant, as the actual size is stored in the dive table.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- qt-models/diveimportedmodel.h
+++ qt-models/diveimportedmodel.h
@@ -44,7 +44,6 @@ signals:
 	void downloadFinished();
 
 private:
-	int lastIndex;
 	std::vector<char> checkStates; // char instead of bool to avoid silly pessimization of std::vector.
 	struct dive_table diveTable;
 	struct dive_site_table sitesTable;
