subsurface/subsurface

commit e63257f0e96797aadfc6f8081b4795386c72c402

Mobile: allow message argument to showBusy function
When called without arguments, no message is shown.

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

mobile-widgets/qml/main.qml
@@ -66,7 +66,9 @@ Kirigami.ApplicationWindow {
 		anchors.centerIn: parent
 	}
 
-	function showBusy() {
+	function showBusy(msg = "") {
+		if (msg !== "")
+			showPassiveNotification(msg, 15000) // show for 15 seconds
 		busy.running = true
 	}
 
@@ -77,6 +79,7 @@ Kirigami.ApplicationWindow {
 
 	function hideBusy() {
 		busy.running = false
+		showPassiveNotification("", 10) // this hides a notification messssage that's still shown
 	}
 
 	function hideBusyAndConnectModel() { // this is used by QMLManager when done filtering

commit 1a5c7f1048e3d85c9450f05685ea542761626015

mobile-widget/qml: correct syntax error introduced in e63257f0e
default parameter values is not supported as this syntax was introduced
in ECMA-262 6th edition while QML only implements the fifth edition
(as of Qt 5.11).

remove default parameter and check for undefined instead.

Signed-off-by: Jan Iversen <jan@casacondor.com>

mobile-widgets/qml/main.qml
@@ -66,8 +66,8 @@ Kirigami.ApplicationWindow {
 		anchors.centerIn: parent
 	}
 
-	function showBusy(msg = "") {
-		if (msg !== "")
+	function showBusy(msg) {
+		if (msg !== undefined)
 			showPassiveNotification(msg, 15000) // show for 15 seconds
 		busy.running = true
 	}