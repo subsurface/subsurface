b4eef5548b593956a4a24da43641b9eed7906efa
Alexander Kjäll <alexander.kjall@gmail.com>
2019-12-09 21:54:37 +0100

    fixed typo in comment
    Signed-off-by: Alexander Kjäll <alexander.kjall@gmail.com>

--- scripts/windows/travisbuild.sh
+++ scripts/windows/travisbuild.sh
@@ -5,7 +5,7 @@ set -e
 
 # this gets executed by Travis when building an installer for Windows
 # it gets started from inside the subsurface directory
-# with all the other projects downloaded and installed in parralel to
+# with all the other projects downloaded and installed in parallel to
 # subsurface; in order to be compatible with the assumed layout, we
 # need to create the secondary build directory
 cd ${TRAVIS_BUILD_DIR}/..
