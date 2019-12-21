eb3fd5a4af30b8bc82fb811d4746c9e9efe8bbc5
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-12-21 20:21:56 +0100

    mobile/divelist: don't set currentIndex on undo of dive deletion
    The core sets the current dive when undoing something.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- mobile-widgets/qmlmanager.cpp
+++ mobile-widgets/qmlmanager.cpp
@@ -1398,11 +1398,10 @@ void QMLManager::saveChangesCloud(bool forceRemoteSync)
 	git_local_only = glo;
 }
 
-bool QMLManager::undoDelete(int)
+void QMLManager::undoDelete(int)
 {
 	undoAction->activate(QAction::Trigger);
 	changesNeedSaving();
-	return true;
 }
 
 void QMLManager::selectDive(int id)
