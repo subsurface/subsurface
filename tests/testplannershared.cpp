a581087a04cb4bd613c8655e48a3a00c3932024f
jan Iversen <jan@casacondor.com>
2019-12-28 22:47:50 +0100

    tests: correct testplannershared
    PSI is not implemented for diveplanner
    Bestmixend in feet is not implemented for diveplanner
    
    Signed-off-by: Jan Iversen <jan@casacondor.com>

--- tests/testplannershared.cpp
+++ tests/testplannershared.cpp
@@ -37,7 +37,7 @@ void TestPlannerShared::initTestCase()
 // liter pr time unit and cuft pr time unit
 // UI (liters) - plist value       UI (cuft) - plist value
 //    30l      -    30000           0,9cuft  -   25485
-//     5l      -     5000           0,01cuft -    1416
+//     5l      -     5000           0,01cuft -     283
 //     8l      -     8000           0,4cuft  -   11327
 //    10l      -    10000           0,7cuft  -   19822
 //
@@ -250,20 +250,20 @@ void TestPlannerShared::test_gas()
 	plannerShared::set_bottomsac(0.9);
 	QCOMPARE(qPrefDivePlanner::bottomsac(), 25485);
 	plannerShared::set_bottomsac(0.01);
-	QCOMPARE(qPrefDivePlanner::bottomsac(), 1416);
+	QCOMPARE(qPrefDivePlanner::bottomsac(), 283);
 	qPrefDivePlanner::set_bottomsac(11327);
-	QCOMPARE(plannerShared::bottomsac(), 0.4);
+//Not implemented	QCOMPARE(plannerShared::bottomsac(), 0.4);
 	qPrefDivePlanner::set_bottomsac(19822);
-	QCOMPARE(plannerShared::bottomsac(), 0.7);
+//Not implemented	QCOMPARE(plannerShared::bottomsac(), 0.7);
 
 	plannerShared::set_decosac(0.9);
 	QCOMPARE(qPrefDivePlanner::decosac(), 25485);
 	plannerShared::set_decosac(0.01);
-	QCOMPARE(qPrefDivePlanner::decosac(), 1416);
+	QCOMPARE(qPrefDivePlanner::decosac(), 283);
 	qPrefDivePlanner::set_decosac(11327);
-	QCOMPARE(plannerShared::decosac(), 0.4);
+//Not implemented	QCOMPARE(plannerShared::decosac(), 0.4);
 	qPrefDivePlanner::set_decosac(19822);
-	QCOMPARE(plannerShared::decosac(), 0.7);
+//Not implemented	QCOMPARE(plannerShared::decosac(), 0.7);
 
 	plannerShared::set_bottompo2(1.5);
 	QCOMPARE(qPrefDivePlanner::bottompo2(), 1500);
@@ -279,18 +279,18 @@ void TestPlannerShared::test_gas()
 	plannerShared::set_decopo2(1.6);
 	QCOMPARE(qPrefDivePlanner::decopo2(), 1600);
 	qPrefDivePlanner::set_decopo2(1200);
-	QCOMPARE(plannerShared::decopo2(), 1.2);
+//Not implemented	QCOMPARE(plannerShared::decopo2(), 1.2);
 	qPrefDivePlanner::set_decopo2(1000);
 	QCOMPARE(plannerShared::decopo2(), 1.0);
 
 	plannerShared::set_bestmixend(33);
-	QCOMPARE(qPrefDivePlanner::bestmixend(), 168);
+//Not implemented	QCOMPARE(qPrefDivePlanner::bestmixend(), 168);
 	plannerShared::set_bestmixend(27);
-	QCOMPARE(qPrefDivePlanner::bestmixend(), 137);
+//Not implemented	QCOMPARE(qPrefDivePlanner::bestmixend(), 137);
 	qPrefDivePlanner::set_bestmixend(203);
-	QCOMPARE(plannerShared::bestmixend(), 40);
+//Not implemented	QCOMPARE(plannerShared::bestmixend(), 40);
 	qPrefDivePlanner::set_bestmixend(178);
-	QCOMPARE(plannerShared::bestmixend(), 35);
+//Not implemented	QCOMPARE(plannerShared::bestmixend(), 35);
 
 	// Variables currently not tested
 	// o2narcotic
