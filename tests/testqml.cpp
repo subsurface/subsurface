07e027d5e732943ceeabdee92f3aa75017caaca2
jan Iversen <jan@casacondor.com>
2019-12-30 20:36:55 +0100

    tests: rewrite testqml use standard Qt Macros.
    The current required level of Qt mobile builds, allows
    to use the standard macros.
    
    Rewrite testqml.cpp.
    
    Signed-off-by: Jan Iversen <jan@casacondor.com>

--- tests/testqml.cpp
+++ tests/testqml.cpp
@@ -1,30 +1,29 @@
 // SPDX-License-Identifier: GPL-2.0
-#include "testqml.h"
+#include <QtQuickTest>
+#include <QQmlEngine>
+#include <QQmlContext>
 #include "core/settings/qPref.h"
 
-#include <QtQuickTest>
 
-// main loosely copied from QUICK_TEST_MAIN_WITH_SETUP macro
-int main(int argc, char **argv)
+class TestQMLSetup : public QObject
 {
-//#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
-#ifndef THIS_IS_REPAIRED
-	return 0;
-#else
-	QTEST_ADD_GPU_BLACKLIST_SUPPORT
-	QTEST_SET_MAIN_SOURCE_PATH
-	QMLTestSetup setup;
+	Q_OBJECT
 
-	// register C++ types and classes (but not objects)
-	qPref::registerQML(NULL);
+public:
+	TestQMLSetup() {}
 
-	return quick_test_main_with_setup(argc, argv, "TestQML", nullptr, &setup);
-#endif //QT_VERSION
-}
-
-void QMLTestSetup::qmlEngineAvailable(QQmlEngine *engine)
-{
-	// register C++ objects (but not types and classes)
-	qPref::registerQML(engine);
+public slots:
+//	void applicationAvailable()
+//	{
+//		// register C++ types and classes (but not objects)
+//	}
+	void qmlEngineAvailable(QQmlEngine *engine)
+	{
+		// register C++ objects
+		qPref::registerQML(NULL);
+	}
 };
 
+QUICK_TEST_MAIN_WITH_SETUP(TestQML, TestQMLSetup)
+
+#include "testqml.moc"
