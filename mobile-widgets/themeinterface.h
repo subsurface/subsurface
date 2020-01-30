2c0a5992a1f044a5b6a5a0d84434d4a1fecf0bfd
jan Iversen <jan@casacondor.com>
2020-01-30 18:05:47 +0100

    mobile-widgets/themeinterface: clean theme colors
    Theme colors are no longer public, so make them local const.
    
    Signed-off-by: jan Iversen <jan@casacondor.com>

--- mobile-widgets/themeinterface.h
+++ mobile-widgets/themeinterface.h
@@ -35,46 +35,6 @@ class themeInterface : public QObject {
 	Q_PROPERTY(QString currentTheme MEMBER m_currentTheme WRITE set_currentTheme NOTIFY currentThemeChanged)
 	Q_PROPERTY(QString iconStyle MEMBER m_iconStyle NOTIFY iconStyleChanged)
 
-	// Compatibility existing code
-	Q_PROPERTY(QColor blueBackgroundColor MEMBER m_blueBackgroundColor CONSTANT)
-	Q_PROPERTY(QColor blueContrastAccentColor MEMBER m_blueTextColor CONSTANT)
-	Q_PROPERTY(QColor blueDarkerPrimaryColor MEMBER m_blueDarkerPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor blueDarkerPrimaryTextColor MEMBER m_blueDarkerPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor blueDrawerColor MEMBER m_blueDrawerColor CONSTANT)
-	Q_PROPERTY(QColor blueLightDrawerColor MEMBER m_blueLightDrawerColor CONSTANT)
-	Q_PROPERTY(QColor blueLightPrimaryColor MEMBER m_blueLightPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor blueLightPrimaryTextColor MEMBER m_blueLightPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor bluePrimaryColor MEMBER m_bluePrimaryColor CONSTANT)
-	Q_PROPERTY(QColor bluePrimaryTextColor MEMBER m_bluePrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor blueSecondaryTextColor MEMBER m_blueSecondaryTextColor CONSTANT)
-	Q_PROPERTY(QColor blueTextColor MEMBER m_blueTextColor CONSTANT)
-
-	Q_PROPERTY(QColor pinkBackgroundColor MEMBER m_pinkBackgroundColor CONSTANT)
-	Q_PROPERTY(QColor pinkContrastAccentColor MEMBER m_pinkContrastAccentColor CONSTANT)
-	Q_PROPERTY(QColor pinkDarkerPrimaryColor MEMBER m_blueDarkerPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor pinkDarkerPrimaryTextColor MEMBER m_blueDarkerPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor pinkDrawerColor MEMBER m_pinkDrawerColor CONSTANT)
-	Q_PROPERTY(QColor pinkLightDrawerColor MEMBER m_pinkLightDrawerColor CONSTANT)
-	Q_PROPERTY(QColor pinkLightPrimaryColor MEMBER m_blueLightPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor pinkLightPrimaryTextColor MEMBER m_blueLightPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor pinkPrimaryColor MEMBER m_pinkPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor pinkPrimaryTextColor MEMBER m_pinkPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor pinkSecondaryTextColor MEMBER m_blueSecondaryTextColor CONSTANT)
-	Q_PROPERTY(QColor pinkTextColor MEMBER m_pinkTextColor CONSTANT)
-
-	Q_PROPERTY(QColor darkBackgroundColor MEMBER m_darkBackgroundColor CONSTANT)
-	Q_PROPERTY(QColor darkContrastAccentColor MEMBER m_darkContrastAccentColor CONSTANT)
-	Q_PROPERTY(QColor darkDarkerPrimaryColor MEMBER m_blueDarkerPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor darkDarkerPrimaryTextColor MEMBER m_blueDarkerPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor darkDrawerColor MEMBER m_drawerColor CONSTANT)
-	Q_PROPERTY(QColor darkLightDrawerColor MEMBER m_darkLightDrawerColor CONSTANT)
-	Q_PROPERTY(QColor darkLightPrimaryColor MEMBER m_blueLightPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor darkLightPrimaryTextColor MEMBER m_blueLightPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor darkPrimaryColor MEMBER m_darkPrimaryColor CONSTANT)
-	Q_PROPERTY(QColor darkPrimaryTextColor MEMBER m_darkPrimaryTextColor CONSTANT)
-	Q_PROPERTY(QColor darkSecondaryTextColor MEMBER m_blueSecondaryTextColor CONSTANT)
-	Q_PROPERTY(QColor darkTextColor MEMBER m_darkTextColor CONSTANT)
-
 public:
 	static themeInterface *instance();
 
@@ -135,45 +95,5 @@ private:
 
 	static QString m_currentTheme;
 	static QString m_iconStyle;
-
-	// Compatibility existing code
-	static const QColor m_blueBackgroundColor;
-	static const QColor m_blueContrastAccentColor;
-	static const QColor m_blueDarkerPrimaryColor;
-	static const QColor m_blueDarkerPrimaryTextColor;
-	static const QColor m_blueDrawerColor;
-	static const QColor m_blueLightDrawerColor;
-	static const QColor m_blueLightPrimaryColor;
-	static const QColor m_blueLightPrimaryTextColor;
-	static const QColor m_bluePrimaryColor;
-	static const QColor m_bluePrimaryTextColor;
-	static const QColor m_blueSecondaryTextColor;
-	static const QColor m_blueTextColor;
-
-	static const QColor m_pinkBackgroundColor;
-	static const QColor m_pinkContrastAccentColor;
-	static const QColor m_pinkDarkerPrimaryColor;
-	static const QColor m_pinkDarkerPrimaryTextColor;
-	static const QColor m_pinkDrawerColor;
-	static const QColor m_pinkLightDrawerColor;
-	static const QColor m_pinkLightPrimaryColor;
-	static const QColor m_pinkLightPrimaryTextColor;
-	static const QColor m_pinkPrimaryColor;
-	static const QColor m_pinkPrimaryTextColor;
-	static const QColor m_pinkSecondaryTextColor;
-	static const QColor m_pinkTextColor;
-
-	static const QColor m_darkBackgroundColor;
-	static const QColor m_darkContrastAccentColor;
-	static const QColor m_darkDarkerPrimaryColor;
-	static const QColor m_darkDarkerPrimaryTextColor;
-	static const QColor m_darkDrawerColor;
-	static const QColor m_darkLightDrawerColor;
-	static const QColor m_darkLightPrimaryColor;
-	static const QColor m_darkLightPrimaryTextColor;
-	static const QColor m_darkPrimaryColor;
-	static const QColor m_darkPrimaryTextColor;
-	static const QColor m_darkSecondaryTextColor;
-	static const QColor m_darkTextColor;
 };
 #endif
