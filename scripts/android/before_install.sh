33643bb0af0729f79b28cc0398d6990a6537f810
Christof Arnosti <charno@charno.ch>
2020-03-05 22:36:57 +0100

    travis build-scripts: update container
    While the project doesn't use Travis anymore, these scripts are indeed
    useful when building locally, so it makes sense to keep them current and
    working.
    
    Signed-off-by: Christof Arnosti <charno@charno.ch>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/android/before_install.sh
+++ scripts/android/before_install.sh
@@ -19,5 +19,5 @@ docker run -v $PWD:/android/subsurface \
 	   -v $PARENT/subsurface-mobile-build-docker-arm64:/android/subsurface-mobile-build-arm64 \
 	   --name=android-builder \
 	   -w /android \
-	   -d dirkhh/android-builder:5.12.04.1 \
+	   -d subsurface/android-build-container:5.13.10 \
 	   /bin/sleep 60m
