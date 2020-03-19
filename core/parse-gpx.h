eb76bdb3e377b601ea62fc36cb58c14a55f25565
Dirk Hohndel <dirk@hohndel.org>
2020-03-19 08:41:02 -0700

    cleanup: fix mis-spelling of UCT in comments as well
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- core/parse-gpx.h
+++ core/parse-gpx.h
@@ -8,12 +8,12 @@
 struct dive_coords {         // This structure holds important information after parsing the GPX file:
 	time_t start_dive;            // Start time of the current dive, obtained using current_dive (local time)
 	time_t end_dive;              // End time of current dive (local time)
-	time_t start_track;           // Start time of GPX track (UCT)
-	time_t end_track;             // End time of GPX track (UCT)
+	time_t start_track;           // Start time of GPX track (UTC)
+	time_t end_track;             // End time of GPX track (UTC)
 	double lon;                   // Longitude of the first trackpoint after the start of the dive
 	double lat;                   // Latitude of the first trackpoint after the start of the dive
 	int64_t settingsDiff_offset;  // Local time difference between dive computer and GPS equipment
-	int64_t timeZone_offset;      // UCT international time zone offset of dive site
+	int64_t timeZone_offset;      // UTC international time zone offset of dive site
 };
 
 int getCoordsFromGPXFile(dive_coords *coords, QString fileName);
