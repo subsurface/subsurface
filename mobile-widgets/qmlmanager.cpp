f61b2cec0ebc1e1bd108d13afbb47ff674b7f44a
Dirk Hohndel <dirk@hohndel.org>
2020-04-04 14:40:53 -0700

    mobile/startup: fix potential crash when switching back and forth
    If the user switches away from Subsurface-mobile and back while we are
    in our initialization phase, that code might run more than once leading
    to undesirable results.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- mobile-widgets/qmlmanager.cpp
+++ mobile-widgets/qmlmanager.cpp
@@ -300,6 +300,7 @@ QMLManager::QMLManager() : m_locationServiceEnabled(false),
 
 void QMLManager::applicationStateChanged(Qt::ApplicationState state)
 {
+	static bool initializeOnce = false;
 	QString stateText;
 	switch (state) {
 	case Qt::ApplicationActive: stateText = "active"; break;
@@ -313,8 +314,9 @@ void QMLManager::applicationStateChanged(Qt::ApplicationState state)
 	stateText.append((unsaved_changes() ? QLatin1String("") : QLatin1String("no ")) + QLatin1String("unsaved changes"));
 	appendTextToLog(stateText);
 
-	if (state == Qt::ApplicationActive && !m_initialized) {
+	if (state == Qt::ApplicationActive && !m_initialized && !initializeOnce) {
 		// once the app UI is displayed, finish our setup and mark the app as initialized
+		initializeOnce = true;
 		finishSetup();
 		appInitialized();
 	}
