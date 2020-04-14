54ad5e06bb2c27fb907fcf5e476db423ab6c98fd
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-04-14 19:08:49 +0200

    core: add get_picture_idx() function
    A function that gets the index of a picture in a picture table
    given its filename. Since we are going to identify pictures by
    their filename, we will need this function in the undo code.
    
    Use the function in the remove_picture() function.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/picture.h
+++ core/picture.h
@@ -34,6 +34,7 @@ extern void add_to_picture_table(struct picture_table *, int idx, struct picture
 extern void copy_pictures(const struct picture_table *s, struct picture_table *d);
 extern void add_picture(struct picture_table *, struct picture newpic);
 extern bool remove_picture(struct picture_table *, const char *filename);
+extern int get_picture_idx(const struct picture_table *, const char *filename); /* Return -1 if not found */
 
 #ifdef __cplusplus
 }
