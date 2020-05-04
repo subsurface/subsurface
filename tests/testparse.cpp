435778b162dde02db26a71151312824f8413621f
Miika Turkia <miika.turkia@gmail.com>
2020-05-04 08:20:03 +0300

    CSV import/export test: Add support for dive mode
    Signed-off-by: Miika Turkia <miika.turkia@gmail.com>

--- tests/testparse.cpp
+++ tests/testparse.cpp
@@ -306,7 +306,7 @@ void TestParse::testParseMerge()
 int TestParse::parseCSVmanual(int units, std::string file)
 {
 	verbose = 1;
-	char *params[55];
+	char *params[57];
 	int pnr = 0;
 
 	// Numbers are column numbers
@@ -322,36 +322,38 @@ int TestParse::parseCSVmanual(int units, std::string file)
 	params[pnr++] = intdup(4);
 	params[pnr++] = strdup("meanDepthField");
 	params[pnr++] = intdup(5);
-	params[pnr++] = strdup("airtempField");
+	params[pnr++] = strdup("modeField");
 	params[pnr++] = intdup(6);
-	params[pnr++] = strdup("watertempField");
+	params[pnr++] = strdup("airtempField");
 	params[pnr++] = intdup(7);
-	params[pnr++] = strdup("cylindersizeField");
+	params[pnr++] = strdup("watertempField");
 	params[pnr++] = intdup(8);
-	params[pnr++] = strdup("startpressureField");
+	params[pnr++] = strdup("cylindersizeField");
 	params[pnr++] = intdup(9);
-	params[pnr++] = strdup("endpressureField");
+	params[pnr++] = strdup("startpressureField");
 	params[pnr++] = intdup(10);
-	params[pnr++] = strdup("o2Field");
+	params[pnr++] = strdup("endpressureField");
 	params[pnr++] = intdup(11);
-	params[pnr++] = strdup("heField");
+	params[pnr++] = strdup("o2Field");
 	params[pnr++] = intdup(12);
-	params[pnr++] = strdup("locationField");
+	params[pnr++] = strdup("heField");
 	params[pnr++] = intdup(13);
-	params[pnr++] = strdup("gpsField");
+	params[pnr++] = strdup("locationField");
 	params[pnr++] = intdup(14);
-	params[pnr++] = strdup("divemasterField");
+	params[pnr++] = strdup("gpsField");
 	params[pnr++] = intdup(15);
-	params[pnr++] = strdup("buddyField");
+	params[pnr++] = strdup("divemasterField");
 	params[pnr++] = intdup(16);
-	params[pnr++] = strdup("suitField");
+	params[pnr++] = strdup("buddyField");
 	params[pnr++] = intdup(17);
+	params[pnr++] = strdup("suitField");
+	params[pnr++] = intdup(18);
 	params[pnr++] = strdup("notesField");
-	params[pnr++] = intdup(20);
-	params[pnr++] = strdup("weightField");
 	params[pnr++] = intdup(21);
-	params[pnr++] = strdup("tagsField");
+	params[pnr++] = strdup("weightField");
 	params[pnr++] = intdup(22);
+	params[pnr++] = strdup("tagsField");
+	params[pnr++] = intdup(23);
 	// Numbers are indices of possible options
 	params[pnr++] = strdup("separatorIndex");
 	params[pnr++] = intdup(0);
