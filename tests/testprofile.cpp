291564463dce5bda1f906bec16bdd5de65e33855
Robert C. Helling <helling@atdotde.de>
2020-05-05 14:39:44 +0200

    Explanatory comment in testprofile.cpp
    Signed-off-by: Robert C. Helling <helling@atdotde.de>

--- tests/testprofile.cpp
+++ tests/testprofile.cpp
@@ -5,6 +5,14 @@
 #include "core/file.h"
 #include "core/save-profiledata.h"
 
+// This test compares the content of struct profile against a known reference version for a list
+// of dives to prevent accidental regressions. Thus is you change anything in the profile this
+// test will fail. If this change was intentional, run the test manually. Make sure only the
+// indended fields change (for example by computing a diff between exportprofile.csv and
+// ..dives/exportprofilereference.csv) and copy the former over the later and commit that change
+// as well.
+
+
 void TestProfile::testProfileExport()
 {
 	parse_file("../dives/abitofeverything.ssrf", &dive_table, &trip_table, &dive_site_table);
