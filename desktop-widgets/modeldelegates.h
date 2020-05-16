d85f92c5dfcc73ebd0f90b8f6b7e2554f05d0fe7
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-05-16 09:17:41 +0200

    cleanup: whitespace fixes in modeldelegates.[cpp|h]
    We put the pointer and reference modifier to the variable, not the
    type. Some people don't like this, but
    1) This is consistent with the rest of the code base.
    2) This is how C and C++ parse, love it or hate it.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/modeldelegates.h
+++ desktop-widgets/modeldelegates.h
@@ -23,7 +23,7 @@ public:
 	explicit StarWidgetsDelegate(QWidget *parent = 0);
 	void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 	QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
-	const QSize& starSize() const;
+	const QSize &starSize() const;
 
 private:
 	QWidget *parentWidget;
@@ -74,7 +74,7 @@ public:
 	explicit TankUseDelegate(QObject *parent = 0);
 	void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
 	QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
-	void setEditorData(QWidget * editor, const QModelIndex & index) const override;
+	void setEditorData(QWidget *editor, const QModelIndex &index) const override;
 };
 
 class WSInfoDelegate : public ComboBoxDelegate {
