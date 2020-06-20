81aba4371e3eee9afe8b9226ce19311f1f02de45
Paul Buxton <paulbuxton.mail@googlemail.com>
2020-06-20 09:44:49 +0100

    Build qtconnectivity with native-win32-bluetooth Adds a patch to MXE
    Signed-off-by: Paul Buxton <paulbuxton.mail@googlemail.com>

--- scripts/docker/mxe-build-container/build-container.sh
+++ scripts/docker/mxe-build-container/build-container.sh
@@ -6,7 +6,7 @@ SCRIPTPATH=$(dirname $0)
 
 export VERSION=1.1
 pushd $SCRIPTPATH
-docker build -t subsurface/mxe-build-container:$VERSION --build-arg=mxe_sha=1ee37f8 -f Dockerfile-stage1 . 
+docker build -t subsurface/mxe-build-container:$VERSION --build-arg=mxe_sha=1ee37f8 -f Dockerfile-stage1 .
 docker build -t subsurface/mxe-build-container:$VERSION --build-arg=VERSION=$VERSION -f Dockerfile-stage2 .
 popd
  
