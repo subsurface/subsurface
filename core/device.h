936cf453f9f9e338148ff08293340d960612e12d
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-09-13 18:16:01 +0200

    cleanup: move set_dc_nickname() declaration from dive.h to device.h
    The function *looks* like it is a dive function. However, in reality
    it implicitly works on the global device list. Therefore, it is
    thematically more aptly located in device.h with the other device
    functions.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/device.h
+++ core/device.h
@@ -11,6 +11,7 @@ extern "C" {
 struct divecomputer;
 extern void fake_dc(struct divecomputer *dc);
 extern void set_dc_deviceid(struct divecomputer *dc, unsigned int deviceid);
+extern void set_dc_nickname(struct dive *dive);
 extern void create_device_node(const char *model, uint32_t deviceid, const char *serial, const char *firmware, const char *nickname);
 extern void call_for_each_dc(void *f, void (*callback)(void *, const char *, uint32_t,
 						       const char *, const char *, const char *), bool select_only);
