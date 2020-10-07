2c4bd152769368e6434e60fcd546bbb2da2a08bf
Dirk Hohndel <dirk@hohndel.org>
2020-10-07 12:48:57 -0700

    build-system: remove Grantlee from the get-dep-lib script
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/get-dep-lib.sh
+++ scripts/get-dep-lib.sh
@@ -16,7 +16,6 @@ CURRENT_LIBXML2="v2.9.4"
 CURRENT_LIBFTDI="1.3"
 CURRENT_KIRIGAMI="v5.62.0"
 CURRENT_BREEZE_ICONS="4daac191fb33c8c03bba8356db9767816cb8ee02"
-CURRENT_GRANTLEE="v5.1.0"
 CURRENT_MDBTOOLS="master"
 CURRENT_QT_ANDROID_CMAKE="master"
 
@@ -189,11 +188,6 @@ for package in "${PACKAGES[@]}" ; do
 			;;
 		sqlite)
 			curl_download_library sqlite https://www.sqlite.org/2017/ sqlite-autoconf-${CURRENT_SQLITE}.tar.gz
-			;;
-		grantlee)
-			git_checkout_library grantlee $CURRENT_GRANTLEE https://github.com/steveire/grantlee.git
-			# this should be removed once there is a newer Grantlee release than v5.1.0
-			( cd grantlee ; git -c user.email=in@val.id -c user.name='Subsurface build automation' cherry-pick 1b4f22431ae35dfd11f07a5ae88a1b4db3de2a85 ; cd .. )
 			;;
 		mdbtools)
 			git_checkout_library mdbtools $CURRENT_MDBTOOLS https://github.com/brianb/mdbtools.git
