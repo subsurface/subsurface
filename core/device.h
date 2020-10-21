37d6a7a7bd227611a2d3db797ab040bf891d0c2a
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-10-21 15:25:18 +0200

    devices: return index from function adding / removing devices
    This will be used to keep the model representing the device-list
    up to date.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/device.h
+++ core/device.h
@@ -29,8 +29,8 @@ extern bool device_used_by_selected_dive(const struct device *dev);
 
 extern const struct device *get_device_for_dc(const struct device_table *table, const struct divecomputer *dc);
 extern bool device_exists(const struct device_table *table, const struct device *dev);
-extern void add_to_device_table(struct device_table *table, const struct device *dev);
-extern void remove_device(struct device_table *table, const struct device *dev);
+extern int add_to_device_table(struct device_table *table, const struct device *dev); // returns index
+extern int remove_device(struct device_table *table, const struct device *dev); // returns index or -1 if not found
 
 // struct device accessors for C-code. The returned strings are not stable!
 const char *device_get_model(const struct device *dev);
