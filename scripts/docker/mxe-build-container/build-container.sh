e6d6b31924cc55a0d71df726daff6e9c9516a45c
Dirk Hohndel <dirk@hohndel.org>
2020-10-30 11:28:00 -0700

    build-system: switch to the 32/64bit MXE build container
    This doesn't actually build 32 bit binaries, yet. One step at a time.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/docker/mxe-build-container/build-container.sh
+++ scripts/docker/mxe-build-container/build-container.sh
@@ -7,8 +7,11 @@ MXE_SHA="8966a64"
 SCRIPTPATH=$(dirname $0)
 
 # version of the docker image
-VERSION=2.0
+VERSION=2.1
 
 pushd $SCRIPTPATH
+
+# we use the 'experimental' --squash argument to significantly reduce the size of the massively huge
+# Docker container this produces
 docker build --squash -t subsurface/mxe-build-container:$VERSION --build-arg=mxe_sha=$MXE_SHA -f Dockerfile .
 popd
