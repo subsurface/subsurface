542536080e42995085f1c12f9d9634eaa42fec9b
Dirk Hohndel <dirk@hohndel.org>
2020-11-14 19:44:06 -0800

    downloader: first outline of downloading dives from a device
    This is of course not functional at all, but it gives a first idea of
    what we will need to do in this code.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ core/cli-downloader.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "qt-models/diveimportedmodel.h"
+
+#include <QObject>
+#include <QUndoStack>
+
+extern "C"
+void cliDownloader(const char *vendor, const char *product, const char *device)
+{
+	DiveImportedModel *diveImportedModel = new DiveImportedModel();
+	DiveImportedModel::connect(diveImportedModel, &DiveImportedModel::downloadFinished, [] {
+		// do something useful at the end of the download
+	});
+
+	auto data = diveImportedModel->thread.data();
+	data->setVendor(vendor);
+	data->setProduct(product);
+	if (data->vendor() == "Uemis") {
+		char *colon;
+		char *devname = strdup(device);
+		if ((colon = strstr(devname, ":\\ (UEMISSDA)")) != NULL) {
+			*(colon + 2) = '\0';
+			fprintf(stderr, "shortened devname to \"%s\"", devname);
+		}
+		data->setDevName(devname);
+	} else {
+		data->setDevName(device);
+	}
+
+	// some assumptiond - should all be configurable
+	data->setForceDownload(false);
+	data->setSaveLog(false);
+	data->setSaveDump(false);
+
+	// before we start, remember where the dive_table ended
+	diveImportedModel->startDownload();
+}
