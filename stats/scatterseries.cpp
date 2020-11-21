397f255fd2f4c7a5dcc292b524ff368c3f267649
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-11-21 19:06:12 +0100

    stats: add dive information to scatter plots
    When hovering over items, show dive information. This took some
    plumbing, because the dive now have to be exported to the UI.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- stats/scatterseries.cpp
+++ stats/scatterseries.cpp
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0
 #include "scatterseries.h"
+#include "statstranslations.h"
+#include "statstypes.h"
+#include "core/dive.h"
+#include "core/divelist.h"
+#include "core/qthelper.h"
 
 #include <QChart>
 #include <QGraphicsPixmapItem>
@@ -11,9 +16,12 @@ static const QColor scatterItemHighlightedColor(Qt::yellow);
 static const QColor scatterItemHighlightedBorderColor(0xaa, 0xaa, 0x22);
 static const int scatterItemDiameter = 10;
 static const int scatterItemBorder = 1;
+static const QColor informationBorderColor(Qt::black);
+static const QColor informationColor(0xff, 0xff, 0x00, 192); // Note: fourth argument is opacity
+static const int informationBorder = 2;
 
-ScatterSeries::ScatterSeries()
-	: highlighted(-1)
+ScatterSeries::ScatterSeries(const StatsType &typeX, const StatsType &typeY)
+	: highlighted(-1), typeX(typeX), typeY(typeY)
 {
 }
 
@@ -50,8 +58,9 @@ static const QPixmap &scatterPixmap(bool highlight)
 	return highlight ? *scatterPixmapHighlightedPtr : *scatterPixmapPtr;
 }
 
-ScatterSeries::Item::Item(QtCharts::QChart *chart, ScatterSeries *series, double pos, double value) :
+ScatterSeries::Item::Item(QtCharts::QChart *chart, ScatterSeries *series, dive *d, double pos, double value) :
 	item(new QGraphicsPixmapItem(scatterPixmap(false), chart)),
+	d(d),
 	pos(pos),
 	value(value)
 {
@@ -71,9 +80,9 @@ void ScatterSeries::Item::highlight(bool highlight)
 	item->setPixmap(scatterPixmap(highlight));
 }
 
-void ScatterSeries::append(double pos, double value)
+void ScatterSeries::append(dive *d, double pos, double value)
 {
-	items.emplace_back(chart(), this, pos, value);
+	items.emplace_back(chart(), this, d, pos, value);
 }
 
 void ScatterSeries::updatePositions()
@@ -128,7 +137,91 @@ void ScatterSeries::highlight(int index)
 	if (highlighted >= 0 && highlighted < (int)items.size())
 		items[highlighted].highlight(false);
 	highlighted = index;
+
 	// Highlight new item (if any)
-	if (highlighted >= 0 && highlighted < (int)items.size())
-		items[highlighted].highlight(true);
+	if (highlighted >= 0 && highlighted < (int)items.size()) {
+		Item &item = items[highlighted];
+		item.highlight(true);
+		QPointF pos = item.item->pos();
+		if (!information)
+			information.reset(new Information(chart()));
+		information->set(items[highlighted].d, pos, typeX, typeY);
+	} else {
+		information.reset();
+	}
+}
+
+ScatterSeries::Information::Information(QtCharts::QChart *chart) : QGraphicsRectItem(chart)
+{
+	setPen(QPen(informationBorderColor, informationBorder));
+	setBrush(informationColor);
+}
+
+void ScatterSeries::Information::set(const dive *d, QPointF pos, const StatsType &typeX, const StatsType &typeY)
+{
+	width = height = 0.0;
+	textItems.clear();
+
+	// We don't listen to undo-command signals, therefore we have to check whether that dive actually exists!
+	// TODO: fix this.
+	if (get_divenr(d) < 0) {
+		addLine(StatsTranslations::tr("Removed dive"));
+	} else {
+		addLine(StatsTranslations::tr("Dive #%1").arg(d->number));
+		addLine(get_dive_date_string(d->when));
+		addDataLine(typeX, d);
+		addDataLine(typeY, d);
+	}
+
+	width += 4.0 * informationBorder;
+	height += 4.0 * informationBorder;
+
+	QRectF parentRect = parentItem()->boundingRect();
+
+	double x = pos.x() + scatterItemDiameter;
+	if (x + width >= parentRect.right() && pos.x() - width >= parentRect.x())
+		x = pos.x() - width;
+	double y = pos.y() + scatterItemDiameter;
+	if (y + height >= parentRect.bottom() && pos.y() - height >= parentRect.y())
+		y = pos.y() - height;
+
+
+	setRect(x, y, width, height);
+	double actY = y + 2.0 * informationBorder;
+	for (auto &item: textItems) {
+		item->setPos(QPointF(x + 2.0 * informationBorder, actY));
+		actY += item->boundingRect().height();
+	}
+	setZValue(20.0); // What would a proper value be here?
+}
+
+void ScatterSeries::Information::addLine(const QString &s)
+{
+	textItems.emplace_back(new QGraphicsSimpleTextItem(s, this));
+	QGraphicsSimpleTextItem &item = *textItems.back();
+	item.setPos(QPointF(0.0, height));
+	QRectF rect = item.boundingRect();
+	width = std::max(width, rect.width());
+	height += rect.height();
+}
+
+void ScatterSeries::Information::addDataLine(const StatsType &type, const dive *d)
+{
+	// For "numeric" types, we display value and unit.
+	// For "discrete" types, we display all categories the dive belongs to.
+	// There is only one "continuous" type, the date, for which we don't display anything,
+	// because the date is displayed anyway.
+	QString val;
+	switch (type.type()) {
+	case StatsType::Type::Numeric:
+		val = type.valueWithUnit(d);
+		break;
+	case StatsType::Type::Discrete:
+		val = type.diveCategories(d);
+		break;
+	default:
+		return;
+	}
+
+	addLine(QString("%1: %2").arg(type.name(), val));
 }
