847b2b82ff9cca5fc45ae9f26285821d9a2bd3e1
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2020-12-12 13:28:36 +0100

    cleanup: make templateOptions and printOptions reference types
    These two structs describe options used during printing.
    They are passed through numerous classes as pointer. In this
    case, reference semantics are preferred, as references:
     - can never be null
     - can not change during their lifetime
    This not only helps the compiler, as it can optimize away null
    checks, but also your fellow coder. Moreover, it prevents
    unintentional creation of uninitialized references: one can't
    create an instance of a class without initializing a reference
    member. It does not prevent references from going dangling.
    However, pointers have the same disadvantage.
    
    Contains a few whitespace cleanups.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/templateedit.h
+++ desktop-widgets/templateedit.h
@@ -14,30 +14,24 @@ class TemplateEdit : public QDialog
 	Q_OBJECT
 
 public:
-	explicit TemplateEdit(QWidget *parent, struct print_options *printOptions, struct template_options *templateOptions);
+	explicit TemplateEdit(QWidget *parent, const print_options &printOptions, template_options &templateOptions);
 	~TemplateEdit();
 private slots:
 	void on_fontsize_valueChanged(int font_size);
-
 	void on_linespacing_valueChanged(double line_spacing);
-
 	void on_borderwidth_valueChanged(double border_width);
-
 	void on_fontSelection_currentIndexChanged(int index);
-
 	void on_colorpalette_currentIndexChanged(int index);
-
 	void on_buttonBox_clicked(QAbstractButton *button);
-
 	void colorSelect(QAbstractButton *button);
 
 private:
 	Ui::TemplateEdit *ui;
 	QButtonGroup *btnGroup;
 	bool editingCustomColors;
-	struct template_options *templateOptions;
+	const print_options &printOptions;
+	template_options &templateOptions;
 	struct template_options newTemplateOptions;
-	struct print_options *printOptions;
 	QString grantlee_template;
 	void saveSettings();
 	void updatePreview();
