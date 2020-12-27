a281fbe1474f8de2a33ad6a5097b5f5066290276
Subsurface CI <dirk@hohndel.org>
2020-12-27 14:31:39 -0800

    build-system/trusty: use new Qt installer
    Update to Qt 5.12.10, latest OpenSSL, add QtChart, add other missing packages.
    Also switch to gcc-7 as our statistics code requires better C++17 support than
    what gcc-6 can offer.
    
    This then creates trusty-qt512:1.1
    
    Signed-off-by: Subsurface CI <dirk@hohndel.org>

--- scripts/docker/trusty-qt512/getpackages.sh
+++ scripts/docker/trusty-qt512/getpackages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.sh
-wget http://download.qt.io/official_releases/qt/5.12/5.12.4/qt-opensource-linux-x64-5.12.4.run
 curl -O https://dbus.freedesktop.org/releases/dbus/dbus-1.12.16.tar.gz
-wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1.tar.gz
+wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1i.tar.gz
