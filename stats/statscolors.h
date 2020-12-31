25bbcb1ee77d0f346ddea622286f53031b70245f
Dirk Hohndel <dirk@hohndel.org>
2020-12-30 17:32:48 -0800

    statistics: attempt better color palette
    This color list was created using a tool by Gregor Aisch that is available on
    GitHub as https://github.com/gka/palettes to create multi-hued, multi-stop
    color scales that are safe for color blind people and optimize the ability to
    distinguish the various bins of our statistics.
    
    This specific set was created using
    https://vis4.net/palettes/#/14|d|00108c,3ed8ff,ffffe0|ffffe0,ff005e,743535|1|1
    
    What would be even better is an approach that starts with maybe 30 colors
    (which by definition will be fairly close to each other), and then picks a
    subset based on the number of bins, aiming for maximum distance between the
    colors picked. But for that the binColor() function would need to know the
    total number of bins (which I haven't been able to figure out reliably without
    breaking the abstraction for these charts).
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- stats/statscolors.h
+++ stats/statscolors.h
@@ -10,4 +10,15 @@ static const QColor borderColor(0x66, 0xb2, 0xff);
 static const QColor highlightedColor(Qt::yellow);
 static const QColor highlightedBorderColor(0xaa, 0xaa, 0x22);
 
+// Colors created using the Chroma.js Color Palette Helper
+// https://vis4.net/palettes/#/14|d|00108c,3ed8ff,ffffe0|ffffe0,ff005e,743535|1|1
+inline const QColor binColors[] = {
+	QRgb(0x00108c), QRgb(0x2737a0), QRgb(0x3f59b4), QRgb(0x567bc6), QRgb(0x719ed5), QRgb(0x93c0e1), QRgb(0xbfe2e7),
+	QRgb(0xffdac4), QRgb(0xffb2a6), QRgb(0xf68d8b), QRgb(0xe26c72), QRgb(0xc74f5c), QRgb(0xa33c47), QRgb(0x743535)
+};
+inline QColor binColor(int bin)
+{
+	return bin < 0 ? binColors[0] : binColors[bin % std::size(binColors)];
+}
+
 #endif
