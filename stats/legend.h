27e5ac96272efba2afc34de58cbf52cbe8c12dca
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-01-01 17:55:44 +0100

    statistics: implement a legend box
    For some chart (e.g. pie charts or stacked bar charts), we want
    to display a legend. QtCharts' legend interface happens to be
    private and therefore is of no use.
    
    This introduces a legend box which is implemented using
    QGraphicItems, which can be placed on top of QCharts. It's very
    unfancy, but works for now. If there are too many items, not
    all are shown. Currently, the legend is configured to fill
    at most half of the width and half of the height of the chart.
    This might need some optimization.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

---
+++ stats/legend.h
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0
+// A legend box, which is shown on the chart.
+#ifndef STATS_LEGEND_H
+#define STATS_LEGEND_H
+
+#include <memory>
+#include <vector>
+#include <QGraphicsRectItem>
+
+class QGraphicsSceneMouseEvent;
+
+class Legend : public QGraphicsRectItem {
+public:
+	Legend(QGraphicsWidget *chart, const std::vector<QString> &names);
+	void hover(QPointF pos);
+	void resize(); // called when the chart size changes.
+private:
+	// Each entry is a text besides a rectangle showing the color
+	struct Entry {
+		std::unique_ptr<QGraphicsRectItem> rect;
+		std::unique_ptr<QGraphicsSimpleTextItem> text;
+		QPointF pos;
+		double width;
+		Entry(const QString &name, int idx, int numBins, QGraphicsItem *parent);
+	};
+	QGraphicsWidget *chart;
+	int displayedItems;
+	double width;
+	double height;
+	int fontHeight;
+	std::vector<Entry> entries;
+	void updatePosition();
+	void hide();
+};
+
+#endif
