fd1341e4630010b4202c27c4ebba9e019278dd2f
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-01-01 01:37:56 +0100

    statistics: add statistic variables
    The StatisticVariable class hierarchy encapsulates the concept
    of a dive-variable, which can be plotted in charts either as
    dependend or independend variable.
    
    There are three types of these variables:
    1) discrete: For example dive buddies or suit type.
    2) continuous: Has a notion of linear metric - can be
       used as histogram or scatter plot axis.
    3) numeric: Like continuous, but allows for operations
       such as calculating the mean or the sum over numerous
       dives.
    
    All variables support binning. The bins are defined per
    variable.
    
    Continuous variables can be converted into an arbitrary
    double value, which is used to be plotted on a continuous
    axis.
    
    Moreover, numeric variables support a number of operations,
    which depend on the variable.
    
    Since binning is based on different types, the code is rather
    template-heavy. Of course, this could be solved with
    unions/variants and runtime-polymorphism, but using templates
    was just much quicker. Notably, this uses the CRTP
    (curiously recurring template pattern) where a subclass
    passes itself as argument to the baseclass. This is a weird
    kind of "reverse inheritance".
    
    The StatsTranslations class is a dummy class which will
    be used to collect all translations of the statistics
    module.
    
    This includes changes by Dirk to fix compilation of the
    downloader.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ stats/statstranslations.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+// Dummy object for stats-module related translations
+
+#ifndef STATS_TRANSLATIONS_H
+#define STATS_TRANSLATIONS_H
+
+#include <QObject>
+
+class StatsTranslations : public QObject
+{
+	Q_OBJECT
+};
+
+#endif
