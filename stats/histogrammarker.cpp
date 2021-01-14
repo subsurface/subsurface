1239938355f58c69c3921c4560ba712c9f0849ee
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-01-14 09:48:44 +0100

    statistics: convert HistogramMarkers to QSGNodes
    This is in analogy to the quartile markers.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

---
+++ stats/histogrammarker.cpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "histogrammarker.h"
+#include "statsaxis.h"
+#include "zvalues.h"
+
+static const double histogramMarkerWidth = 2.0;
+
+HistogramMarker::HistogramMarker(StatsView &view, double val, bool horizontal,
+				 QColor color, StatsAxis *xAxis, StatsAxis *yAxis) :
+	ChartLineItem(view, ChartZValue::ChartFeatures, color, histogramMarkerWidth),
+	xAxis(xAxis), yAxis(yAxis),
+	val(val), horizontal(horizontal)
+{
+}
+
+void HistogramMarker::updatePosition()
+{
+	if (!xAxis || !yAxis)
+		return;
+	if (horizontal) {
+		double y = yAxis->toScreen(val);
+		auto [x1, x2] = xAxis->minMaxScreen();
+		setLine(QPointF(x1, y), QPointF(x2, y));
+	} else {
+		double x = xAxis->toScreen(val);
+		auto [y1, y2] = yAxis->minMaxScreen();
+		setLine(QPointF(x, y1), QPointF(x, y2));
+	}
+}
---
+++ stats/histogrammarker.h
@@ -0,0 +1,21 @@
+// A line to show median an mean in histograms
+#ifndef HISTOGRAM_MARKER_H
+#define HISTOGRAM_MARKER_H
+
+#include "chartitem.h"
+
+class StatsAxis;
+class StatsView;
+
+// A line marking median or mean in histograms
+class HistogramMarker : public ChartLineItem {
+public:
+	HistogramMarker(StatsView &view, double val, bool horizontal, QColor color, StatsAxis *xAxis, StatsAxis *yAxis);
+	void updatePosition();
+private:
+	StatsAxis *xAxis, *yAxis;
+	double val;
+	bool horizontal;
+};
+
+#endif
