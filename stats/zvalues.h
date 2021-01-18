891098d58d0314e4ab68cec9ff4032626498ae18
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-01-18 12:51:07 +0100

    statistics: remove float z-value
    These values were used for items on the QGraphicsScene and
    have been replaced by integer values used on the QSG scene.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- stats/zvalues.h
+++ stats/zvalues.h
@@ -2,18 +2,8 @@
 // Defines the z-values of features in the chart view.
 // Objects with higher z-values are painted on top of objects
 // with smaller z-values. For the same z-value objects are
-// drawn in order of addition to the graphics scene.
+// drawn in order of addition to the scene.
 #ifndef ZVALUES_H
-
-struct ZValues {
-	static constexpr double grid = -1.0;
-	static constexpr double series = 0.0;
-	static constexpr double axes = 1.0;
-	static constexpr double seriesLabels = 2.0;
-	static constexpr double chartFeatures = 3.0;	// quartile markers and regression lines
-	static constexpr double informationBox = 4.0;
-	static constexpr double legend = 5.0;
-};
 
 enum class ChartZValue {
 	Grid = 0,
