dc645ce8c672af8d8166c27ff581ae44994faf6d
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-05-08 13:06:49 +0200

    profile: rename GF_LINE color to DURATION_LINE
    The color was misnamed, since it has only been used for the
    duration line for quite some time (since 893bea700c98 to be
    exact).
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/color.h
+++ core/color.h
@@ -135,7 +135,7 @@ typedef enum {
 	CALC_CEILING_SHALLOW,
 	CALC_CEILING_DEEP,
 	TISSUE_PERCENTAGE,
-	GF_LINE
+	DURATION_LINE
 } color_index_t;
 
 QColor getColor(const color_index_t i, bool isGrayscale = false);
