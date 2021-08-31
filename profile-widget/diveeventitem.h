977bafcc0756f6508ba38e755cb773fd62553316
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-08-31 20:44:37 +0200

    profile: save depth of event
    The DivePlotDataModel was saved with every event to access the
    depth. However, since the depth never changes, we can simply
    save the depth instead.
    
    Also, since we only need the model to access the plot_info,
    pass the plot_info directly. As noted in a previous commit
    message, I believe that Qt models are a very bad choice
    for intra-application data transfer. They should only ever
    be used to interface with Qt.
    
    And since touching this code, pass duration_t instead of int
    to depthAtTime() to make the callers less cluttered.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- profile-widget/diveeventitem.h
+++ profile-widget/diveeventitem.h
@@ -5,15 +5,15 @@
 #include "divepixmapitem.h"
 
 class DiveCartesianAxis;
-class DivePixmapCache;
 class DivePixmaps;
 struct event;
+struct plot_info;
 
 class DiveEventItem : public DivePixmapItem {
 	Q_OBJECT
 public:
 	DiveEventItem(const struct dive *d, struct event *ev, struct gasmix lastgasmix,
-		      DivePlotDataModel *model, DiveCartesianAxis *hAxis, DiveCartesianAxis *vAxis,
+		      const struct plot_info &pi, DiveCartesianAxis *hAxis, DiveCartesianAxis *vAxis,
 		      int speed, const DivePixmaps &pixmaps, QGraphicsItem *parent = nullptr);
 	~DiveEventItem();
 	const struct event *getEvent() const;
@@ -21,10 +21,9 @@ public:
 	void eventVisibilityChanged(const QString &eventName, bool visible);
 	void setVerticalAxis(DiveCartesianAxis *axis, int speed);
 	void setHorizontalAxis(DiveCartesianAxis *axis);
-	void setModel(DivePlotDataModel *model);
 	bool shouldBeHidden();
 	static bool isInteresting(const struct dive *d, const struct divecomputer *dc,
-				  const struct event *ev, const DivePlotDataModel &model);
+				  const struct event *ev, const struct plot_info &pi);
 public
 slots:
 	void recalculatePos(int animationSpeed);
@@ -34,9 +33,9 @@ private:
 	void setupPixmap(struct gasmix lastgasmix, const DivePixmaps &pixmaps);
 	DiveCartesianAxis *vAxis;
 	DiveCartesianAxis *hAxis;
-	DivePlotDataModel *dataModel;
 	struct event *ev;
 	const struct dive *dive;
+	int depth;
 };
 
 #endif // DIVEEVENTITEM_H
