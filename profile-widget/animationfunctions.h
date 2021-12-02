9ccfb76190b07ffc6b41e1fbd9e899207ed45a68
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-12-02 23:59:58 +0100

    profile: remove unused animation functions
    A few animation function are not necessary anymore.
    Remove them.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- profile-widget/animationfunctions.h
+++ profile-widget/animationfunctions.h
@@ -2,18 +2,12 @@
 #ifndef ANIMATIONFUNCTIONS_H
 #define ANIMATIONFUNCTIONS_H
 
-#include <QtGlobal>
-#include <QPointF>
-
 class QObject;
 
 namespace Animations {
 	void hide(QObject *obj, int speed);
 	void show(QObject *obj, int speed);
-	void moveTo(QObject *obj, int speed, qreal x, qreal y);
-	void moveTo(QObject *obj, int speed, const QPointF &pos);
-	void animDelete(QObject *obj, int speed);
-	void scaleTo(QObject *obj, int speed, qreal scale);
+	void scaleTo(QObject *obj, int speed, double scale);
 }
 
 #endif // ANIMATIONFUNCTIONS_H
