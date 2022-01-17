a4438c2d07c8a44486eb5113f567b6692fabb777
Dirk Hohndel <dirk@hohndel.org>
2022-01-17 13:17:28 -0800

    update copyright year...
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- translations/subsurface_pt_PT.ts
+++ translations/subsurface_pt_PT.ts
@@ -24,11 +24,11 @@
         <source>Version: %1
 
 © Subsurface developer team
-2011-2021</source>
+2011-2022</source>
         <translation>Versão: %1
 
 © Subsurface developer team
-2011-2021 </translation>
+2011-2022 </translation>
     </message>
     <message>
         <location filename="../mobile-widgets/qml/About.qml" line="65"/>
@@ -9408,8 +9408,8 @@ Files with inappropriate date/time</source>
     </message>
     <message>
         <location filename="../desktop-widgets/about.cpp" line="15"/>
-        <source>&lt;span style=&apos;font-size: 18pt; font-weight: bold;&apos;&gt;Subsurface %1 &lt;/span&gt;&lt;br&gt;&lt;br&gt;Multi-platform divelog software&lt;br&gt;&lt;span style=&apos;font-size: 8pt&apos;&gt;Linus Torvalds, Dirk Hohndel, Tomaz Canabrava, Berthold Stoeger, and others, 2011-2021&lt;/span&gt;</source>
-        <translation>&lt;span style=&apos;font-size: 18pt; font-weight: bold;&apos;&gt;Subsurface %1&lt;/span&gt;&lt;br&gt;&lt;br&gt;Programa multi-plataforma para registo de mergulhos&lt;br&gt;&lt;span style=&apos;font-size: 8pt&apos;&gt; Linus Torvalds, Dirk Hohndel, Tomaz Canabrava, Berthold Stoeger e outros, 2011-2021&lt;/span&gt;</translation>
+        <source>&lt;span style=&apos;font-size: 18pt; font-weight: bold;&apos;&gt;Subsurface %1 &lt;/span&gt;&lt;br&gt;&lt;br&gt;Multi-platform divelog software&lt;br&gt;&lt;span style=&apos;font-size: 8pt&apos;&gt;Linus Torvalds, Dirk Hohndel, Tomaz Canabrava, Berthold Stoeger, and others, 2011-2022&lt;/span&gt;</source>
+        <translation>&lt;span style=&apos;font-size: 18pt; font-weight: bold;&apos;&gt;Subsurface %1&lt;/span&gt;&lt;br&gt;&lt;br&gt;Programa multi-plataforma para registo de mergulhos&lt;br&gt;&lt;span style=&apos;font-size: 8pt&apos;&gt; Linus Torvalds, Dirk Hohndel, Tomaz Canabrava, Berthold Stoeger e outros, 2011-2022&lt;/span&gt;</translation>
     </message>
 </context>
 <context>
