1b8f3a06ecd1361e5c764cddf22fadbf79a28690
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2022-02-14 21:47:12 +0100

    undo: add flag that indicates whether a command has been placed
    In multiple places we have the problem that when an undo command
    is executed, the corresponding value-changed signals are emitted,
    which in turn updates the UI. This can have nasty UI effects, such
    as the cursor position in the notes field being reset.
    
    To avoid this, add a flag that indicates whether a newly placed
    command is currently executed.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- commands/command.h
+++ commands/command.h
@@ -26,6 +26,7 @@ bool isClean();				// Any changes need to be saved?
 QAction *undoAction(QObject *parent);	// Create an undo action.
 QAction *redoAction(QObject *parent);	// Create an redo action.
 QString changesMade();			// return a string with the texts from all commands on the undo stack -> for commit message
+bool placingCommand();			// Currently executing a new command -> might not have to update the field the user just edited.
 
 // 2) Dive-list related commands
 
