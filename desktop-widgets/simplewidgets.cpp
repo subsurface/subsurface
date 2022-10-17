subsurface/subsurface

commit 7417f865cd628aa5df78961dc198a9070822ed72

cleanup: un-singletonize ShiftTimesDialog
There is no reason that this dialog is a singleton. Since it is modal,
it can be created on demand. Apart from removing superfluous global state,
this simplifies code, because preparing the widget can now be done in
the constructor instead of overriding the showEvent() function.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

desktop-widgets/simplewidgets.cpp
@@ -179,12 +179,6 @@ SetpointDialog::SetpointDialog(struct dive *dIn, int dcNrIn, int seconds) : QDia
 	connect(quit, &QShortcut::activated, MainWindow::instance(), &QWidget::close);
 }
 
-ShiftTimesDialog *ShiftTimesDialog::instance()
-{
-	static ShiftTimesDialog *self = new ShiftTimesDialog(MainWindow::instance());
-	return self;
-}
-
 void ShiftTimesDialog::buttonClicked(QAbstractButton *button)
 {
 	int amount;
@@ -196,10 +190,7 @@ void ShiftTimesDialog::buttonClicked(QAbstractButton *button)
 		if (amount != 0)
 			Command::shiftTime(getDiveSelection(), amount);
 	}
-}
 
-void ShiftTimesDialog::showEvent(QShowEvent*)
-{
 	ui.timeEdit->setTime(QTime(0, 0, 0, 0));
 	when = get_times(); //get time of first selected dive
 	ui.currentTime->setText(get_dive_date_string(when));

desktop-widgets/divelistview.cpp
@@ -860,7 +860,8 @@ void DiveListView::contextMenuEvent(QContextMenuEvent *event)
 
 void DiveListView::shiftTimes()
 {
-	ShiftTimesDialog::instance()->show();
+	ShiftTimesDialog dialog(MainWindow::instance());
+	dialog.exec();
 }
 
 void DiveListView::loadImages()

desktop-widgets/simplewidgets.h
@@ -78,15 +78,13 @@ private:
 class ShiftTimesDialog : public QDialog {
 	Q_OBJECT
 public:
-	static ShiftTimesDialog *instance();
-	void showEvent(QShowEvent *event);
+	explicit ShiftTimesDialog(QWidget *parent);
 private
 slots:
 	void buttonClicked(QAbstractButton *button);
 	void changeTime();
 
 private:
-	explicit ShiftTimesDialog(QWidget *parent);
 	int64_t when;
 	Ui::ShiftTimesDialog ui;
 };

commit e52de538c6e47a8a4caa05d67e99f34ad0586ca3

desktop: make time shift dialog show correct times
This got broken in commit 7417f865cd ("cleanup: un-singletonize
ShiftTimesDialog") and no one ever noticed.

We need to intitialize the when variable and set up the initial texts in
order for the time shift dialog to show the correct information. Doing
this in the ButtonClicked member (right before the dialog is destroyed)
makes absolutely no sense.

Fixes #3535

Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

desktop-widgets/simplewidgets.cpp
@@ -89,13 +89,6 @@ void ShiftTimesDialog::buttonClicked(QAbstractButton *button)
 		if (amount != 0)
 			Command::shiftTime(getDiveSelection(), amount);
 	}
-
-	ui.timeEdit->setTime(QTime(0, 0, 0, 0));
-	dive *d = first_selected_dive();
-	if (d) {
-		ui.currentTime->setText(get_dive_date_string(d->when));
-		ui.shiftedTime->setText(get_dive_date_string(d->when));
-	}
 }
 
 void ShiftTimesDialog::changeTime()
@@ -120,6 +113,12 @@ ShiftTimesDialog::ShiftTimesDialog(QWidget *parent) : QDialog(parent),
 	connect(close, SIGNAL(activated()), this, SLOT(close()));
 	QShortcut *quit = new QShortcut(QKeySequence(Qt::CTRL | Qt::Key_Q), this);
 	connect(quit, SIGNAL(activated()), parent, SLOT(close()));
+	dive *d = first_selected_dive();
+	if (d) {
+		when = d->when;
+		ui.currentTime->setText(get_dive_date_string(when));
+		ui.shiftedTime->setText(get_dive_date_string(when));
+	}
 }
 
 void ShiftImageTimesDialog::syncCameraClicked()