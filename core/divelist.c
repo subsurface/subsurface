8f4b6cfcb9e439a0c1b304f8ffae38b079da8b0a
Anton Lundin <glance@ac2.se>
2023-10-11 22:15:02 +0200

    Ignore not used cylinders in get_dive_gas
    Signed-off-by: Anton Lundin <glance@ac2.se>

--- core/divelist.c
+++ core/divelist.c
@@ -43,6 +43,8 @@ void get_dive_gas(const struct dive *dive, int *o2_p, int *he_p, int *o2max_p)
 			continue;
 		if (cyl->cylinder_use == OXYGEN)
 			continue;
+		if (cyl->cylinder_use == NOT_USED)
+			continue;
 		if (o2 > maxo2)
 			maxo2 = o2;
 		if (he > maxhe)
