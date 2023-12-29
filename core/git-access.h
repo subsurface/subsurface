e06193446fabfd36398fb766a2aa6b276dd347a2
Dirk Hohndel <dirk@hohndel.org>
2023-12-29 13:22:19 -0800

    some explanations for the different servers
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- core/git-access.h
+++ core/git-access.h
@@ -13,10 +13,10 @@ extern "C" {
 #include <stdbool.h>
 #endif
 
-#define CLOUD_HOST_US "ssrf-cloud-us.subsurface-divelog.org"
-#define CLOUD_HOST_U2 "ssrf-cloud-u2.subsurface-divelog.org"
-#define CLOUD_HOST_EU "ssrf-cloud-eu.subsurface-divelog.org"
-#define CLOUD_HOST_E2 "ssrf-cloud-e2.subsurface-divelog.org"
+#define CLOUD_HOST_US "ssrf-cloud-us.subsurface-divelog.org"  // preferred (faster/bigger) server in the US
+#define CLOUD_HOST_U2 "ssrf-cloud-u2.subsurface-divelog.org"  // secondary (older) server in the US
+#define CLOUD_HOST_EU "ssrf-cloud-eu.subsurface-divelog.org"  // preferred (faster/bigger) server in Germany
+#define CLOUD_HOST_E2 "ssrf-cloud-e2.subsurface-divelog.org"  // secondary (older) server in Germany
 #define CLOUD_HOST_PATTERN "ssrf-cloud-..\\.subsurface-divelog\\.org"
 #define CLOUD_HOST_GENERIC "cloud.subsurface-divelog.org"
 
