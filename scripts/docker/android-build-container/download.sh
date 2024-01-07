7d751a81608a3eae26d2cb9063187c39270d8397
Michael Keller <github@ike.ch>
2024-01-07 15:05:29 +1300

    Re-activated the GitHub action to build and publish the docker image.
    Signed-off-by: Michael Keller <github@ike.ch>

--- scripts/docker/android-build-container/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://download.qt.io/official_releases/qt/5.13/5.13.1/qt-opensource-linux-x64-5.13.1.run
-wget https://dl.google.com/android/repository/android-ndk-r18b-linux-x86_64.zip
-wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-wget https://ftp.osuosl.org/pub/blfs/conglomeration/cmake/cmake-3.13.2.tar.gz
