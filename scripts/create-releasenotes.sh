7d918aeef145d447b356bd4785aa7776430c4e06
Dirk Hohndel <dirk@hohndel.org>
2024-01-10 19:13:48 -0800

    CICD: gracefully handle non merge pushes
    Also improve the spacing of the merge text a tiny bit.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/create-releasenotes.sh
+++ scripts/create-releasenotes.sh
@@ -7,14 +7,20 @@
 json=$(gh pr list -s merged -S "$1" --json title,number,url)
 
 cp gh_release_notes_top gh_release_notes
+if [[ $json != "[]" ]]; then
 (
-	echo -n 'This build was created by [merging PR'
+	echo -n 'This build was created by [merging pull request '
 	echo -n $json | jq -j '.[0]|{number}|join(" ")'
-	echo -n '('
+	echo -n ' ('
 	echo -n $json | jq -j '.[0]|{title}|join(" ")'
 	echo -n ')]('
 	echo -n $json | jq -j '.[0]|{url}|join(" ")'
 	echo ' )'
 ) >> gh_release_notes
-
+else
+(
+	echo "This build was created by directly pushing to the Subsurface repo."
+	echo "The latest commit was [$(git log --pretty=format:"%an: '%s'" -n1)](https://github.com/subsurface/subsurface/commit/$1 )"
+) >> gh_release_notes
+fi
 cat gh_release_notes_bottom >> gh_release_notes
