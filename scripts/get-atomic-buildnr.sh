8fb504941e22f0e96588685bc6fb8e3b442e7ce8
Dirk Hohndel <dirk@hohndel.org>
2024-01-10 16:36:09 -0800

    the path confusion is vert annoying
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/get-atomic-buildnr.sh
+++ scripts/get-atomic-buildnr.sh
@@ -11,16 +11,21 @@
 # checkout the nightly-builds repo in parallel to the main repo
 # the clone followed by the pointless push should verify that the password is stored in the config
 # that way the script doesn't need the password
-cd ..
+SCRIPT_SOURCE=$(dirname "${BASH_SOURCE[0]}")
+PARENT_DIR=$(cd "$SCRIPT_SOURCE"/../.. && pwd)
 url="https://subsurface:$2@github.com/subsurface/nightly-builds"
+cd "$PARENT_DIR"
 git clone -b main https://github.com/subsurface/nightly-builds
 cd nightly-builds
 git remote set-url origin "$url"
 git push origin main
 echo "build number prior to get-or-create was $(<latest-subsurface-buildnumber)"
-cd ..
+cd "$PARENT_DIR"
 bash -x subsurface/scripts/get-or-create-build-nr.sh "$1"
 echo "build number after get-or-create is $(<nightly-builds/latest-subsurface-buildnumber)"
-cp nightly-builds/latest-subsurface-buildnumber subsurface
+cp nightly-builds/latest-subsurface-buildnumber subsurface/
 [[ -n $3 ]] && echo "$3" > subsurface/latest-subsurface-buildnumber-extension
 bash subsurface/scripts/get-version > subsurface/release-version
+echo "$PARENT_DIR"
+ls -l "$PARENT_DIR"/subsurface/release-version
+cat "$PARENT_DIR"/subsurface/release-version
