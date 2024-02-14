b98c35ab52bc1b0c985b092a96cfdbe67b7f21e3
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2024-02-14 21:29:48 +0100

    events: make event severity explicit
    Currently the event type code uses libdivecomputer's flags
    to differentiate between events. Make this explicit and extract
    the event severity.
    
    The reason is that later we want to be more explicit about showing/
    hiding events and thereto we must format the name of events.
    
    Moreover, this encapsulates the complexities of extracting
    the severity in the event code (that used to be in the profile
    code).
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/event.h
+++ core/event.h
@@ -12,6 +12,13 @@
 extern "C" {
 #endif
 
+enum event_severity {
+	EVENT_SEVERITY_NONE = 0,
+	EVENT_SEVERITY_INFO,
+	EVENT_SEVERITY_WARN,
+	EVENT_SEVERITY_ALARM
+};
+
 /*
  * Events are currently based straight on what libdivecomputer gives us.
  *  We need to wrap these into our own events at some point to remove some of the limitations.
@@ -46,11 +53,11 @@ extern void free_events(struct event *ev);
 extern struct event *create_event(unsigned int time, int type, int flags, int value, const char *name);
 extern struct event *clone_event_rename(const struct event *ev, const char *name);
 extern bool same_event(const struct event *a, const struct event *b);
+extern enum event_severity get_event_severity(const struct event *ev);
 
 /* Since C doesn't have parameter-based overloading, two versions of get_next_event. */
 extern const struct event *get_next_event(const struct event *event, const char *name);
 extern struct event *get_next_event_mutable(struct event *event, const char *name);
-
 
 #ifdef __cplusplus
 }
