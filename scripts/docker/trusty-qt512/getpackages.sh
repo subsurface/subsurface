b2f23c33c01f5a470000f2f25d77b3404bf3e9ef
=Michael Keller <github@ike.ch>
2024-05-08 22:39:06 +1200

    CICD: Update the AppImage Build to Use ubuntu 16.04.
    Update the linux AppImage build to use ubuntu 16.04, and simplify it to
    a single workflow running on a vanilla docker image.
    
    This still uses the upload-artifact@v3 Action that will be EOL in
    November 2024, because v4 relies on node 20 which has an unmet glibc
    dependency in ubuntu 16.04. But this workflow can be updated to run on
    ubuntu 18.04 by a simple search / replace and disabling some 16.04
    specific PPAs.
    
    @dirkh, @probonopd: I have moved this here from #4183 to be able to
    review and discuss it without the noise of the workflow cleanup.
    
    The workflow now also publishes the AppImage as an artifact on pull
    request builds, available under Checks / Details / Summary.
    
    Signed-off-by: Michael Keller <github@ike.ch>

--- scripts/docker/trusty-qt512/getpackages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.sh
-curl -O https://dbus.freedesktop.org/releases/dbus/dbus-1.12.16.tar.gz
-wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1i.tar.gz
