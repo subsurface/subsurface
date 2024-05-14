subsurface/subsurface

commit da7ea17b6621e9c5611d0ba76b8f0e41c28038f8

cleanup: replace fprintf to stderr by report_info()
Let's try to unify debugging output!

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

core/serial_ftdi.c
@@ -37,14 +37,9 @@
 #include <time.h>       // nanosleep
 #endif
 
-#ifndef __ANDROID__
-#define INFO(context, fmt, ...)	fprintf(stderr, "INFO: " fmt "\n", ##__VA_ARGS__)
-#define ERROR(context, fmt, ...)	fprintf(stderr, "ERROR: " fmt "\n", ##__VA_ARGS__)
-#else
-#include <android/log.h>
-#define INFO(context, fmt, ...)	__android_log_print(ANDROID_LOG_DEBUG, __FILE__, "INFO: " fmt "\n", ##__VA_ARGS__)
-#define ERROR(context, fmt, ...)	__android_log_print(ANDROID_LOG_DEBUG, __FILE__, "ERROR: " fmt "\n", ##__VA_ARGS__)
-#endif
+#include "errorhelper.h"
+#define INFO(context, fmt, ...)	report_info(stderr, "INFO: " fmt, ##__VA_ARGS__)
+#define ERROR(context, fmt, ...)	report_info(stderr, "ERROR: " fmt, ##__VA_ARGS__)
 //#define SYSERROR(context, errcode)	ERROR(__FILE__ ":" __LINE__ ": %s", strerror(errcode))
 #define SYSERROR(context, errcode)	;
 
@@ -161,7 +156,7 @@ static int serial_ftdi_open_device (struct ftdi_context *ftdi_ctx)
 	for (i = 0; i < num_accepted_pids; i++) {
 		pid = accepted_pids[i];
 		ret = ftdi_usb_open (ftdi_ctx, VID, pid);
-		INFO(0, "FTDI tried VID %04x pid %04x ret %d\n", VID, pid, ret);
+		INFO(0, "FTDI tried VID %04x pid %04x ret %d", VID, pid, ret);
 		if (ret == -3) // Device not found
 			continue;
 		else
@@ -257,7 +252,7 @@ static dc_status_t serial_ftdi_close (void *io)
 
 	int ret = ftdi_usb_close(device->ftdi_ctx);
 	if (ret < 0) {
-		ERROR (device->context, "Unable to close the ftdi device : %d (%s)\n",
+		ERROR (device->context, "Unable to close the ftdi device : %d (%s)",
 		       ret, ftdi_get_error_string(device->ftdi_ctx));
 		return ret;
 	}

commands/command_pictures.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include "command_pictures.h"
+#include "core/errorhelper.h"
 #include "core/subsurface-qt/divelistnotifier.h"
 #include "qt-models/divelocationmodel.h"
 
@@ -24,7 +25,7 @@ void SetPictureOffset::redo()
 {
 	picture *pic = dive_get_picture(d, filename);
 	if (!pic) {
-		fprintf(stderr, "SetPictureOffset::redo(): picture disappeared!\n");
+		report_info("SetPictureOffset::redo(): picture disappeared!");
 		return;
 	}
 	std::swap(pic->offset, offset);
@@ -73,7 +74,7 @@ static std::vector<PictureListForAddition> removePictures(std::vector<PictureLis
 		for (const std::string &fn: list.filenames) {
 			int idx = get_picture_idx(&list.d->pictures, fn.c_str());
 			if (idx < 0) {
-				fprintf(stderr, "removePictures(): picture disappeared!\n");
+				report_info("removePictures(): picture disappeared!");
 				continue; // Huh? We made sure that this can't happen by filtering out non-existent pictures.
 			}
 			filenames.push_back(QString::fromStdString(fn));
@@ -103,7 +104,7 @@ static std::vector<PictureListForDeletion> addPictures(std::vector<PictureListFo
 		for (const PictureObj &pic: list.pics) {
 			int idx = get_picture_idx(&list.d->pictures, pic.filename.c_str()); // This should *not* already exist!
 			if (idx >= 0) {
-				fprintf(stderr, "addPictures(): picture disappeared!\n");
+				report_info("addPictures(): picture disappeared!");
 				continue; // Huh? We made sure that this can't happen by filtering out existing pictures.
 			}
 			picsForSignal.push_back(pic);

core/checkcloudconnection.cpp
@@ -35,7 +35,7 @@ CheckCloudConnection::CheckCloudConnection(QObject *parent) :
 bool CheckCloudConnection::checkServer()
 {
 	if (verbose)
-		fprintf(stderr, "Checking cloud connection...\n");
+		report_info("Checking cloud connection...");
 
 	QEventLoop loop;
 	QNetworkAccessManager *mgr = new QNetworkAccessManager();

core/dive.cpp
@@ -658,7 +658,7 @@ extern "C" void update_setpoint_events(const struct dive *dive, struct divecompu
 		ev->value = new_setpoint;
 	} else {
 		if (!add_event(dc, 0, SAMPLE_EVENT_PO2, 0, new_setpoint, "SP change"))
-			fprintf(stderr, "Could not add setpoint change event\n");
+			report_info("Could not add setpoint change event");
 	}
 }
 
@@ -3063,7 +3063,7 @@ extern "C" struct dive *make_first_dc(const struct dive *d, int dc_number)
 		;
 	if (!dc) {
 		free(newdc);
-		fprintf(stderr, "data inconsistent: can't find the current DC");
+		report_info("data inconsistent: can't find the current DC");
 		return res;
 	}
 	dc->next = old_dc->next;
@@ -3386,7 +3386,7 @@ extern "C" struct dive *get_dive_by_uniq_id(int id)
 	}
 #ifdef DEBUG
 	if (dive == NULL) {
-		fprintf(stderr, "Invalid id %x passed to get_dive_by_diveid, try to fix the code\n", id);
+		report_info("Invalid id %x passed to get_dive_by_diveid, try to fix the code", id);
 		exit(1);
 	}
 #endif
@@ -3404,7 +3404,7 @@ extern "C" int get_idx_by_uniq_id(int id)
 	}
 #ifdef DEBUG
 	if (dive == NULL) {
-		fprintf(stderr, "Invalid id %x passed to get_dive_by_diveid, try to fix the code\n", id);
+		report_info("Invalid id %x passed to get_dive_by_diveid, try to fix the code", id);
 		exit(1);
 	}
 #endif

core/divelog.cpp
@@ -3,6 +3,7 @@
 #include "divelist.h"
 #include "divesite.h"
 #include "device.h"
+#include "errorhelper.h"
 #include "filterpreset.h"
 #include "trip.h"
 
@@ -70,7 +71,7 @@ void divelog::clear()
 	while (sites->nr)
 		delete_dive_site(get_dive_site(0, sites), sites);
 	if (trips->nr != 0) {
-		fprintf(stderr, "Warning: trip table not empty in divelog::clear()!\n");
+		report_info("Warning: trip table not empty in divelog::clear()!");
 		trips->nr = 0;
 	}
 	clear_device_table(devices);

core/divesite.c
@@ -4,6 +4,7 @@
 #include "dive.h"
 #include "divelist.h"
 #include "divelog.h"
+#include "errorhelper.h"
 #include "membuffer.h"
 #include "subsurface-string.h"
 #include "table.h"
@@ -371,17 +372,17 @@ void add_dive_to_dive_site(struct dive *d, struct dive_site *ds)
 {
 	int idx;
 	if (!d) {
-		fprintf(stderr, "Warning: add_dive_to_dive_site called with NULL dive\n");
+		report_info("Warning: add_dive_to_dive_site called with NULL dive");
 		return;
 	}
 	if (!ds) {
-		fprintf(stderr, "Warning: add_dive_to_dive_site called with NULL dive site\n");
+		report_info("Warning: add_dive_to_dive_site called with NULL dive site");
 		return;
 	}
 	if (d->dive_site == ds)
 		return;
 	if (d->dive_site) {
-		fprintf(stderr, "Warning: adding dive that already belongs to a dive site to a different site\n");
+		report_info("Warning: adding dive that already belongs to a dive site to a different site");
 		unregister_dive_from_dive_site(d);
 	}
 	idx = dive_table_get_insertion_index(&ds->dives, d);

core/equipment.c
@@ -16,6 +16,7 @@
 #include "dive.h"
 #include "divelist.h"
 #include "divelog.h"
+#include "errorhelper.h"
 #include "pref.h"
 #include "subsurface-string.h"
 #include "table.h"
@@ -460,7 +461,7 @@ cylinder_t *get_cylinder(const struct dive *d, int idx)
 	 * in the table to mark no-cylinder surface interavals. This is horrendous. Fix ASAP. */
 	// if (idx < 0 || idx >= d->cylinders.nr) {
 	if (idx < 0 || idx >= d->cylinders.nr + 1 || idx >= d->cylinders.allocated) {
-		fprintf(stderr, "Warning: accessing invalid cylinder %d (%d existing)\n", idx, d->cylinders.nr);
+		report_info("Warning: accessing invalid cylinder %d (%d existing)", idx, d->cylinders.nr);
 		return NULL;
 	}
 	return &d->cylinders.cylinders[idx];
@@ -469,7 +470,7 @@ cylinder_t *get_cylinder(const struct dive *d, int idx)
 cylinder_t *get_or_create_cylinder(struct dive *d, int idx)
 {
 	if (idx < 0) {
-		fprintf(stderr, "Warning: accessing invalid cylinder %d\n", idx);
+		report_info("Warning: accessing invalid cylinder %d", idx);
 		return NULL;
 	}
 	while (idx >= d->cylinders.nr)

core/file.cpp
@@ -125,7 +125,7 @@ static int try_to_open_db(const char *filename, std::string &mem, struct divelog
 	retval = sqlite3_open(filename, &handle);
 
 	if (retval) {
-		fprintf(stderr, "Database connection failed '%s'.\n", filename);
+		report_info("Database connection failed '%s'", filename);
 		return 1;
 	}
 
@@ -258,7 +258,7 @@ bool remote_repo_uptodate(const char *filename, struct git_info *info)
 	if (is_git_repository(filename, info) && open_git_repository(info)) {
 		std::string sha = get_sha(info->repo, info->branch);
 		if (!sha.empty() && current_sha == sha) {
-			fprintf(stderr, "already have loaded SHA %s - don't load again\n", sha.c_str());
+			report_info("already have loaded SHA %s - don't load again", sha.c_str());
 			return true;
 		}
 	}
@@ -314,7 +314,7 @@ extern "C" int parse_file(const char *filename, struct divelog *log)
 		std::string wl_name = std::string(filename, t - filename) + ".add";
 		auto [wl_mem, err] = readfile(wl_name.c_str());
 		if (err < 0) {
-			fprintf(stderr, "No file %s found. No WLog extensions.\n", wl_name.c_str());
+			report_info("No file %s found. No WLog extensions.", wl_name.c_str());
 			wl_mem.clear();
 		}
 		return datatrak_import(mem, wl_mem, log);

commit 9b80393c978e3913f2cdf22151162f23c66b6005

core: fix INFO() and ERROR() macros in core/serial_ftdi.cpp
Error introduced in da7ea17b66: the INFO() and ERROR() macros
pass stdout instead of the format string as first parameter
to report_error(). Ooooops. How did this ever pass the
compile tests!?

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

core/serial_ftdi.c
@@ -38,8 +38,8 @@
 #endif
 
 #include "errorhelper.h"
-#define INFO(context, fmt, ...)	report_info(stderr, "INFO: " fmt, ##__VA_ARGS__)
-#define ERROR(context, fmt, ...)	report_info(stderr, "ERROR: " fmt, ##__VA_ARGS__)
+#define INFO(context, fmt, ...)	report_info("INFO: " fmt, ##__VA_ARGS__)
+#define ERROR(context, fmt, ...)	report_info("ERROR: " fmt, ##__VA_ARGS__)
 //#define SYSERROR(context, errcode)	ERROR(__FILE__ ":" __LINE__ ": %s", strerror(errcode))
 #define SYSERROR(context, errcode)	;
 
@@ -467,7 +467,7 @@ static dc_status_t serial_ftdi_purge (void *io, dc_direction_t queue)
 
 	size_t input;
 	serial_ftdi_get_available (io, &input);
-	INFO (device->context, "Flush: queue=%u, input=%lu, output=%i", queue, input,
+	INFO (device->context, "Flush: queue=%u, input=%lu, output=%i", queue, (unsigned long)input,
 	      serial_ftdi_get_transmitted (device));
 
 	switch (queue) {