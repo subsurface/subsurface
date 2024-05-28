398cc2b6399f55de2f58d39b830ddcae2fd94aa1
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2024-05-28 19:13:57 +0200

    cleanup: remove localized snprintf() functions
    The last use of these functions was removed in ae299d5e663c.
    
    And that's a good thing, because snprintf-style interfaces
    make zero sense in times of variable-length character
    encodings.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/format.h
+++ core/format.h
@@ -12,22 +12,7 @@
 __printf(1, 2) QString qasprintf_loc(const char *cformat, ...);
 __printf(1, 0) QString vqasprintf_loc(const char *cformat, va_list ap);
 __printf(1, 2) std::string casprintf_loc(const char *cformat, ...);
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-__printf(3, 4) int snprintf_loc(char *dst, size_t size, const char *cformat, ...);
-__printf(3, 0) int vsnprintf_loc(char *dst, size_t size, const char *cformat, va_list ap);
-__printf(2, 3) int asprintf_loc(char **dst, const char *cformat, ...);
-__printf(2, 0) int vasprintf_loc(char **dst, const char *cformat, va_list ap);
-
-#ifdef __cplusplus
-}
-
 __printf(1, 2) std::string format_string_std(const char *fmt, ...);
-
 #endif
 
 #endif
