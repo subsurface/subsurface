52dffac8b64f09152662ca547d827f576450f939
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2024-06-09 06:32:08 +0200

    desktop: use std::string to format subtitles
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/save-profiledata.h
+++ core/save-profiledata.h
@@ -2,7 +2,9 @@
 #ifndef SAVE_PROFILE_DATA_H
 #define SAVE_PROFILE_DATA_H
 
+#include <string>
+
 int save_profiledata(const char *filename, bool selected_only);
-void save_subtitles_buffer(struct membuffer *b, struct dive *dive, int offset, int length);
+std::string save_subtitles_buffer(struct dive *dive, int offset, int length);
 
 #endif // SAVE_PROFILE_DATA_H
