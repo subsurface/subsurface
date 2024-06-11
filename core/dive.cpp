3fde6ca17ac032fd1f29a1d85aefdb8dd45dedc8
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2024-06-11 17:08:43 +0200

    core: remove structured_list.h
    No more users of this, since we switched to C++ containers.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- core/dive.cpp
+++ core/dive.cpp
@@ -27,7 +27,6 @@
 #include "sample.h"
 #include "tag.h"
 #include "trip.h"
-#include "structured_list.h"
 #include "fulltext.h"
 
 // For user visible text but still not translated
--- core/tag.cpp
+++ core/tag.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include "tag.h"
-#include "structured_list.h"
 #include "subsurface-string.h"
 #include "membuffer.h"
 #include "gettext.h"
