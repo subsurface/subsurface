subsurface/subsurface

commit 009e8a32bb5e37b25c2e8093f79fb724868413ef

Fix Windows versioning as well.
Signed-off-by: Michael Keller <github@ike.ch>

smtk-import/cmake/Modules/version.cmake
@@ -1,17 +1,26 @@
 message(STATUS "processing version.cmake")
-execute_process(
-	COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh
-	WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
-	OUTPUT_VARIABLE CANONICAL_VERSION_STRING
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
 
-execute_process(
-	COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh 4
-	WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
-	OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(DEFINED ENV{CANONICALVERSION})
+       set(CANONICAL_VERSION_STRING $ENV{CANONICALVERSION})
+else()
+       execute_process(
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh
+               WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
+               OUTPUT_VARIABLE CANONICAL_VERSION_STRING
+               OUTPUT_STRIP_TRAILING_WHITESPACE
+       )
+endif()
+
+if(DEFINED ENV{CANONICALVERSION_4})
+       set(CANONICAL_VERSION_STRING_4 $ENV{CANONICALVERSION_4})
+else()
+       execute_process(
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 4
+               WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
+               OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
+               OUTPUT_STRIP_TRAILING_WHITESPACE
+       )
+endif()
 
 configure_file(${SRC} ${DST} @ONLY)
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")

cmake/Modules/version.cmake
@@ -1,24 +1,26 @@
+message(STATUS "processing version.cmake")
 
-execute_process(
-	COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 4
-	WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
-	OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(DEFINED ENV{CANONICALVERSION})
+	set(CANONICAL_VERSION_STRING $ENV{CANONICALVERSION})
+else()
+	execute_process(
+		COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh
+		WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
+		OUTPUT_VARIABLE CANONICAL_VERSION_STRING
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+endif()
 
-execute_process(
-	COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 3
-	WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
-	OUTPUT_VARIABLE CANONICAL_VERSION_STRING_3
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-execute_process(
-	COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh
-	WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
-	OUTPUT_VARIABLE CANONICAL_VERSION_STRING
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(DEFINED ENV{CANONICALVERSION_4})
+	set(CANONICAL_VERSION_STRING_4 $ENV{CANONICALVERSION_4})
+else()
+	execute_process(
+		COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 4
+		WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
+		OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+endif()
 
 configure_file(${SRC} ${DST} @ONLY)
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")

.github/workflows/windows.yml
@@ -43,6 +43,9 @@ jobs:
         bash -x subsurface/packaging/windows/container-prep.sh 2>&1 | tee pre-build.log
 
     - name: run build
+      env:
+        CANONICALVERSION: ${{ steps.version_number.outputs.version }}
+        CANONICALVERSION_4: ${{ steps.version_number.outputs.version_4 }}
       run: |
         export OUTPUT_DIR="$GITHUB_WORKSPACE"
         cd /win

packaging/ios/build.sh
@@ -77,12 +77,15 @@ if [[ $QT_VERSION = 5.15* ]] ; then
 fi
 
 # set up the Subsurface versions by hand
-CANONICALVERSION=$("$SUBSURFACE_SOURCE"/scripts/get-version.sh)
+if [ -z "${CANONICALVERSION+X}" ] ; then
+        CANONICALVERSION=$("$SUBSURFACE_SOURCE"/scripts/get-version.sh)
+fi
 echo "#define CANONICAL_VERSION_STRING \"$CANONICALVERSION\"" > "$SUBSURFACE_SOURCE"/ssrf-version.h
-CANONICALVERSION_4=$("$SUBSURFACE_SOURCE"/scripts/get-version.sh 4)
+if [ -z "${CANONICALVERSION_4+X}" ] ; then
+        CANONICALVERSION_4=$("$SUBSURFACE_SOURCE"/scripts/get-version.sh 4)
+fi
 echo "#define CANONICAL_VERSION_STRING_4 \"$CANONICALVERSION_4\"" >> "$SUBSURFACE_SOURCE"/ssrf-version.h
-CANONICALVERSION_3=$("$SUBSURFACE_SOURCE"/scripts/get-version.sh 3)
-echo "#define CANONICAL_VERSION_STRING_3 \"$CANONICALVERSION_3\"" >> "$SUBSURFACE_SOURCE"/ssrf-version.h
+CANONICALVERSION_3=${CANONICALVERSION_4%.*}
 
 BUNDLE=org.subsurface-divelog.subsurface-mobile
 if [ "${IOS_BUNDLE_PRODUCT_IDENTIFIER}" != "" ] ; then

packaging/windows/mxe-based-build.sh
@@ -137,12 +137,11 @@ else
 	touch Release
 fi
 
-
+cd "$BASEDIR"/subsurface
 
 # libdivecomputer
 # ensure the git submodule is present and the autotools are set up
 
-cd "$BASEDIR"/subsurface
 if [ ! -d libdivecomputer/src ] ; then
 	git submodule init
 	git submodule update --recursive

scripts/get-atomic-buildnr.sh
@@ -6,7 +6,6 @@
 #
 # Usage: get-atomic-buildnr.sh SHA secrets.NIGHTLY_BUILDS [extra-name-component]
 #
-# the resulting release version is stored in the file ./release-version
 
 # checkout the nightly-builds repo in parallel to the main repo
 # the clone followed by the pointless push should verify that the password is stored in the config
@@ -25,4 +24,3 @@ bash subsurface/scripts/get-or-create-build-nr.sh "$1"
 echo "build number after get-or-create is $(<nightly-builds/latest-subsurface-buildnumber)"
 cp nightly-builds/latest-subsurface-buildnumber subsurface/
 [[ -n $3 ]] && echo "$3" > subsurface/latest-subsurface-buildnumber-extension
-bash subsurface/scripts/get-version.sh > subsurface/release-version

commit 2db9c6096a25306df7a379a69f301e99c342896b

Windows: Fix smtk-import Build.
Fix a copy/paste error introduced in #4273.

Signed-off-by: Michael Keller <github@ike.ch>

smtk-import/cmake/Modules/version.cmake
@@ -4,7 +4,7 @@ if(DEFINED ENV{CANONICALVERSION})
        set(CANONICAL_VERSION_STRING $ENV{CANONICALVERSION})
 else()
        execute_process(
-               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh
                WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
                OUTPUT_VARIABLE CANONICAL_VERSION_STRING
                OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -15,7 +15,7 @@ if(DEFINED ENV{CANONICALVERSION_4})
        set(CANONICAL_VERSION_STRING_4 $ENV{CANONICALVERSION_4})
 else()
        execute_process(
-               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 4
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh 4
                WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
                OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
                OUTPUT_STRIP_TRAILING_WHITESPACE