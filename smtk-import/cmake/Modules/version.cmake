48b4308a7d9858dc494afb93bfe453780f96cb7f
Michael Keller <github@ike.ch>
2024-08-14 18:13:54 +1200

    Windows: Fix smtk-import Build.
    Fix a copy/paste error introduced in #4273.
    
    Signed-off-by: Michael Keller <github@ike.ch>

--- smtk-import/cmake/Modules/version.cmake
+++ smtk-import/cmake/Modules/version.cmake
@@ -4,7 +4,7 @@ if(DEFINED ENV{CANONICALVERSION})
        set(CANONICAL_VERSION_STRING $ENV{CANONICALVERSION})
 else()
        execute_process(
-               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh
                WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
                OUTPUT_VARIABLE CANONICAL_VERSION_STRING
                OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -15,7 +15,7 @@ if(DEFINED ENV{CANONICALVERSION_4})
        set(CANONICAL_VERSION_STRING_4 $ENV{CANONICALVERSION_4})
 else()
        execute_process(
-               COMMAND bash ${CMAKE_TOP_SRC_DIR}/scripts/get-version.sh 4
+               COMMAND bash ${CMAKE_TOP_SRC_DIR}/../scripts/get-version.sh 4
                WORKING_DIRECTORY ${CMAKE_TOP_SRC_DIR}
                OUTPUT_VARIABLE CANONICAL_VERSION_STRING_4
                OUTPUT_STRIP_TRAILING_WHITESPACE
