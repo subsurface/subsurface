subsurface/subsurface

commit 2d8e343221e3f0c89d459a29f603a92f9893ab70

Planner: Improve Gas Handling in CCR Mode.
This has become a bit of a catch-all overhaul of a large portion of the
planner - I started out wanting to improve the CCR mode, but then as I
started pulling all the other threads that needed addressing started to
come with it.

Improve how the gas selection is handled when planning dives in CCR
mode, by making the type (OC / CCR) of segments dependent on the gas use
type that was set for the selected gas.
Add a preference to allow the user to chose to use OC gases as diluent,
in a similar fashion to the original implementation.
Hide gases that cannot be used in the currently selected dive mode in
all drop downs.
Include usage type in gas names if this is needed.
Hide columns and disable elements in the 'Dive planner points' table if
they can they can not be edited in the curently selected dive mode.
Visually identify gases and usage types that are not appropriate for the
currently selected dive mode.
Move the 'Dive mode' selection to the top of the planner view, to
accommodate the fact that this is a property of the dive and not a
planner setting.
Show a warning instead of the dive plan if the plan contains gases that
are not usable in the selected dive mode.
Fix the data entry for the setpoint in the 'Dive planner points' table.
Fix problems with enabling / disabling planner settings when switching
between dive modes.
Refactor some names to make them more appropriate for their current
usage.

One point that is still open is to hide gas usage graphs in the planner
profile if the gas isn't used for OC, as there is no way to meaningfully
interpolate such usage.

Signed-off-by: Michael Keller <github@ike.ch>

core/plannernotes.cpp
@@ -96,7 +96,7 @@ extern std::string get_planner_disclaimer_formatted()
 	return format_string_std(get_planner_disclaimer(), deco);
 }
 
-void add_plan_to_notes(struct diveplan *diveplan, struct dive *dive, bool show_disclaimer, bool error)
+void add_plan_to_notes(struct diveplan *diveplan, struct dive *dive, bool show_disclaimer, planner_error_t error)
 {
 	std::string buf;
 	std::string icdbuf;
@@ -122,12 +122,31 @@ void add_plan_to_notes(struct diveplan *diveplan, struct dive *dive, bool show_d
 	if (!dp)
 		return;
 
-	if (error) {
+	if (error != PLAN_OK) {
+		const char *message;
+		switch (error) {
+		case PLAN_ERROR_TIMEOUT:
+			message = translate("gettextFromC", "Decompression calculation aborted due to excessive time");
+
+			break;
+		case PLAN_ERROR_INAPPROPRIATE_GAS:
+			message = translate("gettextFromC", "One or more tanks with a tank use type inappropriate for the selected dive mode are included in the dive plan. "
+				"Please change them to appropriate tanks to enable the generation of a dive plan.");
+
+			break;
+		default:
+			message = translate("gettextFromC", "An error occurred during dive plan generation");
+
+			break;
+		}
+
 		buf += format_string_std("<span style='color: red;'>%s </span> %s<br/>",
-				translate("gettextFromC", "Warning:"),
-				translate("gettextFromC", "Decompression calculation aborted due to excessive time"));
+			translate("gettextFromC", "Warning:"), message);
+
 		// TODO: avoid copy
 		dive->notes = buf;
+		dive->notes = strdup(buf.c_str());
+
 		return;
 	}
 

commit bb70d5eb34d06cb54a7211ffe5eade5ecebfe12b

planner: remove unnecessary strdup()
This leaks memory and was (probably unintentionally) introduced
in 2d8e343221e3f0c89d459a29f603a92f9893ab70.

Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

core/plannernotes.cpp
@@ -145,7 +145,6 @@ void add_plan_to_notes(struct diveplan *diveplan, struct dive *dive, bool show_d
 
 		// TODO: avoid copy
 		dive->notes = buf;
-		dive->notes = strdup(buf.c_str());
 
 		return;
 	}