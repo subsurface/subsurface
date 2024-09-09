subsurface/subsurface

commit 14b9074f407ccfa979095ab37854a8dd129d885c

Fix layering violations in DivePlannerPointsModel.
Signed-off-by: Michael Keller <mikeller@042.ch>

desktop-widgets/diveplanner.cpp
@@ -677,7 +677,9 @@ void PlannerWidgets::printDecoPlan()
 
 void PlannerWidgets::setDiveMode(int mode)
 {
-	DivePlannerPointsModel::instance()->setDiveMode(mode);
+	planned_dive->get_dc(dcNr)->divemode = (divemode_t)mode;
+	DivePlannerPointsModel::instance()->cylindersChanged();
+
 	plannerWidget.setColumnVisibility(mode);
 	plannerSettingsWidget.setBailoutVisibility(mode);
 }

mobile-widgets/qmlinterface.h
@@ -235,7 +235,10 @@ public slots:
 	void set_ascrate75(int value) { DivePlannerPointsModel::instance()->setAscrate75Display(value); }
 	void set_descrate(int value) { DivePlannerPointsModel::instance()->setDescrateDisplay(value); }
 
-	void set_dive_mode(DIVE_MODE value) { DivePlannerPointsModel::instance()->setDiveMode((int)value); }
+	void set_dive_mode(DIVE_MODE value) {
+		//TODO: Actually change the dive mode in the dive here - this will need untangling to enable access to the dive
+		DivePlannerPointsModel::instance()->cylindersChanged();
+	}
 	void set_planner_deco_mode(DECO_MODE value) { PlannerShared::set_planner_deco_mode((deco_mode)value); }
 	void set_reserve_gas(int value) { PlannerShared::set_reserve_gas(value); }
 	void set_safetystop(bool value) { DivePlannerPointsModel::instance()->setSafetyStop(value); }

qt-models/diveplannermodel.cpp
@@ -588,11 +588,10 @@ int DivePlannerPointsModel::gfLow() const
 	return diveplan.gflow;
 }
 
-void DivePlannerPointsModel::setDiveMode(int mode)
+void DivePlannerPointsModel::cylindersChanged()
 {
-	struct divecomputer *dc = d->get_dc(dcNr);
-	if (dc)
-		dc->divemode = (divemode_t) mode;
+	if (!d)
+		return;
 
 	cylinders.updateDive(d, dcNr);
 

qt-models/diveplannermodel.h
@@ -66,7 +66,7 @@ public:
 
 	void loadFromDive(dive *d, int dcNr);
 	void addStop(int millimeters, int seconds);
-	void setDiveMode(int mode);
+	void cylindersChanged();
 public
 slots:
 	void addDefaultStop();

commit 515f593a56064f6b287eca23b12ad2d75a59138b

Planner: Start with Correct Dive Mode.
Fix a bug introduced in #4245, causing an incorrect dive mode to be
selected when starting the dive planner from a CCR dive.

Signed-off-by: Michael Keller <github@ike.ch>

desktop-widgets/diveplanner.cpp
@@ -594,7 +594,7 @@ void PlannerWidgets::preparePlanDive(const dive *currentDive, int currentDcNr)
 			plannerWidget.setSalinity(SEAWATER_SALINITY);
 	}
 
-	setDiveMode(getDiveMode());
+	plannerWidget.setDiveMode(getDiveMode());
 }
 
 void PlannerWidgets::planDive()
@@ -611,7 +611,7 @@ void PlannerWidgets::prepareReplanDive(const dive *currentDive, int currentDcNr)
 	copy_dive(currentDive, planned_dive.get());
 	dcNr = currentDcNr;
 
-	setDiveMode(getDiveMode());
+	plannerWidget.setDiveMode(getDiveMode());
 }
 
 void PlannerWidgets::replanDive()