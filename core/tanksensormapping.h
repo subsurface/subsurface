c3819f239d2edefa4626f85d66c0f24b66971d34
Michael Keller <github@ike.ch>
2025-03-09 01:42:58 +1300

    Added loading / storing of the tank sensor mapping.
    Signed-off-by: Michael Keller <github@ike.ch>

---
+++ core/tanksensormapping.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef TANKSENSORMAPPING_H
+#define TANKSENSORMAPPING_H
+
+struct tank_sensor_mapping {
+	unsigned int sensor_index;
+	int cylinder_id;
+};
+
+#endif
