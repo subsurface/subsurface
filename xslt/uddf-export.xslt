subsurface/subsurface

commit ab7217a94c73f2ab0d9d67552aff54c12ecb1b1a

Export: Fix 'visibility' in UDDF Export.
Make the export of UDDF files compliant with the UDDF 3.2.1
specification for the `visibility` element.

Fixes #4445.

Signed-off-by: Michael Keller <github@ike.ch>

xslt/uddf-export.xslt
@@ -687,25 +687,27 @@
             </ratingvalue>
           </rating>
         </xsl:if>
-        <visibility>
-            <xsl:choose>
-              <xsl:when test="./@visibility = 1">
-                <xsl:value-of select="1"/>
-              </xsl:when>
-              <xsl:when test="./@visibility = 2">
-                <xsl:value-of select="3"/>
-              </xsl:when>
-              <xsl:when test="./@visibility = 3">
-                <xsl:value-of select="5"/>
-              </xsl:when>
-              <xsl:when test="./@visibility = 4">
-                <xsl:value-of select="10"/>
-              </xsl:when>
-              <xsl:when test="./@visibility = 5">
-                <xsl:value-of select="15"/>
-              </xsl:when>
-            </xsl:choose>
-        </visibility>
+        <xsl:if test="./@visibility">
+          <visibility>
+              <xsl:choose>
+                <xsl:when test="./@visibility = 1">
+                  <xsl:value-of select="1"/>
+                </xsl:when>
+                <xsl:when test="./@visibility = 2">
+                  <xsl:value-of select="3"/>
+                </xsl:when>
+                <xsl:when test="./@visibility = 3">
+                  <xsl:value-of select="5"/>
+                </xsl:when>
+                <xsl:when test="./@visibility = 4">
+                  <xsl:value-of select="10"/>
+                </xsl:when>
+                <xsl:otherwise>
+                  <xsl:value-of select="15"/>
+                </xsl:when>
+              </xsl:choose>
+          </visibility>
+        </xsl:if>
       </informationafterdive>
 
     </dive>

commit 48fcec6a07ac243c945ce9445a8c3b46b7ddbc60

Fix UDDF export error introduced in #4459
Signed-off-by: Michael Keller <github@ike.ch>

xslt/uddf-export.xslt
@@ -704,7 +704,7 @@
                 </xsl:when>
                 <xsl:otherwise>
                   <xsl:value-of select="15"/>
-                </xsl:when>
+                </xsl:otherwise>
               </xsl:choose>
           </visibility>
         </xsl:if>