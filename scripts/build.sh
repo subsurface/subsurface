2d30458630b75a9bcbad18bc8e9be732b0132122
Michael Keller <github@ike.ch>
2025-05-11 23:05:38 +1200

    Bind libmtp to a Fixed Version.
    Bind libmtp to v1.1.22 to make builds more reproducible, and to prevent
    our builds from breaking.
    
    Signed-off-by: Michael Keller <github@ike.ch>

--- scripts/build.sh
+++ scripts/build.sh
@@ -509,7 +509,7 @@ if [[ $PLATFORM = Darwin && "$BUILD_DEPS" == "1" ]] ; then
 
 	./${SRC_DIR}/scripts/get-dep-lib.sh single . libmtp
 	pushd libmtp
-	patch -p1 < ../${SRC_DIR}/scripts/libmtp.patch || true
+	autoupdate
 	echo 'N' | NOCONFIGURE="1" bash ./autogen.sh
 	CFLAGS="$MAC_OPTS" ./configure --prefix="$INSTALL_ROOT"
 	make -j4
