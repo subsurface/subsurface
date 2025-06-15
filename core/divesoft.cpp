e6703e6f22360e114aa3d8046e4625a2820a33e0
Michael Keller <github@ike.ch>
2025-06-16 11:22:04 +1200

    Divesoft: Fix the Log File Import and Add New Liberty Model.
    Fix the import of Divesoft log files to default to the Freedom model if
    an unknown model identifier is encountered.
    Also add support for the 'Liberty 2021' model identifier.
    
    Fixes #4536.
    
    Signed-off-by: Michael Keller <github@ike.ch>

--- core/divesoft.cpp
+++ core/divesoft.cpp
@@ -16,6 +16,7 @@
 // As supplied by Divesoft
 static constexpr std::string_view divesoft_liberty_serial_prefix = "7026";
 static constexpr std::string_view divesoft_freedom_serial_prefix = "7044";
+static constexpr std::string_view divesoft_liberty_2021_serial_prefix = "7203";
 static constexpr std::string_view divesoft_freedom_plus_serial_prefix = "7273";
 
 // From libdivecomputer
@@ -25,11 +26,16 @@ static constexpr int divesoft_freedom_model = 19;
 int divesoft_import(const std::string &buffer, struct divelog *log)
 {
 	std::string model_identifier = buffer.substr(52, 4);
-	int model = 0;
-	if (model_identifier == divesoft_liberty_serial_prefix)
+	int model;
+	if (model_identifier == divesoft_liberty_serial_prefix || model_identifier == divesoft_liberty_2021_serial_prefix) {
 		model = divesoft_liberty_model;
-	else if (model_identifier == divesoft_freedom_serial_prefix || model_identifier == divesoft_freedom_plus_serial_prefix)
+	} else if (model_identifier == divesoft_freedom_serial_prefix || model_identifier == divesoft_freedom_plus_serial_prefix) {
 		model = divesoft_freedom_model;
+	} else {
+		report_error(translate("gettextFromC", "Unknown Divesoft model %s, using Freedom as default"), model_identifier.c_str());
+
+		model = divesoft_freedom_model;
+	}
 
 	device_data_t devdata;
 	devdata.log = log;
