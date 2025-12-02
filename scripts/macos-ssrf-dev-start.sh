f15e0323a29e0ee1918aa0a9eaca6b8bc723c909
Dirk Hohndel <dirk@hohndel.org>
2025-12-01 22:37:06 -0800

    improve the macOS helper script
    Test for a sane Qt install, make sure we build and install the user manual.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/macos-ssrf-dev-start.sh
+++ scripts/macos-ssrf-dev-start.sh
@@ -8,18 +8,29 @@ clang --version > /dev/null 2>&1 || { echo "Clang not working"; exit 1; }
 qtver=$(qmake -query QT_VERSION 2>/dev/null) || { echo "Qt not found in PATH"; exit 1; }
 [[ "$(printf '%s\n' "6.8.3" "$qtver" | sort -V | head -n1)" == "6.8.3" ]] || { echo "Qt $qtver < 6.8.3"; exit 1; }
 
+# check if this looks like Qt binaries (vs Homebrew or something else)
+grep -q "$qtver" <(qmake -query QT_INSTALL_LIBS) || { echo "this doesn't look like it's finding a Qt binary distribution";  exit 1;}
+
+# check for asciidoc
+hash asciidoc >&/dev/null || { echo "missing asciido - install from Homebrew"; exit 1; }
+
 # clone the repo
 # WARNING -- this is cloning the macos-qt6 branch... will need to be updated once merged
 git clone -b dirkhh-macos-qt6 https://github.com/subsurface/subsurface
 
+# build the user manual
+pushd subsurface/Documentation || { echo "can't cd to subsurface/Documentation"; exit 1; }
+make doc || { echo "failed to create HTML files"; exit 1; }
+popd
+
 # build the dependencies
 export PKG_CONFIG_LIBDIR=$(pwd)/install-root/lib/pkgconfig
-bash -e -x ./subsurface/scripts/build.sh -build-deps-only -ftdi 
+bash -e -x ./subsurface/scripts/build.sh -build-deps-only -ftdi
 
 mv build.log build-deps.log
 
 # build Subsurface with Qt6
-bash -e -x ./subsurface/scripts/build.sh -desktop -build-with-qt6 -ftdi
+bash -e -x ./subsurface/scripts/build.sh -desktop -build-with-qt6 -ftdi -install-docs
 
 cd subsurface/build
 codesign --force --deep --sign - Subsurface.app
