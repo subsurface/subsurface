5c1bfd4220c484f33a46b5b32b3a50ce985e155b
Dirk Hohndel <dirk@hohndel.org>
2025-12-01 22:37:06 -0800

    improve the macOS helper script
    Test for a sane Qt install, make sure we build and install the user manual.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/macos-ssrf-dev-start.sh
+++ scripts/macos-ssrf-dev-start.sh
@@ -13,7 +13,7 @@ clang --version > /dev/null 2>&1 || croak "Clang not working"
 
 # verify Homebrew is installed
 brew info > /dev/null 2>&1 || croak "Homebrew required"
-brew install autoconf automake libtool pkg-config gettext confuse \
+brew install autoconf automake libtool pkg-config gettext confuse aciidoctor \
      cmake libgit2 libraw libftdi libmtp hidapi libusb libssh2 libzip \
      qtbase qtconnectivity qt5compat qtlocation qtpositioning qttools qtdeclarative \
         || croak "brew install failed"
@@ -23,6 +23,9 @@ brew install autoconf automake libtool pkg-config gettext confuse \
 qtver=$(qmake -query QT_VERSION 2>/dev/null) || { echo "Qt not found in PATH"; exit 1; }
 [[ "$(printf '%s\n' "6.8.3" "$qtver" | sort -V | head -n1)" == "6.8.3" ]] || { echo "Qt $qtver < 6.8.3"; exit 1; }
 
+# check if this looks like Qt binaries (vs Homebrew or something else)
+grep -q "$qtver" <(qmake -query QT_INSTALL_LIBS) || { echo "this doesn't look like it's finding a Qt binary distribution";  exit 1;}
+
 # clone the repo
 # WARNING -- this is cloning the macos-qt6 branch... will need to be updated once merged
 [ ! -d subsurface ] && git clone -b dirkhh-macos-qt6 https://github.com/subsurface/subsurface
@@ -30,14 +33,23 @@ qtver=$(qmake -query QT_VERSION 2>/dev/null) || { echo "Qt not found in PATH"; e
 # if requested, clean the old build remnants
 [ "$CLEAN" = "1" ] && rm -rf googlemaps install-root subsurface/build subsurface/libdivecomputer/build
 
+# build the user manual
+pushd subsurface/Documentation || { echo "can't cd to subsurface/Documentation"; exit 1; }
+make doc || { echo "failed to create HTML files"; exit 1; }
+popd
+
+# build the dependencies
+export PKG_CONFIG_LIBDIR=$(pwd)/install-root/lib/pkgconfig
+bash -e -x ./subsurface/scripts/build.sh -build-deps-only -ftdi
+
 # # build the dependencies
 # export PKG_CONFIG_LIBDIR=$(pwd)/install-root/lib/pkgconfig
-# bash -e -x ./subsurface/scripts/build.sh -build-deps-only -ftdi 
-# 
+# bash -e -x ./subsurface/scripts/build.sh -build-deps-only -ftdi
+#
 # mv build.log build-deps.log
 
 # build Subsurface with Qt6
-bash -e -x ./subsurface/scripts/build.sh -desktop -build-with-qt6 -build-with-homebrew -ftdi
+bash -e -x ./subsurface/scripts/build.sh -desktop -build-with-qt6 -build-with-homebrew -ftdi -install-docs
 
 # not sure why I need to do this again here, but without this second install of googlemaps
 # I get no map in Subsurface
