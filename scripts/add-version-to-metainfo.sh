2082dbbe2151a7be3b52b8dbfb8b98c41fd3c64f
Dirk Hohndel <dirk@hohndel.org>
2025-12-18 22:31:09 -0800

    avoid pointless error during OBS packaging
    The error shows up in the log, but of course there are the two '||' statements
    that address the issue of git not being installed. So no point in printing that
    error.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/add-version-to-metainfo.sh
+++ scripts/add-version-to-metainfo.sh
@@ -5,8 +5,8 @@
 # this will, however, fail for plain tar balls create via git archive
 
 SCRIPT_DIR="$( cd "${BASH_SOURCE%/*}" ; pwd )"
-VERSION=$(git describe --match "v[0-9]*" --abbrev=12) || VERSION=$(cat "$SCRIPT_DIR"/../.gitversion)
-DATE=$(git log -1 --format="%ct" | xargs -I{} date -d @{} +%Y-%m-%d)
+VERSION=$(git describe --match "v[0-9]*" --abbrev=12 2>/dev/null) || VERSION=$(cat "$SCRIPT_DIR"/../.gitversion)
+DATE=$(git log -1 --format="%ct" 2>/dev/null | xargs -I{} date -d @{} +%Y-%m-%d)
 if [ "$DATE" = "" ] ; then
 	DATE=$(cat "$SCRIPT_DIR"/../.gitdate)
 fi
