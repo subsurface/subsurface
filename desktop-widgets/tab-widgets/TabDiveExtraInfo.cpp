f4ec205f68c9c22ab24c338127ce939dc5f85568
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2026-02-01 23:03:29 +0100

    desktop: show information on divecomputer tab
    Show divecomputer-specific data on the divecomputer tab, which
    was not shown up to now.
    
    Start with:
    model name, serial number, firmware version
    date, duration, dive mode
    
    UI was copied from the DiveInformation tab.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- desktop-widgets/tab-widgets/TabDiveExtraInfo.cpp
+++ desktop-widgets/tab-widgets/TabDiveExtraInfo.cpp
@@ -2,7 +2,9 @@
 #include "TabDiveExtraInfo.h"
 #include "ui_TabDiveExtraInfo.h"
 #include "core/dive.h"
+#include "core/gettextfromc.h"
 #include "core/selection.h"
+#include "core/string-format.h"
 #include "qt-models/divecomputerextradatamodel.h"
 
 TabDiveExtraInfo::TabDiveExtraInfo(MainTab *parent) :
@@ -23,15 +25,36 @@ TabDiveExtraInfo::~TabDiveExtraInfo()
 
 void TabDiveExtraInfo::updateData(const std::vector<dive *> &, dive *currentDive, int currentDC)
 {
-	if (currentDive)
-		extraDataModel->updateDiveComputer(currentDive->get_dc(currentDC));
+	if (currentDive) {
+		const divecomputer *dc = currentDive->get_dc(currentDC);
 
-	ui->extraData->setVisible(false); // This will cause the resize to include rows outside the current viewport
-	ui->extraData->resizeColumnsToContents();
-	ui->extraData->setVisible(true);
+		ui->model->setText(QString::fromStdString(dc->model));
+		ui->serial->setText(QString::fromStdString(dc->serial));
+		ui->firmware->setText(QString::fromStdString(dc->fw_version));
+		ui->date->setText(get_dive_date_string(dc->when));
+		ui->duration->setText(get_dive_duration_string(dc->duration.seconds, tr("h"), tr("min"), tr("sec"),
+				" ", dc->divemode == FREEDIVE));
+		if (dc->divemode >= 0 && dc->divemode < NUM_DIVEMODE)
+			ui->diveMode->setText(gettextFromC::tr(divemode_text_ui[dc->divemode]));
+		else
+			ui->diveMode->clear();
+
+		extraDataModel->updateDiveComputer(dc);
+		ui->extraData->setVisible(false); // This will cause the resize to include rows outside the current viewport
+		ui->extraData->resizeColumnsToContents();
+		ui->extraData->setVisible(true);
+	} else {
+		clear();
+	}
 }
 
 void TabDiveExtraInfo::clear()
 {
+	ui->model->clear();
+	ui->serial->clear();
+	ui->firmware->clear();
+	ui->date->clear();
+	ui->duration->clear();
+	ui->diveMode->clear();
 	extraDataModel->clear();
 }
