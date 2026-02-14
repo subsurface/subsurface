9adda5bc579e4c72a824ce6af0e452ced112244c
Dirk Hohndel <dirk@hohndel.org>
2026-02-14 14:16:43 -0800

    Windows/MSVC: use PowerShell for helper scripts
    While it's possible to use WSL/bash, that causes all kinds of problems with
    trying to get the paths right. Converting things to appropriate PowerShell
    scripts seems much cleaner.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- cmake/Modules/HandleVersionGeneration.cmake
+++ cmake/Modules/HandleVersionGeneration.cmake
@@ -7,6 +7,12 @@ file(WRITE ${CMAKE_BINARY_DIR}/version.h.in
 file(COPY cmake/Modules/version.cmake
     DESTINATION ${CMAKE_BINARY_DIR})
 
+if(MSVC)
+	set(USING_MSVC TRUE)
+else()
+	set(USING_MSVC FALSE)
+endif()
+
 add_custom_target(
 	version ALL COMMAND ${CMAKE_COMMAND}
 	-D SRC=${CMAKE_BINARY_DIR}/version.h.in
@@ -14,5 +20,6 @@ add_custom_target(
 	-D CMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
 	-D CMAKE_TOP_SRC_DIR=${CMAKE_SOURCE_DIR}
 	-D CMAKE_BINARY_DIR=${CMAKE_BINARY_DIR}
+	-D USING_MSVC=${USING_MSVC}
 	-P ${CMAKE_BINARY_DIR}/version.cmake
 )
