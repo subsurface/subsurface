d0ce71e9867f498a345b39767ec9a846d57effbd
Dirk Hohndel <dirk@hohndel.org>
2026-02-14 12:54:40 -0800

    Windows/MSVC: use cmake -E to execute commands
    This should make this more cross-platform compatible.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- cmake/Modules/RunOnBuildDir.cmake
+++ cmake/Modules/RunOnBuildDir.cmake
@@ -1,23 +1,21 @@
 # install a few things so that one can run Subsurface from the build
 # directory
+# Use cmake -E commands for cross-platform compatibility (works on Windows/MSVC)
 add_custom_target(themeLink ALL
-	COMMAND
-	rm -f ${CMAKE_BINARY_DIR}/theme &&
-	ln -sf ${CMAKE_SOURCE_DIR}/theme ${CMAKE_BINARY_DIR}/theme
+	COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/theme
+	COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/theme ${CMAKE_BINARY_DIR}/theme
 )
 if(NOT NO_PRINTING)
 	add_custom_target(printing_templatesLink ALL
-		COMMAND
-		rm -f ${CMAKE_BINARY_DIR}/printing_templates &&
-		ln -sf ${CMAKE_SOURCE_DIR}/printing_templates ${CMAKE_BINARY_DIR}/printing_templates
+		COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/printing_templates
+		COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/printing_templates ${CMAKE_BINARY_DIR}/printing_templates
 	)
 endif()
 if(BUILD_DOCS OR INSTALL_DOCS)
 	add_custom_target(
 		install_documentation ALL
-		COMMAND
-		rm -rf ${CMAKE_BINARY_DIR}/Documentation &&
-		ln -sf ${CMAKE_SOURCE_DIR}/Documentation/output ${CMAKE_BINARY_DIR}/Documentation
+		COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/Documentation
+		COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/Documentation/output ${CMAKE_BINARY_DIR}/Documentation
 	)
 endif()
 if(BUILD_DOCS)
