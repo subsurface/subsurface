05cb7acb5e8b560cb5427be2f2186d7f136c2d66
Dirk Hohndel <dirk@hohndel.org>
2026-02-14 14:53:51 -0800

    Windows/MSVC: avoid symlinks
    Yes, they are possible on Windows (with admin rights, I think). But this is
    cleaner. Well, it's more verbose and it's noise in the files, but it won't come
    back to bite us.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- cmake/Modules/RunOnBuildDir.cmake
+++ cmake/Modules/RunOnBuildDir.cmake
@@ -1,22 +1,45 @@
 # install a few things so that one can run Subsurface from the build
 # directory
 # Use cmake -E commands for cross-platform compatibility (works on Windows/MSVC)
-add_custom_target(themeLink ALL
-	COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/theme
-	COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/theme ${CMAKE_BINARY_DIR}/theme
-)
-if(NOT NO_PRINTING)
-	add_custom_target(printing_templatesLink ALL
-		COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/printing_templates
-		COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/printing_templates ${CMAKE_BINARY_DIR}/printing_templates
+# On Windows/MSVC, use copy_directory instead of symlinks (symlinks require admin privileges)
+if(MSVC)
+	add_custom_target(themeLink ALL
+		COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/theme
+		COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/theme ${CMAKE_BINARY_DIR}/theme
+	)
+else()
+	add_custom_target(themeLink ALL
+		COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/theme
+		COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/theme ${CMAKE_BINARY_DIR}/theme
 	)
 endif()
+if(NOT NO_PRINTING)
+	if(MSVC)
+		add_custom_target(printing_templatesLink ALL
+			COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/printing_templates
+			COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/printing_templates ${CMAKE_BINARY_DIR}/printing_templates
+		)
+	else()
+		add_custom_target(printing_templatesLink ALL
+			COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/printing_templates
+			COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/printing_templates ${CMAKE_BINARY_DIR}/printing_templates
+		)
+	endif()
+endif()
 if(BUILD_DOCS OR INSTALL_DOCS)
-	add_custom_target(
-		install_documentation ALL
-		COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/Documentation
-		COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/Documentation/output ${CMAKE_BINARY_DIR}/Documentation
-	)
+	if(MSVC)
+		add_custom_target(
+			install_documentation ALL
+			COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/Documentation
+			COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/Documentation/output ${CMAKE_BINARY_DIR}/Documentation
+		)
+	else()
+		add_custom_target(
+			install_documentation ALL
+			COMMAND ${CMAKE_COMMAND} -E rm -rf ${CMAKE_BINARY_DIR}/Documentation
+			COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_SOURCE_DIR}/Documentation/output ${CMAKE_BINARY_DIR}/Documentation
+		)
+	endif()
 endif()
 if(BUILD_DOCS)
 	add_custom_target(
