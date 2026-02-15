694d124b2d30ba10e8052840978a7021d23c5603
Dirk Hohndel <dirk@hohndel.org>
2026-02-15 08:42:50 -0800

    Windows/MSVC: add qlitehtml to the build
    We need our own fork for the user manual to work.
    
    Oddly, the Windows MSVC build uses qlitehtml1.lib, so let's add that to the
    names we are looking for.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- cmake/Modules/HandleFindQLiteHtml.cmake
+++ cmake/Modules/HandleFindQLiteHtml.cmake
@@ -2,8 +2,11 @@
 # Find QLiteHtml library for Qt6 user manual rendering
 
 # QLiteHtml doesn't provide pkg-config, so we manually search for it
+# Search in CMAKE_PREFIX_PATH directories as well as standard locations
 find_path(QLITEHTML_INCLUDE_DIR
 	NAMES qlitehtmlwidget.h
+	PATH_SUFFIXES qlitehtml include/qlitehtml
+	HINTS ${CMAKE_PREFIX_PATH}
 	PATHS
 		${CMAKE_INSTALL_PREFIX}/include/qlitehtml
 		/usr/local/include/qlitehtml
@@ -12,7 +15,8 @@ find_path(QLITEHTML_INCLUDE_DIR
 )
 
 find_library(QLITEHTML_LIBRARY
-	NAMES qlitehtml libqlitehtml
+	NAMES qlitehtml qlitehtml1 libqlitehtml
+	HINTS ${CMAKE_PREFIX_PATH}
 	PATHS
 		${CMAKE_INSTALL_PREFIX}/lib
 		/usr/local/lib
