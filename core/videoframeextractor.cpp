66a47485952127bbac3ee914e7765b410a60294c
Dirk Hohndel <dirk@hohndel.org>
2026-03-03 18:15:22 -0800

    iOS: first stab of iOS build
    This was mainly done by Claude - but with a fair bit of help from me.
    Still, definitely an LLM driven development step.
    
    We switched to cmake and structured the build process along the lines
    of the Android process to keep as much similarity as possible.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- core/videoframeextractor.cpp
+++ core/videoframeextractor.cpp
@@ -5,7 +5,9 @@
 #include "core/errorhelper.h"
 
 #include <QtConcurrent>
+#ifndef Q_OS_IOS
 #include <QProcess>
+#endif
 
 // Note: this is a global instead of a function-local variable on purpose.
 // We don't want this to be generated in a different thread context if
@@ -78,6 +80,10 @@ void VideoFrameExtractor::processItem(QString originalFilename, QString filename
 		position.seconds = clamp(duration.seconds * prefs.extract_video_thumbnails_position / 100,
 					 0, duration.seconds);
 	}
+#ifdef Q_OS_IOS
+	// QProcess is not available on iOS; video thumbnail extraction is not supported
+	return fail(originalFilename, duration, false);
+#else
 	QString posString = QString("%1:%2:%3").arg(position.seconds / 3600, 2, 10, QChar('0'))
 					       .arg((position.seconds % 3600) / 60, 2, 10, QChar('0'))
 					       .arg(position.seconds % 60, 2, 10, QChar('0'));
@@ -115,4 +121,5 @@ void VideoFrameExtractor::processItem(QString originalFilename, QString filename
 	emit extracted(originalFilename, std::move(img), duration, position);
 	QMutexLocker l(&lock);
 	workingOn.remove(originalFilename);
+#endif
 }
