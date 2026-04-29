b4c17025d865c76a6ae3ab8eea94a6535c8bce8d
Dirk Hohndel <dirk@hohndel.org>
2026-04-28 17:46:26 -0700

    translations: fix Qt 6 QtLinguist usage
    The call to the Qt 6 version of qt_add_translation(s) was completely bogus, no
    idea why this didn't give us an error.
    
    In order to get both desktop and mobile sources, this needs to be called in the
    desktop context and explicitly include the mobile sources. That's very fragile.
    
    On the plus side, the creation of updated translation sources is much simpler,
    hence the substantial changes to the updatetranslationsource script.
    
    We also need to declare a language for our translation source file.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- scripts/updatetranslationsource.sh
+++ scripts/updatetranslationsource.sh
@@ -1,71 +1,80 @@
 #!/bin/bash
 #
-# ugly hack - this does NOT work with the normal layout of our sources
-# the build dir needs to be outside of the src/subsurface tree for this to work
-# in order to run this I have this layout
-# .../src/subsurface
-# .../src/subsurface-build
-# and then run this in that latter directory
+# translations have become much simpler with Qt 6 (after some birthing pains),
+# and as far as I can tell only one major annoyance remains... now build paths
+# are included in the locations - which is redundant, as those are derived from
+# existing source files already included.
+
+# The recommended process now is as follows:
+# do a regular Subsurface desktop build with Qt 6
+# assuming Subsurface sources are in .../src/subsurface:
+# cd .../src
+# bash ./subsurface/scripts/build.sh -build-with-qt6 -desktop
+#
+# with this the build directory will be .../src/subsurface/build -- this is used
+# later in this code!
+#
+# then run this script before pushing changes to Transifex
 
 if [[ ! -d translations || ! -f translations/subsurface_source.qm ]] ; then
-	echo Start from the build folder
+	echo "Start from the build folder after successful desktop build"
 	exit 1
 fi
 
-if [[ "$1" = "-nopush" ]] ; then
-	NOPUSH="1"
+if [[ $(basename "$(pwd)") != "build" || $(basename "$(cd .. ; pwd)") != "subsurface" ]] ; then
+	echo "The directory layout doesn't match my expectations, please consult the source of this script"
+	exit 1
 fi
+
+PUSH=""
+COMMIT=""
+
+while [ $# -gt 0 ]; do
+        arg="$1"
+        case $arg in
+                -push)
+                        PUSH="1"
+                        ;;
+                -commit)
+                        COMMIT="1"
+                        ;;
+                *)
+                        echo "Unknown command line argument $arg"
+                        echo "Usage: ${BASH_SOURCE[0]} [-push] [-commit]"
+        esac
+        shift
+done
 
 SRC=$(grep Subsurface_SOURCE_DIR CMakeCache.txt | cut -d= -f2)
 
-pushd $SRC
+pushd "$SRC" 2>/dev/null || ( echo "pushd $SRC failed" ; exit )
 
 # let's make sure the tree is clean
-git status | grep "Changes not staged for commit" 2>/dev/null && echo "tree not clean" && exit 1
-git status | grep "Changes to be committed" 2>/dev/null && echo "tree not clean" && exit 1
+if git status | grep -E "(Changes not staged for commit|Changes to be committed)" 2>/dev/null ; then
+	echo "tree not clean"
+	exit 1
+fi
 
-# now remove the translations and remove access to the kirigami sources
-# and any old sources under tmp as well as build directories inside the source tree
-chmod 000 mobile-widgets/3rdparty
-[ -d tmp ] && chmod 000 tmp
-[ -d build ] && chmod 000 build
-[ -d build-mobile ] && chmod 000 build-mobile
-rm translations/subsurface_source.ts
+popd 2>/dev/null || ( echo "popd failed" ; exit )
 
-# enable creating the translation strings
-sed -i.bak 's/# qt5_create_translation/ qt5_create_translation/ ; s/# add_custom_target(translations_update/ add_custom_target(translations_update/' translations/CMakeLists.txt
+make update_translations 2>&1 | tee translationsupdate.log
 
-popd
+cd "$SRC" || ( echo "cd $SRC failed" ; exit )
 
-# recreate make files and create translation strings
-cmake .
-pushd translations
-make translations_update > translationsupdate.log 2>&1
-popd
+for TSFILE in translations/*.ts ; do sed -i.bak '/..\/build/d' "$TSFILE" ; done
 
-# restore the CMakeLists.txt and rebuild makefiles
-cp $SRC/translations/CMakeLists.txt.bak $SRC/translations/CMakeLists.txt
-cmake .
+if git status | grep -E "(Changes not staged for commit|Changes to be committed)" 2>/dev/null ; then
+	if [ "$COMMIT" = "1" ] && git status | grep -E "(Changes not staged for commit|Changes to be committed)" 2>/dev/null ; then
+		# now add the new source strings to git and remove the rest of the files we created
+		git add translations/subsurface_source.ts
+		git commit -s -m "Update translation source strings"
+		git reset --hard
+	fi
 
-pushd $SRC
-
-# double up the numerusform lines so Transifex is happy
-awk '/<numerusform><\/numerusform>/{print $0}{print $0}' translations/subsurface_source.ts > translations/subsurface_source.ts.new
-mv translations/subsurface_source.ts.new translations/subsurface_source.ts
-
-# now add the new source strings to git and remove the rest of the files we created 
-git add translations/subsurface_source.ts
-git commit -s -m "Update translation source strings"
-git reset --hard
-
-# now enable access to kirigami again
-chmod 755 mobile-widgets/3rdparty
-[ -d tmp ] && chmod 755 tmp
-[ -d build ] && chmod 755 build
-[ -d build-mobile ] && chmod 755 build-mobile
-
-# this really depends on my filesystem layout
-# push sources to Transifex
-if [[ "$NOPUSH" != "1" ]] ; then
-	~/transifex-client/tx push -s
+	if [ "$PUSH" = "1" ] ; then
+		# push sources to Transifex
+		tx push -s
+	fi
+else
+	echo "no changes to translation sources"
 fi
