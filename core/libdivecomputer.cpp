subsurface/subsurface

commit 37deb3048933940c00b2de429862572e07b6e371

Import: Add Option to Force Bluetooth Classic.
Add an option to force Bluetooth Classic without falling back to BLE.
This can be achieved by using a 'BT:' prefix to a Bluetooth address in
the same way 'LE:' forces BLE.
This can be used in cases where it is known that BLE will not succed
with the import / firmware upload.

Signed-off-by: Michael Keller <github@ike.ch>

core/libdivecomputer.cpp
@@ -49,6 +49,7 @@
 #include "core/qthelper.h"
 #include "core/pref.h"
 #include "core/file.h"
+#include "core/bluetoothaddress.h"
 #include <algorithm>
 #include <array>
 #include <charconv>
@@ -1318,8 +1319,11 @@ unsigned int get_supported_transports(device_data_t *data)
 		 */
 		if (data->bluetooth_mode) {
 			supported &= (DC_TRANSPORT_BLUETOOTH | DC_TRANSPORT_BLE);
-			if (starts_with(data->devname, "LE:"))
+			QString devname = QString::fromStdString(data->devname);
+			if (isBluetoothLowEnergyAddress(devname))
 				supported &= DC_TRANSPORT_BLE;
+			else if (isBluetoothClassicAddress(devname))
+				supported &= DC_TRANSPORT_BLUETOOTH;
 		} else {
 			supported &= ~(DC_TRANSPORT_BLUETOOTH | DC_TRANSPORT_BLE);
 		}
@@ -1399,9 +1403,9 @@ static dc_status_t irda_device_open(dc_iostream_t **iostream, dc_context_t *cont
 }
 
 #if defined(BT_SUPPORT) && !defined(__ANDROID__) && !defined(__APPLE__)
-static dc_status_t bluetooth_device_open(dc_context_t *context, device_data_t *data)
+static dc_status_t bluetooth_device_open(dc_context_t *context, device_data_t *data, const char *devname)
 {
-	dc_bluetooth_address_t address = dc_bluetooth_str2addr(data->devname.c_str());
+	dc_bluetooth_address_t address = dc_bluetooth_str2addr(devname);
 	dc_iterator_t *iterator = NULL;
 	dc_bluetooth_device_t *device = NULL;
 
@@ -1443,12 +1447,13 @@ dc_status_t divecomputer_device_open(device_data_t *data)
 
 #ifdef BT_SUPPORT
 	if (transports & DC_TRANSPORT_BLUETOOTH) {
-		dev_info("Opening rfcomm stream %s", data->devname.c_str());
+		std::string address = bluetoothAddressWithoutPrefix(QString::fromStdString(data->devname)).toStdString();
+		dev_info("Opening rfcomm stream %s", address.c_str());
 #if defined(__ANDROID__) || defined(__APPLE__)
 		// we don't have BT on iOS in the first place, so this is for Android and macOS
-		rc = rfcomm_stream_open(&data->iostream, context, data->devname.c_str());
+		rc = rfcomm_stream_open(&data->iostream, context, address.c_str());
 #else
-		rc = bluetooth_device_open(context, data);
+		rc = bluetooth_device_open(context, data, address.c_str());
 #endif
 		if (rc == DC_STATUS_SUCCESS)
 			return rc;

cli-downloader.cpp
@@ -3,6 +3,7 @@
 #include "core/configuredivecomputer.h"
 #include "core/downloadfromdcthread.h"
 #include "core/libdivecomputer.h"
+#include "core/bluetoothaddress.h"
 #include "core/subsurfacestartup.h"
 #include <QCoreApplication>
 #include <QEventLoop>
@@ -18,52 +19,19 @@ static QString expandTilde(const QString &path)
 	return path;
 }
 
-static bool isHexDigit(QChar c)
-{
-	return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
-}
-
-static bool looksLikeBluetoothAddress(const QString &device)
-{
-	QString address = device.trimmed();
-	if (address.startsWith("LE:"))
-		address = address.mid(3);
-
-	if (address.size() == 12) {
-		for (QChar c: address) {
-			if (!isHexDigit(c))
-				return false;
-		}
-		return true;
-	}
-
-	if (address.size() == 17) {
-		for (int i = 0; i < address.size(); ++i) {
-			if (i % 3 == 2) {
-				if (address[i] != ':' && address[i] != '-')
-					return false;
-			} else if (!isHexDigit(address[i])) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	return false;
-}
-
 // Perform a firmware update from the command line. Returns 0 on success, non-zero on failure.
 int cliFirmwareUpdate(const std::string &vendor, const std::string &product, const std::string &device, const std::string &filename, bool force)
 {
 	ConfigureDiveComputer config;
 	DCDeviceData localData;
 	QString deviceName = QString::fromStdString(device);
+	QString bluetoothAddress = extractBluetoothAddress(deviceName);
 
 	// prepare device_data
 	localData.setVendor(QString::fromStdString(vendor));
 	localData.setProduct(QString::fromStdString(product));
-	localData.setBluetoothMode(looksLikeBluetoothAddress(deviceName));
-	localData.setDevName(deviceName);
+	localData.setBluetoothMode(!bluetoothAddress.isEmpty());
+	localData.setDevName(bluetoothAddress.isEmpty() ? deviceName : bluetoothAddress);
 	localData.setSaveLog(!logfile_name.empty());
 	device_data_t *data = localData.internalData();
 
@@ -152,11 +120,13 @@ void cliDownloader(const std::string &vendor, const std::string &product, const 
 	});
 
 	auto data = diveImportedModel.thread.data();
+	QString deviceName = QString::fromStdString(device);
+	QString bluetoothAddress = extractBluetoothAddress(deviceName);
 	data->setVendor(QString::fromStdString(vendor));
 	data->setProduct(QString::fromStdString(product));
-	data->setBluetoothMode(looksLikeBluetoothAddress(QString::fromStdString(device)));
+	data->setBluetoothMode(!bluetoothAddress.isEmpty());
 	if (data->vendor() == "Uemis") {
-		QString devname = QString::fromStdString(device);
+		QString devname = deviceName;
 		int colon = devname.indexOf(QStringLiteral(":\\ (UEMISSDA)"));
 		if (colon >= 0) {
 			devname.truncate(colon + 2);
@@ -164,7 +134,7 @@ void cliDownloader(const std::string &vendor, const std::string &product, const 
 		}
 		data->setDevName(devname);
 	} else {
-		data->setDevName(QString::fromStdString(device));
+		data->setDevName(bluetoothAddress.isEmpty() ? deviceName : bluetoothAddress);
 	}
 
 	// some assumptions - should all be configurable

core/CMakeLists.txt
@@ -39,6 +39,8 @@ endif()
 
 # compile the core library part in C, part in C++
 set(SUBSURFACE_CORE_LIB_SRCS
+	bluetoothaddress.cpp
+	bluetoothaddress.h
 	checkcloudconnection.cpp
 	checkcloudconnection.h
 	cloudstorage.cpp

core/bluetoothaddress.cpp
@@ -1,0 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0
+#include "bluetoothaddress.h"
+
+#include "errorhelper.h"
+#include <QRegularExpression>
+
+bool isBluetoothAddress(const QString &address)
+{
+	return !extractBluetoothAddress(address).isEmpty();
+}
+
+bool isBluetoothLowEnergyAddress(const QString &address)
+{
+	return address.startsWith("LE:", Qt::CaseInsensitive) && extractBluetoothAddress(address) == address.trimmed();
+}
+
+bool isBluetoothClassicAddress(const QString &address)
+{
+	return address.startsWith("BT:", Qt::CaseInsensitive) && extractBluetoothAddress(address) == address.trimmed();
+}
+
+QString bluetoothAddressWithoutPrefix(const QString &address)
+{
+	return isBluetoothLowEnergyAddress(address) || isBluetoothClassicAddress(address) ? address.mid(3) : address;
+}
+
+QString extractBluetoothAddress(const QString &address)
+{
+	// UUIDs identify BLE devices on Apple platforms and cannot be forced to Classic.
+	if (address.contains("BT:{", Qt::CaseInsensitive))
+		return {};
+
+	QRegularExpression compactMac("^(?:(?:LE|BT):)?[0-9A-F]{12}$", QRegularExpression::CaseInsensitiveOption);
+	if (compactMac.match(address.trimmed()).hasMatch())
+		return address.trimmed();
+
+	static const QString macAddress = "(?:(?:[0-9A-F]{2}:){5}[0-9A-F]{2}|(?:[0-9A-F]{2}-){5}[0-9A-F]{2})";
+	QRegularExpression re(QString("(?:(?:LE|BT):)?%1|(?:LE:)?{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}}").arg(macAddress),
+			      QRegularExpression::CaseInsensitiveOption);
+	return re.match(address).captured(0);
+}
+
+std::pair<QString, QString> extractBluetoothNameAddress(const QString &address)
+{
+	QString extractedAddress = extractBluetoothAddress(address);
+	if (extractedAddress == address.trimmed())
+		return { extractedAddress, QString() };
+	if (extractedAddress.isEmpty()) {
+		report_info("can't parse address %s", qPrintable(address));
+		return { QString(), QString() };
+	}
+
+	QRegularExpression re("^([^()]+)\\(([^)]*\\))$");
+	QRegularExpressionMatch m = re.match(address);
+	if (m.hasMatch())
+		return { extractedAddress, m.captured(1).trimmed() };
+	report_info("can't parse address %s", qPrintable(address));
+	return { QString(), QString() };
+}

core/bluetoothaddress.h
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef BLUETOOTHADDRESS_H
+#define BLUETOOTHADDRESS_H
+
+#include <QString>
+#include <utility>
+
+bool isBluetoothAddress(const QString &address);
+bool isBluetoothLowEnergyAddress(const QString &address);
+bool isBluetoothClassicAddress(const QString &address);
+QString bluetoothAddressWithoutPrefix(const QString &address);
+QString extractBluetoothAddress(const QString &address);
+std::pair<QString, QString> extractBluetoothNameAddress(const QString &address);
+
+#endif

core/btdiscovery.cpp
@@ -503,36 +503,6 @@ void BTDiscovery::stopAgent()
 	discoveryAgent->stop();
 }
 
-bool isBluetoothAddress(const QString &address)
-{
-	return extractBluetoothAddress(address) != QString();
-}
-QString extractBluetoothAddress(const QString &address)
-{
-	QRegularExpression re("(LE:)*([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}|{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}})",
-			      QRegularExpression::CaseInsensitiveOption);
-	QRegularExpressionMatch m = re.match(address);
-	return m.captured(0);
-}
-
-std::pair<QString, QString> extractBluetoothNameAddress(const QString &address)
-{
-	// sometimes our device text is of the form "name (address)", sometimes it's just "address"
-	// let's simply return the address
-	QString extractedAddress = extractBluetoothAddress(address);
-	if (extractedAddress == address.trimmed())
-		return { address, QString() };
-
-	QRegularExpression re("^([^()]+)\\(([^)]*\\))$");
-	QRegularExpressionMatch m = re.match(address);
-	if (m.hasMatch()) {
-		QString name = m.captured(1).trimmed();
-		return { extractedAddress, name };
-	}
-	report_info("can't parse address %s", qPrintable(address));
-	return { QString(), QString() };
-}
-
 void saveBtDeviceInfo(const QString &devaddr, QBluetoothDeviceInfo deviceInfo)
 {
 	btDeviceInfo[devaddr] = deviceInfo;

core/btdiscovery.h
@@ -10,6 +10,7 @@
 #include <QBluetoothDeviceDiscoveryAgent>
 #include <QBluetoothUuid>
 #include "core/libdivecomputer.h"
+#include "core/bluetoothaddress.h"
 
 #if defined(Q_OS_ANDROID)
 #include <QJniObject>
@@ -17,10 +18,7 @@
 #endif
 
 void saveBtDeviceInfo(const QString &devaddr, QBluetoothDeviceInfo deviceInfo);
-bool isBluetoothAddress(const QString &address);
 bool matchesKnownDiveComputerNames(QString btName);
-QString extractBluetoothAddress(const QString &address);
-std::pair<QString, QString> extractBluetoothNameAddress(const QString &address); // returns address/name pair
 QBluetoothDeviceInfo getBtDeviceInfo(const QString &devaddr);
 
 class BTDiscovery : public QObject {

desktop-widgets/btdeviceselectiondialog.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QMessageBox>
 #include <QMenu>
+#include <QStandardItemModel>
 #include "core/btdiscovery.h"
 
 #include <QBluetoothUuid>
@@ -222,6 +223,7 @@ void BtDeviceSelectionDialog::currentItemChanged(QListWidgetItem *item, QListWid
 {
 	// If the list is cleared, we get a signal with a null item pointer
 	if (!item) {
+		updateBluetoothModeAvailability(nullptr);
 		ui->save->setEnabled(false);
 		return;
 	}
@@ -256,7 +258,25 @@ void BtDeviceSelectionDialog::currentItemChanged(QListWidgetItem *item, QListWid
 #endif
 	// Update the status message and the save button
 	ui->dialogStatus->setText(statusMessage);
+	updateBluetoothModeAvailability(&remoteDeviceInfo);
 	ui->save->setEnabled(enableSaveButton);
+}
+
+void BtDeviceSelectionDialog::updateBluetoothModeAvailability(const QBluetoothDeviceInfo *device)
+{
+	auto model = qobject_cast<QStandardItemModel *>(ui->btMode->model());
+	QStandardItem *classicItem = model ? model->item(2) : nullptr;
+	if (!classicItem)
+		return;
+
+	bool classicAvailable = !device || !device->address().isNull();
+	classicItem->setEnabled(classicAvailable);
+	if (!classicAvailable) {
+		if (ui->btMode->currentIndex() == 2)
+			ui->btMode->setCurrentIndex(0);
+		ui->dialogStatus->setText(ui->dialogStatus->text() + " " +
+					  tr("Bluetooth Classic is unavailable for devices identified by UUID."));
+	}
 }
 
 void BtDeviceSelectionDialog::localDeviceChanged(int index)
@@ -410,8 +430,10 @@ QString BtDeviceSelectionDialog::getSelectedDeviceAddress()
 		return markBLEAddress(device);
 	case 1:		// Force LE
 		return btDeviceAddress(device, true);
-	case 2:		// Force classical
-		return btDeviceAddress(device, false);
+	case 2:		// Force Classic
+		// Classic uses a MAC address. Apple platforms expose BLE devices as UUIDs,
+		// for which forcing Classic is not meaningful.
+		return device->address().isNull() ? QString() : "BT:" + btDeviceAddress(device, false);
 	}
 }
 

commit d045ed9a937e0ea33d46b6901eefec459a327f07

Linux: Fix Bluetooth Classic Support.
Fix the Bluetooth classic support under linux that was broken in #4865.

Signed-off-by: Michael Keller <github@ike.ch>

core/libdivecomputer.cpp
@@ -1402,7 +1402,7 @@ static dc_status_t irda_device_open(dc_iostream_t **iostream, dc_context_t *cont
 	return dc_irda_open(&data->iostream, context, address, 1);
 }
 
-#if defined(BT_SUPPORT) && !defined(__ANDROID__) && !defined(__APPLE__)
+#if defined(BT_SUPPORT) && defined(_WIN32)
 static dc_status_t bluetooth_device_open(dc_context_t *context, device_data_t *data, const char *devname)
 {
 	dc_bluetooth_address_t address = dc_bluetooth_str2addr(devname);
@@ -1449,8 +1449,9 @@ dc_status_t divecomputer_device_open(device_data_t *data)
 	if (transports & DC_TRANSPORT_BLUETOOTH) {
 		std::string address = bluetoothAddressWithoutPrefix(QString::fromStdString(data->devname)).toStdString();
 		dev_info("Opening rfcomm stream %s", address.c_str());
-#if defined(__ANDROID__) || defined(__APPLE__)
-		// we don't have BT on iOS in the first place, so this is for Android and macOS
+#if !defined(_WIN32)
+		// Use the Qt RFCOMM implementation on platforms where libdivecomputer's
+		// native Bluetooth implementation is not part of the bundled build.
 		rc = rfcomm_stream_open(&data->iostream, context, address.c_str());
 #else
 		rc = bluetooth_device_open(context, data, address.c_str());

core/qtserialbluetooth.cpp
@@ -56,7 +56,11 @@ static dc_status_t qt_serial_open(qt_serial_t **io, dc_context_t*, const char *d
 	// Wait until the connection succeeds or until an error occurs
 	QEventLoop loop;
 	loop.connect(serial_port->socket, SIGNAL(connected()), SLOT(quit()));
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	loop.connect(serial_port->socket, &QBluetoothSocket::errorOccurred, &loop, &QEventLoop::quit);
+#else
 	loop.connect(serial_port->socket, SIGNAL(error(QBluetoothSocket::SocketError)), SLOT(quit()));
+#endif
 
 	// Create a timer. If the connection doesn't succeed after five seconds or no error occurs then stop the opening step
 	QTimer timer;